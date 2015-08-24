38194a15586a1d88c61bc979abe7b0ac0a169ba0
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-08-24 09:12:00 +0200

    added serviceRunner project

---
+++ ServiceRunner/ServiceRunner/CrmService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceRunner
+{
+	public partial class CrmService : ServiceBase
+	{
+		public CrmService()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnStart(string[] args)
+		{
+		}
+
+		protected override void OnStop()
+		{
+		}
+	}
+}
