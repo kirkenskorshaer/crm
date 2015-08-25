7ddab6df83d4497805e3fc63f08a55b26e61cd23
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-08-25 08:50:25 +0200

    (RemoteAdministration) added CopyFile method

--- SystemInterface/SystemInterface/RemoteAdministration.cs
+++ SystemInterface/SystemInterface/RemoteAdministration.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
 using System.Security;
@@ -68,6 +70,62 @@ namespace SystemInterface
 			Collection<PSObject> psObjects = ExecutePowershell(ip, username, password, powerShellAction);
 
 			return psObjects.Count == 1;
+		}
+
+		public void CopyFile(string ip, string username, string password, string sourcePath, string targetPath)
+		{
+			string shell = "http://schemas.microsoft.com/powershell/Microsoft.PowerShell";
+
+			using (SecureString passing = new SecureString())
+			{
+				foreach (char passwordCharacter in password)
+				{
+					passing.AppendChar(passwordCharacter);
+				}
+
+				passing.MakeReadOnly();
+
+				PSCredential cred = new PSCredential(username, passing);
+
+				WSManConnectionInfo connectionInfo = new WSManConnectionInfo(true, ip, 5986, "/wsman", shell, cred);
+
+				connectionInfo.OperationTimeout = 4 * 60 * 1000; // 4 minutes.
+				connectionInfo.OpenTimeout = 1 * 60 * 1000;
+				connectionInfo.SkipCACheck = true;
+
+				using (Runspace runSpace = RunspaceFactory.CreateRunspace(connectionInfo))
+				{
+					runSpace.Open();
+
+					PowerShell powershell = PowerShell.Create();
+					powershell.Runspace = runSpace;
+					string powershellScript = "";
+					powershellScript += Environment.NewLine + "$append = [System.io.FileMode]::Append";
+
+					powershellScript += Environment.NewLine + "[System.io.FileStream] $targetStream = New-Object -TypeName 'System.io.FileStream' -ArgumentList(\"" + targetPath + "\", $append);";
+
+					FileStream sourceStream = File.OpenRead(sourcePath);
+					int bufferSize = 1024;
+					byte[] readBuffer = new byte[bufferSize];
+					int readBytes;
+					while ((readBytes = sourceStream.Read(readBuffer, 0, readBuffer.Length)) > 0)
+					{
+						string binaryString = readBuffer.ToList().Aggregate("", (summed, byteValue) => (string.IsNullOrWhiteSpace(summed) ? "" : summed + ",") + byteValue.ToString());
+						powershellScript += Environment.NewLine + "[byte[]] $readBuffer = " + binaryString;
+						powershellScript += Environment.NewLine + "$targetStream.Write($readBuffer, 0, " + readBytes + ");";
+					}
+
+					sourceStream.Flush();
+					sourceStream.Dispose();
+
+					powershellScript += Environment.NewLine + "$targetStream.Flush();";
+					powershellScript += Environment.NewLine + "$targetStream.Dispose()";
+
+					powershell.AddScript(powershellScript);
+
+					powershell.Invoke();
+				}
+			}
 		}
 	}
 }
