600e6024e744145e5ad3993f0eb22eb48f95fc94
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-08-26 14:12:55 +0200

    (Heart) added Heart file for administration layer

---
+++ Administration/Administration/Heart.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Administration
+{
+	public class Heart
+	{
+		private bool _run = true;
+
+		public void Run()
+		{
+			while (_run)
+			{
+				try
+				{
+					HeartBeat();
+				}
+				catch
+				{
+					// ignored
+				}
+			}
+		}
+
+		public void HeartBeat()
+		{
+			_run = false;
+		}
+	}
+}
