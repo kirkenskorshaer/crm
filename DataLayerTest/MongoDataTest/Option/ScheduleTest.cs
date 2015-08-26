2f6c412b1bde311b9d6f57f100af3fb6c71dfb43
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-08-26 14:17:39 +0200

    (ScheduleTest) added ScheduleTest

---
+++ DataLayerTest/MongoDataTest/Option/ScheduleTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using DataLayer.MongoData.Option;
+using NUnit.Framework;
+
+namespace DataLayerTest.MongoDataTest.Option
+{
+	[TestFixture]
+	public class ScheduleTest
+	{
+		[TestCase(25, 0, 67, 25, 1, 7)]
+		[TestCase(22, 0, 67, 24, 0, 0)]
+		[TestCase(25, 9, 67, 25, 18, 0)]
+		[TestCase(10, 19, 67, 17, 0, 0)]
+		public void MoveNextTest(int dayOrigin, int hourOrigin, int minutesBetweenExecution, int dayExpected, int hourExpected, int minuteExpected)
+		{
+			Schedule schedule = new Schedule()
+			{
+				DaysOfMonthToSkip = new List<int> { 1, 2, 3, 4, 5, 10, 11, 12, 13, 14, 15 },
+				DaysOfWeekToSkip = new List<DayOfWeek>() { DayOfWeek.Saturday, DayOfWeek.Sunday },
+				HoursOfDayToSkip = new List<int>() { 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 },
+				Recurring = true,
+				TimeBetweenAllowedExecutions = TimeSpan.FromMinutes(minutesBetweenExecution),
+				NextAllowedExecution = new DateTime(2015, 8, dayOrigin, hourOrigin, 0, 0),
+			};
+
+			schedule.MoveNext();
+
+			Assert.AreEqual(new DateTime(2015, 8, dayExpected, hourExpected, minuteExpected, 0).ToString("s"), schedule.NextAllowedExecution.ToString("s"));
+		}
+	}
+}
