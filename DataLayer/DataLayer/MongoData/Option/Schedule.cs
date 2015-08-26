822fdd74aeb8a85ee96f208d455f8ecf2205963d
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-08-26 14:16:06 +0200

    (Schedule) added scheduling class

---
+++ DataLayer/DataLayer/MongoData/Option/Schedule.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DataLayer.MongoData.Option
+{
+	public class Schedule
+	{
+		public bool Recurring { get; set; }
+		[BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+		public DateTime NextAllowedExecution { get; set; }
+		public TimeSpan TimeBetweenAllowedExecutions { get; set; }
+		public List<int> HoursOfDayToSkip { get; set; }
+		public List<DayOfWeek> DaysOfWeekToSkip { get; set; }
+		public List<int> DaysOfMonthToSkip { get; set; }
+
+		public void MoveNext()
+		{
+			NextAllowedExecution += TimeBetweenAllowedExecutions;
+			MoveToFreeTime();
+		}
+
+		private void MoveToFreeTime()
+		{
+			if (DayBlocksForExecution())
+			{
+				NextAllowedExecution = NextAllowedExecution.Date.AddDays(1);
+				MoveToFreeTime();
+				return;
+			}
+			if (HourBlocksForExecution())
+			{
+				int minutes = NextAllowedExecution.Minute;
+				int seconds = NextAllowedExecution.Second;
+				NextAllowedExecution = NextAllowedExecution.AddHours(1).AddMinutes(-minutes).AddSeconds(-seconds);
+				MoveToFreeTime();
+			}
+		}
+
+		private bool HourBlocksForExecution()
+		{
+			if (HoursOfDayToSkip.Contains(NextAllowedExecution.Hour))
+			{
+				return true;
+			}
+			return false;
+		}
+
+		private bool DayBlocksForExecution()
+		{
+			if (DaysOfWeekToSkip.Contains(NextAllowedExecution.DayOfWeek))
+			{
+				return true;
+			}
+			if (DaysOfMonthToSkip.Contains(NextAllowedExecution.Day))
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+}
