e136e61e6c5ef5c6e4677dfeca38caf432bef89b
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-09-01 12:03:52 +0200

    (Email) added email option

---
+++ Administration/Administration/Option/Options/Email.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DataLayer;
+using DatabaseEmail = DataLayer.MongoData.Option.Options.Email;
+using DatabaseOptionBase = DataLayer.MongoData.Option.OptionBase;
+
+namespace Administration.Option.Options
+{
+	public class Email : OptionBase
+	{
+		public Email(MongoConnection connection) : base(connection)
+		{
+		}
+
+		private DatabaseEmail _databaseEmail;
+
+		public static List<Email> Find(MongoConnection connection)
+		{
+			List<DatabaseEmail> databaseEmails = DatabaseOptionBase.ReadAllowed<DatabaseEmail>(connection);
+
+			return databaseEmails.Select(databaseEmail => new Email(connection)
+			{
+				_databaseEmail = databaseEmail,
+			}).ToList();
+		}
+
+		public override void Execute()
+		{
+			SystemInterface.Email emailSender = new SystemInterface.Email();
+			emailSender.Send(_databaseEmail.MessageBody, _databaseEmail.Subject, Config.Email, _databaseEmail.To, Config.EmailSmtpHost, Config.EmailSmtpPort, Config.Email, Config.EmailPassword);
+		}
+	}
+}
