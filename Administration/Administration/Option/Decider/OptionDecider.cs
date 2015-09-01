0d332a2a5c4e44bb64346a043ca8f30956996735
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-09-01 12:05:12 +0200

    (OptionDecider)

---
+++ Administration/Administration/Option/Decider/OptionDecider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Administration.Option.Decider
+{
+	public class OptionDecider
+	{
+		public OptionBase Decide(List<OptionBase> options)
+		{
+			if (options.Any(option => option.GetType().Name == "Email"))
+			{
+				return options.First(option => option.GetType().Name == "Email");
+			}
+
+			return options.First();
+		}
+	}
+}
