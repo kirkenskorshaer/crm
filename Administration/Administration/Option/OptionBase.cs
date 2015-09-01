e6218553fc8d81087aced37f4ca3d0a647927776
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-09-01 11:45:59 +0200

    (OptionBase) added base class for all options

---
+++ Administration/Administration/Option/OptionBase.cs
@@ -0,0 +1,19 @@
+﻿using DataLayer;
+
+
+namespace Administration.Option
+{
+	public abstract class OptionBase
+	{
+		protected readonly MongoConnection Connection;
+		protected readonly DataLayer.MongoData.Config Config;
+
+		protected OptionBase(MongoConnection connection)
+		{
+			Connection = connection;
+			Config = DataLayer.MongoData.Config.GetConfig(Connection);
+		}
+
+		public abstract void Execute();
+	}
+}
