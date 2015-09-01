7d064a227df68a9c2b9acaafdb25ad7c33b89036
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-09-01 11:46:16 +0200

    (Sleep) added option to sleep

---
+++ Administration/Administration/Option/Options/Sleep.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using DataLayer;
+
+namespace Administration.Option.Options
+{
+	public class Sleep : OptionBase
+	{
+		internal static int SleepTime = 100;
+
+		public Sleep(MongoConnection connection) : base(connection)
+		{
+		}
+
+		public override void Execute()
+		{
+			Thread.Sleep(SleepTime);
+		}
+	}
+}
