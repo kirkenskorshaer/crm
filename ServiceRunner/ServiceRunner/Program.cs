5a383d4936af97293f32c9bf2caae34a712d5743
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-09-03 09:20:31 +0200

    Cleanup

--- ServiceRunner/ServiceRunner/Program.cs
+++ ServiceRunner/ServiceRunner/Program.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ServiceProcess;
 
 namespace ServiceRunner
 {
 	static class Program
 	{
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
 		static void Main()
 		{
 			ServiceBase[] ServicesToRun;
