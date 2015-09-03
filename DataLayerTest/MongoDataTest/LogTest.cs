faa3a4efe194b35fd668bf76a08e2b6fcde89fbc
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-09-03 11:48:05 +0200

    (LogTest) added log test

---
+++ DataLayerTest/MongoDataTest/LogTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+using DataLayer.MongoData;
+using NUnit.Framework;
+
+namespace DataLayerTest.MongoDataTest
+{
+	[TestFixture]
+	public class LogTest
+	{
+		private MongoConnection _connection;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_connection = MongoConnection.GetConnection("test");
+			_connection.DropDatabase();
+		}
+
+		[Test]
+		public void ReadLatestTest()
+		{
+			Log.Write(_connection, "test1", string.Empty);
+			Log.Write(_connection, "test2", string.Empty);
+			Log.Write(_connection, "test3", string.Empty);
+
+			Log log = Log.ReadLatest(_connection);
+
+			Assert.AreEqual("test3", log.Message);
+		}
+	}
+}
