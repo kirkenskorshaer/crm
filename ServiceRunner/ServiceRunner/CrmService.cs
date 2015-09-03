c238ac33a94370e17d692d7f2a1299abcdb23225
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-09-03 15:12:38 +0200

    CrmService can now run tasks

--- ServiceRunner/ServiceRunner/CrmService.cs
+++ ServiceRunner/ServiceRunner/CrmService.cs
@@ -1,20 +1,37 @@
-﻿using System.ServiceProcess;
+﻿using System.ComponentModel;
+using System.ServiceProcess;
+using Administration;
 
 namespace ServiceRunner
 {
 	public partial class CrmService : ServiceBase
 	{
+		private BackgroundWorker _backgroundWorker;
+		private Heart _heart;
+
 		public CrmService()
 		{
 			InitializeComponent();
 		}
 
+		private void _backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+		{
+			_heart.Run();
+		}
+
 		protected override void OnStart(string[] args)
 		{
+			_backgroundWorker = new BackgroundWorker();
+			_heart = new Heart();
+
+			_backgroundWorker.DoWork += _backgroundWorker_DoWork;
+
+			_backgroundWorker.RunWorkerAsync();
 		}
 
 		protected override void OnStop()
 		{
+			_heart.Stop();
 		}
 	}
 }
