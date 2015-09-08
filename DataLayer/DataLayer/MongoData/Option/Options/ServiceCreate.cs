85be4cf17118542836e0565e348ee6cd20f68a93
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-09-08 14:32:26 +0200

    (ServiceCreate) added database serviceCreate

---
+++ DataLayer/DataLayer/MongoData/Option/Options/ServiceCreate.cs
@@ -0,0 +1,17 @@
+﻿namespace DataLayer.MongoData.Option.Options
+{
+	public class ServiceCreate : OptionBase
+	{
+		protected override void Execute(MongoConnection connection, bool recurring)
+		{
+			if (recurring)
+			{
+				Update<ServiceCreate>(connection);
+			}
+			else
+			{
+				Delete<ServiceCreate>(connection);
+			}
+		}
+	}
+}
