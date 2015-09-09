b07aa632117eec690088e55b4181485cc3a9cc50
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-09-09 09:20:27 +0200

    (ServiceCreate) added attributes to ServiceCreate

--- DataLayer/DataLayer/MongoData/Option/Options/ServiceCreate.cs
+++ DataLayer/DataLayer/MongoData/Option/Options/ServiceCreate.cs
@@ -2,6 +2,10 @@
 {
 	public class ServiceCreate : OptionBase
 	{
+		public string Ip { get; set; }
+		public string ServiceName { get; set; }
+		public string Path { get; set; }
+
 		protected override void Execute(MongoConnection connection, bool recurring)
 		{
 			if (recurring)
