1a5fa8f3cbd109c2bbff22f363c54937e6a9426c
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-09-09 09:59:51 +0200

    Delete, start and stop service now implements optionBase

--- Administration/Administration/Option/Options/Service/ServiceDelete.cs
+++ Administration/Administration/Option/Options/Service/ServiceDelete.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SystemInterface;
+using DataLayer;
 
 namespace Administration.Option.Options.Service
 {
-	class ServiceDelete
+	public class ServiceDelete : OptionBase
 	{
+		public ServiceDelete(MongoConnection connection, DataLayer.MongoData.Option.OptionBase databaseOption)
+			: base(connection, databaseOption)
+		{
+		}
+
+		protected override void ExecuteOption()
+		{
+			RemoteAdministration administration = new RemoteAdministration();
+
+			DataLayer.MongoData.Option.Options.Service.ServiceCreate serviceCreateDatabase = (DataLayer.MongoData.Option.Options.Service.ServiceCreate)DatabaseOption;
+
+			bool serverExists = DataLayer.MongoData.Server.Exists(Connection, serviceCreateDatabase.Ip);
+
+			if (serverExists == false)
+			{
+				return;
+			}
+
+			DataLayer.MongoData.Server server = DataLayer.MongoData.Server.GetServer(Connection, serviceCreateDatabase.Ip);
+
+			bool serviceExists = administration.ServiceExists(server.Ip, server.Username, server.Password, serviceCreateDatabase.ServiceName);
+
+			if (serviceExists == false)
+			{
+				administration.ServiceDelete(server.Ip, server.Username, server.Password, serviceCreateDatabase.ServiceName);
+			}
+		}
 	}
 }
