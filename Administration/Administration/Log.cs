5e2eefca21179d33e4095c0c19cba3f6aa9e037e
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-09-10 09:25:07 +0200

    (Log) added administration Log class

---
+++ Administration/Administration/Log.cs
@@ -0,0 +1,23 @@
+﻿using DataLayer;
+using DataLayer.MongoData;
+
+namespace Administration
+{
+	public static class Log
+	{
+		public static Config.LogLevelEnum LogLevel = Config.LogLevelEnum.HeartError | Config.LogLevelEnum.OptionError;
+
+		public static void Write(MongoConnection connection, string message, Config.LogLevelEnum logLevel)
+		{
+			Write(connection, message, string.Empty, logLevel);
+		}
+
+		public static void Write(MongoConnection connection, string message, string stackTrace, Config.LogLevelEnum logLevel)
+		{
+			if (LogLevel.HasFlag(logLevel))
+			{
+				DataLayer.MongoData.Log.Write(connection, message, stackTrace, logLevel);
+			}
+		}
+	}
+}
