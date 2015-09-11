a748b4127f7d55ee33c0653fa0aedc3cc705634e
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-09-11 13:00:08 +0200

    added ignored tests on Delete and Start

---
+++ AdministrationTest/Option/Options/Service/ServiceDeleteTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Administration.Option.Options.Service;
+using DataLayer.MongoData;
+using NUnit.Framework;
+
+namespace AdministrationTest.Option.Options.Service
+{
+	[TestFixture]
+	public class ServiceDeleteTest : TestBase
+	{
+		[Test]
+		[Ignore]
+		public void ExecuteOptionTest()
+		{
+			DataLayer.MongoData.Option.Options.Service.ServiceDelete databaseServiceStart = new DataLayer.MongoData.Option.Options.Service.ServiceDelete()
+			{
+				Ip = "127.0.0.1",
+				Name = "localHost",
+				Schedule = CreateSchedule(),
+				ServiceName = "testService",
+			};
+
+			Server server = new Server()
+			{
+				Ip = "127.0.0.1",
+				Password = "test",
+				Username = "test",
+			};
+
+			server.Insert(Connection);
+
+			ServiceDelete serviceDelete = new ServiceDelete(Connection, databaseServiceStart);
+
+			serviceDelete.Execute();
+		}
+	}
+}
---
+++ AdministrationTest/Option/Options/Service/ServiceStartTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Administration.Option.Options.Service;
+using DataLayer.MongoData;
+using NUnit.Framework;
+
+namespace AdministrationTest.Option.Options.Service
+{
+	[TestFixture]
+	public class ServiceStartTest : TestBase
+	{
+		[Test]
+		[Ignore]
+		public void ExecuteOptionTest()
+		{
+			DataLayer.MongoData.Option.Options.Service.ServiceStart databaseServiceStart = new DataLayer.MongoData.Option.Options.Service.ServiceStart()
+			{
+				Ip = "127.0.0.1",
+				Name = "localHost",
+				Schedule = CreateSchedule(),
+				ServiceName = "testService",
+			};
+
+			Server server = new Server()
+			{
+				Ip = "127.0.0.1",
+				Password = "test",
+				Username = "test",
+			};
+
+			server.Insert(Connection);
+
+			ServiceStart serviceStart = new ServiceStart(Connection, databaseServiceStart);
+
+			serviceStart.Execute();
+		}
+	}
+}
