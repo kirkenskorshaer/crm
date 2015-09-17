c58c98e1aa96809092db119a3fc04c07d854d75d
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-09-17 11:14:23 +0200

    (DynamicsCrmConnection) added class to hold a connection to DynamicsCrm

---
+++ SystemInterface/SystemInterface/Dynamics/Crm/DynamicsCrmConnection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xrm.Client;
+using Microsoft.Xrm.Client.Services;
+
+namespace SystemInterface.Dynamics.Crm
+{
+	public class DynamicsCrmConnection
+	{
+		private static DynamicsCrmConnection _connection;
+		public OrganizationService Service;
+
+		private DynamicsCrmConnection(string url, string username, string password)
+		{
+			string connectionString = $"Url={url}; Username={username}; Password={password};";
+			CrmConnection crmConnection = CrmConnection.Parse(connectionString);
+
+			Service = new OrganizationService(crmConnection);
+		}
+
+		public static DynamicsCrmConnection GetConnection(string url, string username, string password)
+		{
+			if (_connection == null)
+			{
+				_connection = new DynamicsCrmConnection(url, username, password);
+			}
+
+			return _connection;
+		}
+	}
+}
