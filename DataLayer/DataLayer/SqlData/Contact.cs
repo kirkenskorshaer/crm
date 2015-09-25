5f89995d8fac3cc80fc95fdba4f3330f6b322cab
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-09-25 08:40:49 +0200

    (Contact) added method MaintainTable

--- DataLayer/DataLayer/SqlData/Contact.cs
+++ DataLayer/DataLayer/SqlData/Contact.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Linq;
 using System.Text;
 
 namespace DataLayer.SqlData
@@ -12,5 +15,30 @@ namespace DataLayer.SqlData
 		public DateTime ModifiedOn;
 		public string Firstname;
 		public string Lastname;
+
+		private static string _databaseName = "contact";
+
+		public static void MaintainTable(SqlConnection sqlConnection)
+		{
+			List<string> columnsInDatabase = Utilities.GetExistingColumns(sqlConnection, _databaseName);
+
+			if (columnsInDatabase.Any() == false)
+			{
+				Utilities.CreateTable(sqlConnection, _databaseName, "id");
+			}
+
+			CreateIfMissing(sqlConnection, columnsInDatabase, "FirstName", "NVARCHAR(MAX)", SqlBoolean.True);
+			CreateIfMissing(sqlConnection, columnsInDatabase, "LastName", "NVARCHAR(MAX)", SqlBoolean.True);
+			CreateIfMissing(sqlConnection, columnsInDatabase, "CreatedOn", "DATETIME", SqlBoolean.False);
+			CreateIfMissing(sqlConnection, columnsInDatabase, "ModifiedOn", "DATETIME", SqlBoolean.False);
+		}
+
+		private static void CreateIfMissing(SqlConnection sqlConnection, List<string> columnsInDatabase, string name, string type, SqlBoolean allowNull)
+		{
+			if (columnsInDatabase.Contains(name) == false)
+			{
+				Utilities.AddColumn(sqlConnection, "contact", name, type, allowNull);
+			}
+		}
 	}
 }
