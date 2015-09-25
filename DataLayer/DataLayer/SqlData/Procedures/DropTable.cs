6f43bb4047f13045a23c0658bd2d4da2013c5ea3
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-09-25 08:55:18 +0200

    (DropTable) added droptable procedure

---
+++ DataLayer/DataLayer/SqlData/Procedures/DropTable.cs
@@ -0,0 +1,70 @@
+﻿using System.Data.SqlClient;
+using System.Text;
+
+namespace DataLayer.SqlData.Procedures
+{
+	internal static class DropTable
+	{
+		internal static void MakeSureDropTableProcedureArePresent(SqlConnection sqlConnection)
+		{
+			DeleteProcedure(sqlConnection);
+
+			CreateProcedure(sqlConnection);
+		}
+
+		private static void DeleteProcedure(SqlConnection sqlConnection)
+		{
+			StringBuilder sqlStringBuilder = new StringBuilder();
+
+			sqlStringBuilder.AppendLine("IF");
+			sqlStringBuilder.AppendLine("(");
+			sqlStringBuilder.AppendLine("	EXISTS");
+			sqlStringBuilder.AppendLine("	(");
+			sqlStringBuilder.AppendLine("		SELECT");
+			sqlStringBuilder.AppendLine("			*");
+			sqlStringBuilder.AppendLine("		FROM");
+			sqlStringBuilder.AppendLine("			SYS.objects");
+			sqlStringBuilder.AppendLine("		WHERE");
+			sqlStringBuilder.AppendLine("			objects.type = 'P'");
+			sqlStringBuilder.AppendLine("			AND");
+			sqlStringBuilder.AppendLine("			objects.name = 'DropTable'");
+			sqlStringBuilder.AppendLine("	)");
+			sqlStringBuilder.AppendLine(")");
+			sqlStringBuilder.AppendLine("BEGIN");
+			sqlStringBuilder.AppendLine("	DROP PROCEDURE DropTable");
+			sqlStringBuilder.AppendLine("END");
+
+			Utilities.ExecuteNonQuery(sqlConnection, sqlStringBuilder, System.Data.CommandType.Text);
+		}
+
+		private static void CreateProcedure(SqlConnection sqlConnection)
+		{
+			StringBuilder sqlStringBuilder = new StringBuilder();
+			sqlStringBuilder = new StringBuilder();
+
+			sqlStringBuilder.AppendLine("CREATE PROCEDURE");
+			sqlStringBuilder.AppendLine("	DropTable");
+			sqlStringBuilder.AppendLine("	@tablename NVARCHAR(128)");
+			sqlStringBuilder.AppendLine("	,@debug BIT = 1");
+			sqlStringBuilder.AppendLine("AS");
+			sqlStringBuilder.AppendLine("BEGIN");
+			sqlStringBuilder.AppendLine("	DECLARE @sql nvarchar(4000)");
+			sqlStringBuilder.AppendLine("");
+			sqlStringBuilder.AppendLine("	SET @sql = '");
+			sqlStringBuilder.AppendLine("	DROP TABLE");
+			sqlStringBuilder.AppendLine("		dbo.'+ QUOTENAME(@tablename)");
+			sqlStringBuilder.AppendLine("");
+			sqlStringBuilder.AppendLine("	IF(@debug = 1)");
+			sqlStringBuilder.AppendLine("	BEGIN");
+			sqlStringBuilder.AppendLine("		PRINT @sql");
+			sqlStringBuilder.AppendLine("	END");
+			sqlStringBuilder.AppendLine("	ELSE");
+			sqlStringBuilder.AppendLine("	BEGIN");
+			sqlStringBuilder.AppendLine("		EXEC SP_EXECUTESQL @sql, N'@tablename NVARCHAR(128)', @tablename = @tablename");
+			sqlStringBuilder.AppendLine("	END");
+			sqlStringBuilder.AppendLine("END");
+
+			Utilities.ExecuteNonQuery(sqlConnection, sqlStringBuilder, System.Data.CommandType.Text);
+		}
+	}
+}
