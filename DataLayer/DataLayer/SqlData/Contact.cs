c4b15de4e9e524d53018afa175533d4790d5b08b
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-09-25 09:17:52 +0200

    (Contact) added sql contact class

---
+++ DataLayer/DataLayer/SqlData/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace DataLayer.SqlData
+{
+	public class Contact
+	{
+		public Guid ContactId;
+		public DateTime CreatedOn;
+		public DateTime ModifiedOn;
+		public string Firstname;
+		public string Lastname;
+	}
+}
