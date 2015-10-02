7b3de80660e8a2b17be95bba963c1bcd7481b4fd
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-10-02 13:19:15 +0200

    (AbstractIdData) added abstract class for all sql data types with a primary key named id

---
+++ DataLayer/DataLayer/SqlData/AbstractIdData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace DataLayer.SqlData
+{
+	public class AbstractIdData : AbstractData
+	{
+		public Guid Id { get; protected set; }
+
+		public void Delete(SqlConnection sqlConnection)
+		{
+			StringBuilder sqlStringBuilder = new StringBuilder();
+			sqlStringBuilder.AppendLine("DELETE FROM");
+			sqlStringBuilder.AppendLine("	" + TableName);
+			sqlStringBuilder.AppendLine("WHERE");
+			sqlStringBuilder.AppendLine("	id = @id");
+
+			Utilities.ExecuteNonQuery(sqlConnection, sqlStringBuilder, CommandType.Text, new KeyValuePair<string, object>("id", Id));
+		}
+	}
+}
