950876d6418c492e270d625765c5b8ce0eb0612a
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-10-07 11:12:55 +0200

    added administrative options for Csv

---
+++ Administration/Administration/Option/Options/Csv/CsvRead.cs
@@ -0,0 +1,36 @@
+﻿using DataLayer;
+using System.Collections.Generic;
+using System.Linq;
+using DatabaseCsvRead = DataLayer.MongoData.Option.Options.Csv.CsvRead;
+using DatabaseOptionBase = DataLayer.MongoData.Option.OptionBase;
+
+namespace Administration.Option.Options.Csv
+{
+	public class CsvRead : OptionBase
+	{
+		private DatabaseCsvRead _databaseCsvRead;
+
+		public CsvRead(MongoConnection connection, DatabaseOptionBase databaseOption) : base(connection, databaseOption)
+		{
+		}
+
+		public static List<CsvRead> Find(MongoConnection connection)
+		{
+			List<DatabaseCsvRead> databaseCsvReads = DatabaseOptionBase.ReadAllowed<DatabaseCsvRead>(connection);
+
+			return databaseCsvReads.Select(databaseCsvRead => new CsvRead(connection, databaseCsvRead)
+			{
+				_databaseCsvRead = databaseCsvRead,
+			}).ToList();
+		}
+
+		protected override bool ExecuteOption()
+		{
+			SystemInterface.Csv.Csv csv = new SystemInterface.Csv.Csv(_databaseCsvRead.Delimeter, _databaseCsvRead.FileName, _databaseCsvRead.FileNameTmp);
+			
+            csv.ReadFields(_databaseCsvRead.KeyName, _databaseCsvRead.KeyValue);
+
+			return true;
+		}
+	}
+}
---
+++ Administration/Administration/Option/Options/Csv/CsvUpdate.cs
@@ -0,0 +1,39 @@
+﻿using DataLayer;
+using System.Collections.Generic;
+using System.Linq;
+using DatabaseCsvUpdate = DataLayer.MongoData.Option.Options.Csv.CsvUpdate;
+using DatabaseOptionBase = DataLayer.MongoData.Option.OptionBase;
+
+namespace Administration.Option.Options.Csv
+{
+	public class CsvUpdate : OptionBase
+	{
+		private DatabaseCsvUpdate _databaseCsvUpdate;
+
+		public CsvUpdate(MongoConnection connection, DatabaseOptionBase databaseOption) : base(connection, databaseOption)
+		{
+		}
+
+		public static List<CsvUpdate> Find(MongoConnection connection)
+		{
+			List<DatabaseCsvUpdate> databaseCsvUpdates = DatabaseOptionBase.ReadAllowed<DatabaseCsvUpdate>(connection);
+
+			return databaseCsvUpdates.Select(databaseCsvUpdate => new CsvUpdate(connection, databaseCsvUpdate)
+			{
+				_databaseCsvUpdate = databaseCsvUpdate,
+			}).ToList();
+		}
+
+		protected override bool ExecuteOption()
+		{
+			string[] columns = _databaseCsvUpdate.CsvElements.Select(csvElement => csvElement.Key).ToArray();
+			string[] values = _databaseCsvUpdate.CsvElements.Select(csvElement => csvElement.Value).ToArray();
+
+			SystemInterface.Csv.Csv csv = new SystemInterface.Csv.Csv(_databaseCsvUpdate.Delimeter, _databaseCsvUpdate.FileName, _databaseCsvUpdate.FileNameTmp, columns);
+
+			csv.Update(_databaseCsvUpdate.KeyName, _databaseCsvUpdate.KeyValue, values);
+
+			return true;
+		}
+	}
+}
