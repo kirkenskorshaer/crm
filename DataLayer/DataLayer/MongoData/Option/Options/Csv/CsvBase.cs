4607b3b656ad1c858bbde0525863c3de1e8ddf5e
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-10-07 09:28:38 +0200

    (CsvBase) added base class for all csv mongo options

---
+++ DataLayer/DataLayer/MongoData/Option/Options/Csv/CsvBase.cs
@@ -0,0 +1,9 @@
+﻿namespace DataLayer.MongoData.Option.Options.Csv
+{
+	public abstract class CsvBase : OptionBase
+	{
+		public string FileName { get; set; }
+		public string FileNameTmp { get; set; }
+		public char Delimeter { get; set; }
+	}
+}
