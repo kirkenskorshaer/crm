058fe3c9d32ccb5a1ebcd9c523bd9616d5e5ba21
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-10-07 09:41:45 +0200

    (MongoTestUtilities) added common class to create objects needed in more than one test class

---
+++ DataLayerTest/MongoDataTest/MongoTestUtilities.cs
@@ -0,0 +1,26 @@
+﻿using DataLayer.MongoData.Option;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayerTest.MongoDataTest
+{
+	public static class MongoTestUtilities
+	{
+		public static Schedule CreateSchedule()
+		{
+			Schedule schedule = new Schedule()
+			{
+				DaysOfMonthToSkip = new List<int> { 1, 2, 3 },
+				DaysOfWeekToSkip = new List<DayOfWeek> { DayOfWeek.Monday, DayOfWeek.Thursday },
+				HoursOfDayToSkip = new List<int>() { 3, 4, 5 },
+				Recurring = true,
+				NextAllowedExecution = DateTime.Now,
+				TimeBetweenAllowedExecutions = TimeSpan.FromMinutes(1),
+			};
+			return schedule;
+		}
+	}
+}
