58037ebf026d346395dc0c98e4cd822b9fc27e99
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-11-04 14:14:49 +0100

    testing utilities

---
+++ UtilitiesTest/GuidConverterTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace UtilitiesTest
+{
+	[TestFixture]
+	public class GuidConverterTest
+	{
+		[TestCase(0, 0, 0, 0, "00000000-0000-0000-0000-000000000000")]
+		[TestCase(2147483647, 0, 0, 0, "7fffffff-0000-0000-0000-000000000000")]
+		[TestCase(0, 2147483647, 0, 0, "00000000-ffff-7fff-0000-000000000000")]
+		[TestCase(0, 0, 2147483647, 0, "00000000-0000-0000-ffff-ff7f00000000")]
+		[TestCase(0, 0, 0, 2147483647, "00000000-0000-0000-0000-0000ffffff7f")]
+		[TestCase(2147483647, 2147483647, 2147483647, 2147483647, "7fffffff-ffff-7fff-ffff-ff7fffffff7f")]
+		public void IntConversion(int source1, int source2, int source3, int source4, string result)
+		{
+			Guid resultGuid = Guid.Parse(result);
+
+			Guid convertedGuid = Utilities.Converter.GuidConverter.Convert(source1, source2, source3, source4);
+
+			Assert.AreEqual(resultGuid, convertedGuid);
+		}
+	}
+}
