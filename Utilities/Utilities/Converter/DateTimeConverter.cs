71a2d83fdf83bab24364418871250c05f534dc45
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-11-04 14:14:29 +0100

    (Utilities) added utilities project

---
+++ Utilities/Utilities/Converter/DateTimeConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Utilities.Converter
+{
+	public static class DateTimeConverter
+	{
+		public static readonly string DateFormatShort = "yyyyMMdd";
+		public static readonly string DateFormatLong = "yyyyMMdd HH:mm:ss";
+
+		public static DateTime DateTimeFromString(string input)
+		{
+			DateTime convertedDateTime;
+			DateTime.TryParseExact(input, DateFormatLong, null, DateTimeStyles.None, out convertedDateTime);
+
+			return convertedDateTime;
+		}
+	}
+}
---
+++ Utilities/Utilities/Converter/GuidConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Utilities.Converter
+{
+	public static class GuidConverter
+	{
+		public static Guid Convert(int source1, int source2, int source3, int source4)
+		{
+			byte[] bytes = new byte[16];
+
+			BitConverter.GetBytes(source1).CopyTo(bytes, 0);
+			BitConverter.GetBytes(source2).CopyTo(bytes, 4);
+			BitConverter.GetBytes(source3).CopyTo(bytes, 8);
+			BitConverter.GetBytes(source4).CopyTo(bytes, 12);
+
+			return new Guid(bytes);
+		}
+	}
+}
