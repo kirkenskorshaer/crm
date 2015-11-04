d45be205d3201b51b641281a2dfe4c7175d05ae2
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-11-04 14:43:10 +0100

    added options to maintain tables

---
+++ Administration/Administration/Option/Options/Data/MaintainTables.cs
@@ -0,0 +1,18 @@
+﻿using DataLayer;
+
+namespace Administration.Option.Options.Data
+{
+	public class MaintainTables : AbstractDataOptionBase
+	{
+		public MaintainTables(MongoConnection connection) : base(connection, null)
+		{
+		}
+
+		protected override bool ExecuteOption()
+		{
+			DataLayer.SqlData.Utilities.MaintainAllTables(SqlConnection);
+
+			return true;
+		}
+	}
+}
---
+++ DataLayer/DataLayer/MongoData/Option/Options/Data/Utilities/MaintainAllTables.cs
@@ -0,0 +1,17 @@
+﻿namespace DataLayer.MongoData.Option.Options.Data.Utilities
+{
+	public class MaintainAllTables : OptionBase
+	{
+		protected override void Execute(MongoConnection connection, bool recurring)
+		{
+			if (recurring)
+			{
+				Update<MaintainAllTables>(connection);
+			}
+			else
+			{
+				Delete<MaintainAllTables>(connection);
+			}
+		}
+	}
+}
