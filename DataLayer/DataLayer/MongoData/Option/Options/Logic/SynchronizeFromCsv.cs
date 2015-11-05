469e124cbea5363557074c3274acb3c9bd415656
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-11-05 08:08:40 +0100

    (SynchronizeFromCsv) added synchronize to database logic option

---
+++ DataLayer/DataLayer/MongoData/Option/Options/Logic/SynchronizeFromCsv.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DataLayer.MongoData.Option.Options.Logic
+{
+	public class SynchronizeFromCsv : OptionBase
+	{
+		public string fileName { get; set; }
+		public string fileNameTmp { get; set; }
+		public char delimeter { get; set; }
+		public string keyName { get; set; }
+		public string dateName { get; set; }
+		public string mappingField { get; set; }
+		public string[] fields { get; set; }
+
+		public Guid changeProviderId { get; set; }
+
+		protected override void Execute(MongoConnection connection, bool recurring)
+		{
+			if (recurring)
+			{
+				Update<SynchronizeFromCsv>(connection);
+			}
+			else
+			{
+				Delete<SynchronizeFromCsv>(connection);
+			}
+		}
+	}
+}
