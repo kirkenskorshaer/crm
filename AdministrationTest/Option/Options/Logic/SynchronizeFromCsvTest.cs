d7558420f9d417a658673573e5d27348bdd43920
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-11-05 08:45:42 +0100

    (SynchronizeFromCsv) synchronize test now creates 2 change providers

--- AdministrationTest/Option/Options/Logic/SynchronizeFromCsvTest.cs
+++ AdministrationTest/Option/Options/Logic/SynchronizeFromCsvTest.cs
@@ -13,7 +13,8 @@ namespace AdministrationTest.Option.Options.Logic
 	[TestFixture]
 	public class SynchronizeFromCsvTest : TestBase
 	{
-		private DatabaseChangeProvider _changeProvider;
+		private DatabaseChangeProvider _changeProvider1;
+		private DatabaseChangeProvider _changeProvider2;
 		private SqlConnection _sqlConnection;
 
 		private string[] fields = new string[] { "id", "collectedDate", "firstName", "test" };
@@ -24,32 +25,39 @@ namespace AdministrationTest.Option.Options.Logic
 		[SetUp]
 		public void SetUp()
 		{
-			string testCsvProvider = "testCsvProvider";
-
 			_sqlConnection = DataLayer.SqlConnectionHolder.GetConnection(Connection, "sql");
 
 			List<DatabaseChangeProvider> changeProviders = DatabaseChangeProvider.ReadAll(_sqlConnection);
 
-			Func<DatabaseChangeProvider, bool> findChangeProvider = changeProvider => changeProvider.Name == testCsvProvider;
+			string testCsvProvider1 = "testCsvProvider1";
+			string testCsvProvider2 = "testCsvProvider2";
 
-			if (changeProviders.Any(findChangeProvider))
-			{
-				_changeProvider = changeProviders.Single(findChangeProvider);
-			}
-			else
-			{
-				_changeProvider = new DatabaseChangeProvider()
-				{
-					Name = testCsvProvider,
-				};
-
-				_changeProvider.Insert(_sqlConnection);
-			}
+			_changeProvider1 = FindOrCreateChangeProvider(testCsvProvider1, changeProviders);
+			_changeProvider2 = FindOrCreateChangeProvider(testCsvProvider2, changeProviders);
 
 			if (File.Exists(fileName))
 			{
 				File.Delete(fileName);
 			}
+		}
+
+		private DatabaseChangeProvider FindOrCreateChangeProvider(string testCsvProvider, List<DatabaseChangeProvider> changeProviders)
+		{
+			Func<DatabaseChangeProvider, bool> findChangeProvider = lChangeProvider => lChangeProvider.Name == testCsvProvider;
+
+			if (changeProviders.Any(findChangeProvider))
+			{
+				return changeProviders.Single(findChangeProvider);
+			}
+
+			DatabaseChangeProvider changeProvider = new DatabaseChangeProvider()
+			{
+				Name = testCsvProvider,
+			};
+
+			changeProvider.Insert(_sqlConnection);
+
+			return changeProvider;
 		}
 
 		[Test]
@@ -62,7 +70,7 @@ namespace AdministrationTest.Option.Options.Logic
 
 			DatabaseSynchronizeFromCsv databaseSynchronizeFromCsv = new DatabaseSynchronizeFromCsv()
 			{
-				changeProviderId = _changeProvider.Id,
+				changeProviderId = _changeProvider1.Id,
 				dateName = "collectedDate",
 				keyName = "id",
 				delimeter = delimeter,
