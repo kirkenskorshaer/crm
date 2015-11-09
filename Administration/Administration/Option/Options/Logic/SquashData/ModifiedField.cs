3714d9556cb5c1eb6a9bf75373ea7595644c02bf
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-11-09 08:45:46 +0100

    (ModifiedField) added mini data holding class defining a field modification

---
+++ Administration/Administration/Option/Options/Logic/SquashData/ModifiedField.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Administration.Option.Options.Logic.SquashData
+{
+	public class ModifiedField
+	{
+		public object Value;
+		public string Name;
+		public DateTime ModifiedOn;
+	}
+}
