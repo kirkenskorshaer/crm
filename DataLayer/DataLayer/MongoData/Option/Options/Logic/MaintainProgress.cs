fe333c8258e6f6ec781c98abb0fd783774554c2a
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-11-09 08:48:36 +0100

    (MaintainProgress) added option to maintain progress

---
+++ DataLayer/DataLayer/MongoData/Option/Options/Logic/MaintainProgress.cs
@@ -0,0 +1,17 @@
+﻿namespace DataLayer.MongoData.Option.Options.Logic
+{
+	public class MaintainProgress : OptionBase
+	{
+		protected override void Execute(MongoConnection connection, bool recurring)
+		{
+			if (recurring)
+			{
+				Update<MaintainProgress>(connection);
+			}
+			else
+			{
+				Delete<MaintainProgress>(connection);
+			}
+		}
+	}
+}
