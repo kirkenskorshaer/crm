42f509f68b0404a7cb6e4ad64b6b79e28898c0fa
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-11-12 08:20:01 +0100

    (MaintainProgress) added logic to maintain progress

---
+++ Administration/Administration/Option/Options/Logic/MaintainProgress.cs
@@ -0,0 +1,67 @@
+﻿using Administration.Option.Options.Data;
+using DataLayer;
+using System;
+using DatabaseOptionBase = DataLayer.MongoData.Option.OptionBase;
+using DatabaseMaintainProgress = DataLayer.MongoData.Option.Options.Logic.MaintainProgress;
+using DatabaseContact = DataLayer.SqlData.Contact.Contact;
+using DatabaseProgress = DataLayer.MongoData.Progress;
+
+namespace Administration.Option.Options.Logic
+{
+	public class MaintainProgress : AbstractDataOptionBase
+	{
+		public const string MaintainProgressContact = "MaintainProgressContact";
+		public const string ProgressContact = "Contact";
+
+		private DatabaseMaintainProgress _databaseMaintainProgress;
+
+		public MaintainProgress(MongoConnection connection, DatabaseOptionBase databaseOption) : base(connection, databaseOption)
+		{
+			_databaseMaintainProgress = (DatabaseMaintainProgress)databaseOption;
+		}
+
+		protected override bool ExecuteOption()
+		{
+			DatabaseProgress progress = DatabaseProgress.ReadNext(Connection, MaintainProgressContact);
+
+			if (progress == null)
+			{
+				progress = new DatabaseProgress()
+				{
+					LastProgressDate = DateTime.Now,
+					TargetId = Guid.Empty,
+					TargetName = MaintainProgressContact,
+				};
+
+				progress.Insert(Connection);
+			}
+
+			Guid currentId = progress.TargetId;
+
+			DatabaseContact contact = DatabaseContact.ReadNextById(SqlConnection, currentId);
+
+			if (contact == null)
+			{
+				Log.Write(Connection, "No contact to maintain", DataLayer.MongoData.Config.LogLevelEnum.OptionMessage);
+				return false;
+			}
+
+			if (DatabaseProgress.Exists(Connection, ProgressContact, contact.Id) == false)
+			{
+				DatabaseProgress newContactProgress = new DatabaseProgress()
+				{
+					LastProgressDate = DateTime.Now,
+					TargetId = contact.Id,
+					TargetName = ProgressContact,
+				};
+
+				newContactProgress.Insert(Connection);
+			}
+
+			progress.TargetId = contact.Id;
+			progress.UpdateAndSetLastProgressDateToNow(Connection);
+
+			return true;
+		}
+	}
+}
