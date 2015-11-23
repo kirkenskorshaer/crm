4c375f2ef871895d38b761d479fefd9972c27c77
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-11-23 10:35:10 +0100

    (ExternalContactTest) testing read methods

--- DataLayerTest/SqlDataTest/ContactTest/ExternalContactTest.cs
+++ DataLayerTest/SqlDataTest/ContactTest/ExternalContactTest.cs
@@ -73,5 +73,45 @@ namespace DataLayerTest.SqlDataTest.ContactTest
 			Assert.AreEqual(externalContactCreated.ChangeProviderId, externalContactRead.ChangeProviderId);
 			Assert.AreEqual(externalContactCreated.ExternalContactId, externalContactRead.ExternalContactId);
 		}
+
+		[Test]
+		public void ReadReadsAListFromChangeProviderId()
+		{
+			ChangeProvider changeProvider = InsertChangeProvider();
+
+			Guid externalContact1Id = Guid.NewGuid();
+			ExternalContact externalContact1Created = new ExternalContact(_sqlConnection, externalContact1Id, changeProvider.Id);
+			externalContact1Created.Insert();
+
+			Guid externalContact2Id = Guid.NewGuid();
+			ExternalContact externalContact2Created = new ExternalContact(_sqlConnection, externalContact2Id, changeProvider.Id);
+			externalContact2Created.Insert();
+
+			List<ExternalContact> externalContactReadList = ExternalContact.Read(_sqlConnection, changeProvider.Id);
+
+			Assert.AreEqual(2, externalContactReadList.Count);
+			Assert.IsTrue(externalContactReadList.Any(externalContact => externalContact.ExternalContactId == externalContact1Id));
+			Assert.IsTrue(externalContactReadList.Any(externalContact => externalContact.ExternalContactId == externalContact2Id));
+		}
+
+		[Test]
+		public void ReadFromChangeProviderAndContact()
+		{
+			ChangeProvider changeProvider = InsertChangeProvider();
+
+			Guid externalContactId = Guid.NewGuid();
+			ExternalContact externalContactCreated = new ExternalContact(_sqlConnection, externalContactId, changeProvider.Id);
+			externalContactCreated.Insert();
+
+			Contact contact = InsertContact(_sqlConnection);
+
+			InsertContactChange(_sqlConnection, contact.Id, externalContactId, changeProvider.Id, DateTime.Now);
+			InsertContactChange(_sqlConnection, contact.Id, externalContactId, changeProvider.Id, DateTime.Now);
+
+			List<ExternalContact> externalContactRead = ExternalContact.ReadFromChangeProviderAndContact(_sqlConnection, changeProvider.Id, contact.Id);
+
+			Assert.AreEqual(externalContactCreated.ChangeProviderId, externalContactRead.Single().ChangeProviderId);
+			Assert.AreEqual(externalContactCreated.ExternalContactId, externalContactRead.Single().ExternalContactId);
+		}
 	}
 }
