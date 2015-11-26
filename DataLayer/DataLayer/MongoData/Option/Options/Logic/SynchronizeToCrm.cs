ad567b110916b345bc7f3ec837d7bf2ce26c726a
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-11-26 14:39:17 +0100

    (SynchronizeToCrm) added data option

---
+++ DataLayer/DataLayer/MongoData/Option/Options/Logic/SynchronizeToCrm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DataLayer.MongoData.Option.Options.Logic
+{
+	public class SynchronizeToCrm : OptionBase
+	{
+		public Guid changeProviderId { get; set; }
+		public string urlLoginName { get; set; }
+
+		protected override void Execute(MongoConnection connection, bool recurring)
+		{
+			if (recurring)
+			{
+				Update<SynchronizeToCrm>(connection);
+			}
+			else
+			{
+				Delete<SynchronizeToCrm>(connection);
+			}
+		}
+	}
+}
