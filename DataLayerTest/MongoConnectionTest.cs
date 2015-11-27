fa6ea6d7e414c255752d25550b58346b434033b5
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-11-27 11:07:32 +0100

    (MongoConnectionTest) testing ReadAsDictionaries

---
+++ DataLayerTest/MongoConnectionTest.cs
@@ -0,0 +1,36 @@
+﻿using DataLayer;
+using DataLayer.MongoData;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayerTest
+{
+	[TestFixture]
+	public class MongoConnectionTest
+	{
+		private MongoConnection _connection;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_connection = MongoConnection.GetConnection("test");
+			_connection.CleanDatabase();
+		}
+
+		[Test]
+		public void ReadAsDictionaries()
+		{
+			string messageValue = "test1";
+
+			Log.Write(_connection, messageValue, string.Empty, Config.LogLevelEnum.OptionMessage);
+
+			List<Dictionary<string, object>> readList = _connection.ReadAsDictionaries(typeof(Log).Name.ToLower());
+
+			Assert.AreEqual(readList.Single()["Message"], messageValue);
+		}
+	}
+}
