cc17c9a43704c40187608bbe56ea28e6045b8c5d
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-11-30 10:31:18 +0100

    adding simple data tabs

--- UserInterface/UserInterface/MainWindow.xaml.cs
+++ UserInterface/UserInterface/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
-﻿using System;
+﻿using DataLayer;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using UserInterface.MongoData;
 
 namespace UserInterface
 {
@@ -23,6 +18,41 @@ namespace UserInterface
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			PopulateTabControlGlobal();
+		}
+
+		private void PopulateTabControlGlobal()
+		{
+			string mongoDataNamespaceName = "DataLayer.MongoData";
+			string optionNamespaceName = "DataLayer.MongoData.Option.Options";
+
+			string databaseName = ConfigurationManager.AppSettings["mongoDatabaseName"];
+			MongoConnection mongoConnection = MongoConnection.GetConnection(databaseName);
+
+			Assembly assembly = Assembly.GetAssembly(typeof(MongoConnection));
+
+			List<Type> allMongoTypes = assembly.GetTypes().Where(type =>
+				type.IsDefined(typeof(CompilerGeneratedAttribute)) == false &&
+				type.IsClass &&
+				type.Namespace.StartsWith(mongoDataNamespaceName)).ToList();
+
+			List<Type> simpleStructureDataTypes = allMongoTypes.Where(type => type.Namespace == mongoDataNamespaceName).ToList();
+			List<Type> optionDataTypes = allMongoTypes.Where(type => type.Namespace.StartsWith(optionNamespaceName)).ToList();
+
+			foreach (Type type in simpleStructureDataTypes)
+			{
+				SimpleDataTabItem tabItem = new SimpleDataTabItem()
+				{
+					Header = type.Name,
+					DataTypeName = type.Name,
+					MongoConnection = mongoConnection,
+				};
+
+				tabItem.RefreshData();
+
+				TabControlGlobal.Items.Add(tabItem);
+			}
 		}
 	}
 }
---
+++ UserInterface/UserInterface/MongoData/SimpleDataTabItem.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace UserInterface.MongoData
+{
+	public class SimpleDataTabItem : TabItem
+	{
+		public string DataTypeName { get; set; }
+		public DataLayer.MongoConnection MongoConnection { get; set; }
+
+		public void RefreshData()
+		{
+			DataGrid dataGrid = new DataGrid();
+			Content = dataGrid;
+
+			List<Dictionary<string, object>> dataDictionaries = MongoConnection.ReadAsDictionaries(DataTypeName);
+			List<string> keys = GetKeys(dataDictionaries);
+			dataGrid.AutoGenerateColumns = false;
+
+			keys.ForEach(key => dataGrid.Columns.Add(new DataGridTextColumn() { Header = key, Binding = new Binding(key) }));
+
+			foreach (Dictionary<string, object> dataDictionary in dataDictionaries)
+			{
+				dynamic row = new ExpandoObject();
+
+				foreach (string key in keys)
+				{
+					TextBlock block = new TextBlock();
+					block.Text = "test";
+
+					if (dataDictionary.ContainsKey(key))
+					{
+						((IDictionary<String, Object>)row)[key] = dataDictionary[key];
+					}
+				}
+				dataGrid.Items.Add(row);
+			}
+		}
+
+		private List<string> GetKeys(List<Dictionary<string, object>> dataDictionaries)
+		{
+			List<string> keys = new List<string>();
+			foreach (Dictionary<string, object> dataDictionary in dataDictionaries)
+			{
+				foreach (string key in dataDictionary.Keys)
+				{
+					if (keys.Contains(key) == false)
+					{
+						keys.Add(key);
+					}
+				}
+			}
+
+			return keys;
+		}
+	}
+}
