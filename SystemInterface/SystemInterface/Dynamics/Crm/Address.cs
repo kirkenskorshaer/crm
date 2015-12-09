aa2656b45d021df0fe739e363ffbd6800653d7ad
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-12-09 14:54:58 +0100

    (Address) adding address crm class

---
+++ SystemInterface/SystemInterface/Dynamics/Crm/Address.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xrm.Client;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SystemInterface.Dynamics.Crm
+{
+	public class Address
+	{
+		public Guid AddressId { get; private set; }
+		public string line1;
+
+		//private static readonly ColumnSet ColumnSetAddress = new ColumnSet("createdby", "createdon", "modifiedby", "modifiedon", "customeraddressid", "ownerid", "owningbusinessunit", "owninguser", "statecode", "statuscode");
+		private static readonly ColumnSet ColumnSetAddress = new ColumnSet("customeraddressid", "line1");
+
+		private static Address EntityToAddress(Entity entity)
+		{
+			return new Address
+			{
+				AddressId = (Guid)entity.Attributes["customeraddressid"],
+				line1 = entity.Attributes["line1"].ToString(),
+			};
+		}
+
+		private CrmEntity GetAddressAsEntity(bool includeId)
+		{
+			CrmEntity crmEntity = new CrmEntity("customeraddress");
+			crmEntity.Attributes.Add(new KeyValuePair<string, object>("line1", line1));
+
+			if (includeId)
+			{
+				crmEntity.Attributes.Add(new KeyValuePair<string, object>("customeraddressid", AddressId));
+			}
+
+			return crmEntity;
+		}
+
+		public static Address Read(DynamicsCrmConnection connection, Guid addressid)
+		{
+			Entity contactEntity = connection.Service.Retrieve("customeraddress", addressid, ColumnSetAddress);
+
+			Address address = EntityToAddress(contactEntity);
+
+			return address;
+		}
+
+		public void Update(DynamicsCrmConnection connection)
+		{
+			CrmEntity crmEntity = GetAddressAsEntity(true);
+
+			connection.Service.Update(crmEntity);
+		}
+
+		public static List<string> GetAllAttributeNames(DynamicsCrmConnection connection, Guid addressId)
+		{
+			List<string> attributeNames = new List<string>();
+
+			ColumnSet columnsAll = new ColumnSet(true);
+
+			Entity entity = connection.Service.Retrieve("customeraddress", addressId, columnsAll);
+
+			attributeNames = entity.Attributes.Select(attribute => $"{attribute.Value.GetType().Name} {attribute.Key}").ToList();
+
+			return attributeNames;
+		}
+
+	}
+}
