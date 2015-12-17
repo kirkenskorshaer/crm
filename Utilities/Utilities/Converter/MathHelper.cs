9bfb69149e41e6b2e0d9f0769e9295f6de89b22a
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-12-17 14:40:16 +0100

    (MathHelper) added mathHelper to Utilities

---
+++ Utilities/Utilities/Converter/MathHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Utilities.Converter
+{
+	public static class MathHelper
+	{
+		public static int DigitSum(int number)
+		{
+			IEnumerable<int> numbers = number.ToString().Select(digit => int.Parse(digit.ToString()));
+
+			int sum = 0;
+			foreach(int digit in numbers)
+			{
+				sum += digit;
+			}
+
+			if(sum < 10)
+			{
+				return sum;
+			}
+
+			return DigitSum(sum);
+		}
+	}
+}
