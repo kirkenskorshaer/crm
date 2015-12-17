fa9441b5d7b89efbbccea727d74291dde744077c
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-12-17 14:44:34 +0100

    (StressTestCrmTest) testing stressTestCrm

---
+++ AdministrationTest/Option/Options/Logic/StressTestCrmTest.cs
@@ -0,0 +1,42 @@
+﻿using Administration.Option.Options.Logic;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatabaseStressTestCrm = DataLayer.MongoData.Option.Options.Logic.StressTestCrm;
+using DatabaseStringIntStatistics = DataLayer.MongoData.Statistics.StringStatistics.StringIntStatistics;
+
+namespace AdministrationTest.Option.Options.Logic
+{
+	[TestFixture]
+	public class StressTestCrmTest : TestBase
+	{
+		[Test]
+		public void StressTestCrmCreatesTheCroeecteNumberOfStatisticsEntries()
+		{
+			DatabaseStressTestCrm databaseStressTestCrm = GetDatabaseStressTestCrm();
+
+			StressTestCrm stressTestCrm = new StressTestCrm(Connection, databaseStressTestCrm);
+
+			stressTestCrm.Execute();
+			stressTestCrm.Execute();
+
+			List<DatabaseStringIntStatistics> statisticsList = DatabaseStringIntStatistics.ReadByName(Connection, "StressTestCrm");
+
+			Assert.AreEqual(2, statisticsList.Count);
+		}
+
+		private DatabaseStressTestCrm GetDatabaseStressTestCrm()
+		{
+			return new DatabaseStressTestCrm()
+			{
+				contactsToCreate = 10,
+				Name = "StressTestCrm",
+				Schedule = CreateScheduleAlwaysOnDoOnce(),
+				urlLoginName = "test",
+			};
+		}
+	}
+}
