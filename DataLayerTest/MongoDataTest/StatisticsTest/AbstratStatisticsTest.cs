7a9193ab30ccf3e74456d2f732a5ca35efe3e9ca
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-12-17 08:18:38 +0100

    testing statistics

---
+++ DataLayerTest/MongoDataTest/StatisticsTest/AbstratStatisticsTest.cs
@@ -0,0 +1,23 @@
+﻿using DataLayer;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayerTest.MongoDataTest.StatisticsTest
+{
+	[TestFixture]
+	public abstract class AbstratStatisticsTest
+	{
+		protected MongoConnection Connection;
+
+		[SetUp]
+		public void SetUp()
+		{
+			Connection = MongoConnection.GetConnection("test");
+			Connection.CleanDatabase();
+		}
+	}
+}
