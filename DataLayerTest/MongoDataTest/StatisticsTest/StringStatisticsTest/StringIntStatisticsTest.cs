9d96f5cf15cbe140f4fda809ee3a9d1733cd9011
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-12-17 08:18:38 +0100

    testing statistics

---
+++ DataLayerTest/MongoDataTest/StatisticsTest/StringStatisticsTest/StringIntStatisticsTest.cs
@@ -0,0 +1,22 @@
+﻿using DataLayer.MongoData.Statistics.StringStatistics;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataLayerTest.MongoDataTest.StatisticsTest.StringStatisticsTest
+{
+	[TestFixture]
+	public class StringIntStatisticsTest : AbstratStatisticsTest
+	{
+		[Test]
+		public void DataWrittenCanBeRead()
+		{
+			StringIntStatistics stringIntStatisticsCreated = StringIntStatistics.Create(Connection, "test", "_100", 10);
+
+			List<StringIntStatistics> stringIntStatisticsRead = StringIntStatistics.Read(Connection, stringIntStatisticsCreated.Id);
+
+			Assert.AreEqual(stringIntStatisticsCreated.XValue, stringIntStatisticsRead.Single().XValue);
+			Assert.AreEqual(stringIntStatisticsCreated.YValue, stringIntStatisticsRead.Single().YValue);
+		}
+	}
+}
