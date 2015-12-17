385888509ad11009215f700e46f684d320455676
kkkSvend <svend.l@kirkenskorshaer.dk>
2015-12-17 14:41:52 +0100

    (StressTestCrm) added mongoData for StressTestCrm

---
+++ DataLayer/DataLayer/MongoData/Option/Options/Logic/StressTestCrm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.MongoData.Option.Options.Logic
+{
+	public class StressTestCrm : OptionBase
+	{
+		public string urlLoginName { get; set; }
+		public int contactsToCreate { get; set; }
+
+		protected override void Execute(MongoConnection connection, bool recurring)
+		{
+			if (recurring)
+			{
+				Update<StressTestCrm>(connection);
+			}
+			else
+			{
+				Delete<StressTestCrm>(connection);
+			}
+		}
+	}
+}
