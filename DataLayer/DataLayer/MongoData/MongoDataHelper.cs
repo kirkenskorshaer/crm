d749752fb6875fc3e6ef3ef37b59784d48e14f6a
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-01-07 13:53:50 +0100

    (MongoDataHelper) added helperclass to throw an exception if a mongo data task times out

---
+++ DataLayer/DataLayer/MongoData/MongoDataHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DataLayer.MongoData
+{
+	public static class MongoDataHelper
+	{
+		internal static SearchedObjectType GetValueOrThrowTimeout<SearchedObjectType>(Task<SearchedObjectType> task)
+		{
+			int timeoutMilliSeconds = MongoConnection.TimeoutMilliSeconds;
+
+			task.Wait(timeoutMilliSeconds);
+
+			if (task.IsCompleted == false)
+			{
+				throw new TimeoutException($"timeout after waiting {timeoutMilliSeconds} seconds to fetch {typeof(SearchedObjectType).Name}");
+			}
+
+			return task.Result;
+		}
+	}
+}
