df90d2340a1543666aab8b1162ca0784316659d2
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-01-07 13:36:56 +0100

    (Log) messages are no longer written to the log if an identical message was recently written

--- Administration/Administration/Log.cs
+++ Administration/Administration/Log.cs
@@ -1,11 +1,14 @@
 ﻿using DataLayer;
 using DataLayer.MongoData;
+using System.Collections;
 
 namespace Administration
 {
 	public static class Log
 	{
 		public static Config.LogLevelEnum LogLevel = Config.LogLevelEnum.HeartError | Config.LogLevelEnum.OptionError;
+		private static Queue _logCache = new Queue();
+		public static int MaxLookbackCacheSize = 20;
 
 		public static void Write(MongoConnection connection, string message, Config.LogLevelEnum logLevel)
 		{
@@ -16,8 +19,34 @@ namespace Administration
 		{
 			if (LogLevel.HasFlag(logLevel))
 			{
-				DataLayer.MongoData.Log.Write(connection, message, stackTrace, logLevel);
+				if (IsRepeatedError(message, stackTrace) == false)
+				{
+					DataLayer.MongoData.Log.Write(connection, message, stackTrace, logLevel);
+				}
 			}
 		}
+
+		private static bool IsRepeatedError(string message, string stackTrace)
+		{
+			string messageAndStackTrace = message;
+			if(string.IsNullOrWhiteSpace(stackTrace) == false)
+			{
+				messageAndStackTrace += stackTrace;
+			}
+
+			if(_logCache.Contains(messageAndStackTrace))
+			{
+				return true;
+			}
+
+			if (_logCache.Count >= MaxLookbackCacheSize)
+			{
+				_logCache.Dequeue();
+			}
+
+			_logCache.Enqueue(messageAndStackTrace);
+
+			return false;
+        }
 	}
 }
