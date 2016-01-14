9a5d2a8308eae0228c9eb27545c05ceaf4fc62c1
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-01-14 15:36:59 +0100

    Refactor StressTestCrm random data is now generated from several different classes, Accounts are now included

---
+++ Administration/Administration/Mapping/RandomData/RandomAccount.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using SystemInterfaceAccount = SystemInterface.Dynamics.Crm.Account;
+
+namespace Administration.Mapping.RandomData
+{
+	public class RandomAccount : AbstractRandom
+	{
+		private string[] _domains;
+		private string[] _topDomains;
+		private string[] _groupNames;
+		private string[] _lastnames;
+		private string[] _firstnames;
+		private Dictionary<string, string> _postalCodes;
+
+		private RandomAccount()
+		{
+			_groupNames = new string[]
+			{
+				"Gave",
+				"MobilePay",
+				"Gaver Mariatjenesten",
+				"Gaver Nicolai Tjenesten",
+				"Gaver Københavnsarbejdet",
+				"Gaver /Årsmærker",
+				"Gaver/Korshærsbladet",
+				"Juleindsamling Korshæerbladet",
+				"Gave Takkebrev ønskes ikke",
+				"Fonds og Legater",
+				"Online-donation",
+				"Gavebrev",
+				"Gavebreve, procentdel",
+				"Gavebreve, andre byer",
+				"Høstoffer",
+				"Kollekter",
+				"Støtten husstandsom. Nykøbing",
+				"Støtten",
+				"Støtten Folkeskolebladet",
+				"Støtten Husstandsomdelt",
+				"Støtten Takkebrev ønskes ikke",
+				"Folkeskolen/Husstand, anonym",
+				"Støtten, nye ved udsend feb.98",
+				"Årsmærker uden beregning",
+				"Årsmærker",
+				"Årsmærker med gave",
+				"Jubilæumsbidrag 100 års jubilæum",
+				"Jubilæumsgave KD",
+				"Korshærsbl. Hovedkontor betal.",
+				"Korshærsbladet Hovedkontoret",
+				"Betales af Viborg",
+				"Arbejdsskadeforsikring",
+				"Korshærsbladet betalt 2002",
+				"Korshærsbladet betalt",
+				"Friab. Korshærsbladet",
+				"Korshærsbladet betalt 2001",
+				"Prøveabonem. Korshærsbladet",
+				"Betaler for en anden",
+				"Betales af butik Vejle",
+				"Betales af en anden",
+				"Betales af Ansgar Aalborg",
+				"Korshærsbladet betalt Pensionist",
+				"Julegave Korshærsblad 2004",
+				"Betalt til arbejdsgrene",
+				"Lodseddelsalg",
+				"listeindsamling",
+				"Kredsgave",
+				"Basarer og fester",
+				"møder og gudstjenester",
+				"Butikker",
+				"Diverse indbetal. og udgifter",
+				"Kredssalg Årsmøde",
+				"Retreat Haslev Udv. Højskole",
+				"Lodsedler til storkredse",
+				"Lodsedler til arbejdsgrene",
+				"Lodsedler diverse",
+				"Lodsedler til indenbys arb.",
+				"Portoudgifter Storkredse",
+				"Lodsedler Arbejdsgrenes andel",
+				"Lodseddeldiff",
+				"Ønsker ingen takkeskrivelse",
+				"Andet (Lilian)",
+				"Støttemedlemsskab",
+				"Støttemedlemsskab/Husstand",
+				"Støttemedlemsskab uden blade",
+				"Støttemedlemsskab/Husstand uden blade",
+				"Girokonto 540-1429",
+				"Girokonto 308-5635",
+				"Kursusbetalinger",
+				"Betaling Årsmøde",
+			};
+		}
+
+		private static RandomAccount Instance;
+		public static RandomAccount GetInstance()
+		{
+			if (Instance == null)
+			{
+				Instance = new RandomAccount();
+			}
+
+			return Instance;
+		}
+
+		public void AnonymizeAccount(SystemInterfaceAccount account, int? seed = null)
+		{
+			Random random;
+			if (seed != null)
+			{
+				random = new Random(seed.Value);
+			}
+			else
+			{
+				random = new Random();
+			}
+
+			RandomAddress randomAddress = RandomAddress.GetInstance();
+			RandomEmail randomEmail = RandomEmail.GetInstance();
+
+			account.address1_postalcode = randomAddress.GetPostalCode(random);
+			account.address1_city = randomAddress.GetCity(account.address1_postalcode);
+			account.address1_line1 = GetRandomString(random);
+			account.address1_line2 = GetRandomString(random);
+
+			account.name = GetRandomString(random);
+
+			account.emailaddress1 = randomEmail.GetRandomEmail(random, account.name);
+			account.telephone1 = GetRandomPhone(random);
+		}
+	}
+}
--- Administration/Administration/Option/Options/Logic/StressTestCrm.cs
+++ Administration/Administration/Option/Options/Logic/StressTestCrm.cs
@@ -5,11 +5,12 @@ using System.Collections.Generic;
 using DatabaseStressTestCrm = DataLayer.MongoData.Option.Options.Logic.StressTestCrm;
 using DatabaseOptionBase = DataLayer.MongoData.Option.OptionBase;
 using SystemInterfaceContact = SystemInterface.Dynamics.Crm.Contact;
+using SystemInterfaceAccount = SystemInterface.Dynamics.Crm.Account;
 using DatabaseUrlLogin = DataLayer.MongoData.UrlLogin;
 using SystemInterface.Dynamics.Crm;
-using Administration.Mapping.Contact;
 using DataLayer.MongoData.Statistics.StringStatistics;
 using System.Linq;
+using Administration.Mapping.RandomData;
 
 namespace Administration.Option.Options.Logic
 {
@@ -31,13 +32,41 @@ namespace Administration.Option.Options.Logic
 		{
 			SystemInterfaceContact contact = new SystemInterfaceContact(_dynamicsCrmConnection);
 
-			ContactAnonymousMapping.GetInstance().AnonymizeContact(contact);
+			RandomContact.GetInstance().AnonymizeContact(contact);
 
 			contact.Groups = contact.Groups.Select(group => Group.ReadOrCreate(_dynamicsCrmConnection, group.Name)).ToList();
 
 			contact.Insert();
 
 			return contact;
+		}
+
+		public void AddAccountRelatedContacts(SystemInterfaceAccount account, List<SystemInterfaceContact> contacts)
+		{
+			Random random = new Random();
+
+			List<SystemInterfaceContact> indsamlere = contacts.Where(contact => RandomAccount.GetInstance().GetRandomBool(random, 25)).ToList();
+			List<SystemInterfaceContact> related = contacts.Where(contact => RandomAccount.GetInstance().GetRandomBool(random, 25)).ToList();
+
+			account.SynchronizeIndsamlere(indsamlere);
+			account.SynchronizeContacts(related);
+		}
+
+		public SystemInterfaceAccount CreateAccount(List<SystemInterfaceContact> contacts)
+		{
+			SystemInterfaceAccount account = new SystemInterfaceAccount(_dynamicsCrmConnection);
+
+			RandomAccount.GetInstance().AnonymizeAccount(account);
+
+			account.Insert();
+
+			List<Group> groups = RandomGroup.GetInstance().GetRandomGroups();
+			groups = groups.Select(group => Group.ReadOrCreate(_dynamicsCrmConnection, group.Name)).ToList();
+			account.SynchronizeGroups(groups);
+
+			AddAccountRelatedContacts(account, contacts);
+
+			return account;
 		}
 
 		public static List<StressTestCrm> Find(MongoConnection connection)
@@ -50,23 +79,46 @@ namespace Administration.Option.Options.Logic
 		protected override bool ExecuteOption()
 		{
 			int contactsToCreate = _databaseStressTestCrm.contactsToCreate;
+			int accountsToCreate = _databaseStressTestCrm.accountsToCreate;
 			DataLayer.MongoData.IntProgress intProgress = GetIntProgress();
 
 			if (intProgress.progressValue == 0)
 			{
-				DeleteAllContacts();
+				bool areContactsDeleted = false;
+				while (areContactsDeleted == false)
+				{
+					areContactsDeleted = DeleteAllContacts(contactsToCreate);
+				}
+
+				bool areAccountsDeleted = false;
+				while (areAccountsDeleted == false)
+				{
+					areAccountsDeleted = DeleteAllAccounts(accountsToCreate);
+				}
 			}
 
-			DateTime BeforeContacts = DateTime.Now;
+			DateTime BeforeInsert = DateTime.Now;
+
+			List<SystemInterfaceContact> contactsCreated = new List<SystemInterfaceContact>();
 
 			for (int contactsIndex = 0; contactsIndex < contactsToCreate; contactsIndex++)
 			{
-				CreateContact();
+				SystemInterfaceContact contact = CreateContact();
+
+				contactsCreated.Add(contact);
 			}
 
-			DateTime AfterContacts = DateTime.Now;
+			for (int accountIndex = 0; accountIndex < accountsToCreate; accountIndex++)
+			{
+				CreateAccount(contactsCreated);
+			}
 
-			int milliSecondsForEachContact = (int)((AfterContacts - BeforeContacts).TotalMilliseconds / contactsToCreate);
+			DateTime AfterInsert = DateTime.Now;
+
+			int totalMilliseconds = (int)((AfterInsert - BeforeInsert).TotalMilliseconds);
+			int milliSecondsForEachContact = totalMilliseconds / (contactsToCreate + accountsToCreate);
+
+			Log.Write(Connection, $"inserted {contactsToCreate} contacts and {accountsToCreate} accounts in {totalMilliseconds} milli seconds", DataLayer.MongoData.Config.LogLevelEnum.OptionMessage);
 
 			int step = 1000;
 			int min = (intProgress.progressValue / step) * step;
@@ -105,9 +157,9 @@ namespace Administration.Option.Options.Logic
 			return intProgress;
 		}
 
-		private void DeleteAllContacts()
+		private bool DeleteAllContacts(int maxNumberOfContactsToDelete)
 		{
-			List<SystemInterfaceContact> contacts = SystemInterfaceContact.ReadLatest(_dynamicsCrmConnection, DateTime.MinValue);
+			List<SystemInterfaceContact> contacts = SystemInterfaceContact.ReadLatest(_dynamicsCrmConnection, DateTime.MinValue, maxNumberOfContactsToDelete);
 
 			Log.Write(Connection, $"starting to delete {contacts.Count} contacts", DataLayer.MongoData.Config.LogLevelEnum.OptionMessage);
 
@@ -116,7 +168,35 @@ namespace Administration.Option.Options.Logic
 				contact.Delete();
 			}
 
-			Log.Write(Connection, "contacts deleted", DataLayer.MongoData.Config.LogLevelEnum.OptionMessage);
+			Log.Write(Connection, $"{contacts.Count} contacts deleted", DataLayer.MongoData.Config.LogLevelEnum.OptionMessage);
+
+			if (maxNumberOfContactsToDelete == contacts.Count)
+			{
+				return false;
+			}
+
+			return true;
+		}
+
+		private bool DeleteAllAccounts(int maxNumberOfAccountsToDelete)
+		{
+			List<SystemInterfaceAccount> accounts = SystemInterfaceAccount.ReadLatest(_dynamicsCrmConnection, DateTime.MinValue, maxNumberOfAccountsToDelete);
+
+			Log.Write(Connection, $"starting to delete {accounts.Count} accounts", DataLayer.MongoData.Config.LogLevelEnum.OptionMessage);
+
+			foreach (SystemInterfaceAccount account in accounts)
+			{
+				account.Delete();
+			}
+
+			Log.Write(Connection, $"{accounts.Count} accounts deleted", DataLayer.MongoData.Config.LogLevelEnum.OptionMessage);
+
+			if (maxNumberOfAccountsToDelete == accounts.Count)
+			{
+				return false;
+			}
+
+			return true;
 		}
 	}
 }
