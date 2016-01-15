0411860c866831808c36b483827cc4a0c4b4a48f
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-01-15 14:07:08 +0100

    added Account Group relations

---
+++ DataLayer/DataLayer/SqlData/Group/AccountChangeGroup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace DataLayer.SqlData.Group
+{
+	public class AccountChangeGroup : AbstractData
+	{
+		private Guid _accountChangeId;
+		private Guid _groupId;
+
+		public AccountChangeGroup(Guid AccountChangeId, Guid GroupId)
+		{
+			_accountChangeId = AccountChangeId;
+			_groupId = GroupId;
+		}
+
+		public static void MaintainTable(SqlConnection sqlConnection)
+		{
+			string tableName = typeof(AccountChangeGroup).Name;
+
+			List<string> columnsInDatabase = Utilities.GetExistingColumns(sqlConnection, tableName);
+
+			if (columnsInDatabase.Any() == false)
+			{
+				Utilities.CreateCompositeTable2Tables(sqlConnection, tableName, "AccountChangeId", "GroupId");
+			}
+
+			CreateKeyIfMissing(sqlConnection, tableName, "AccountChangeId", typeof(Account.AccountChange).Name, "id");
+			CreateKeyIfMissing(sqlConnection, tableName, "GroupId", typeof(Group).Name, "id");
+		}
+
+		public void Insert(SqlConnection sqlConnection)
+		{
+			StringBuilder sqlStringBuilderColumns = new StringBuilder();
+			StringBuilder sqlStringBuilderParameters = new StringBuilder();
+			List<KeyValuePair<string, object>> parameters = new List<KeyValuePair<string, object>>();
+			AddInsertParameterIfNotNull(_accountChangeId, "AccountChangeId", sqlStringBuilderColumns, sqlStringBuilderParameters, parameters);
+			AddInsertParameterIfNotNull(_groupId, "GroupId", sqlStringBuilderColumns, sqlStringBuilderParameters, parameters);
+
+			StringBuilder sqlStringBuilder = new StringBuilder();
+			sqlStringBuilder.AppendLine("INSERT INTO");
+			sqlStringBuilder.AppendLine("	" + TableName);
+			sqlStringBuilder.AppendLine("(");
+			sqlStringBuilder.Append(sqlStringBuilderColumns);
+			sqlStringBuilder.AppendLine(")");
+			sqlStringBuilder.AppendLine("VALUES");
+			sqlStringBuilder.AppendLine("(");
+			sqlStringBuilder.Append(sqlStringBuilderParameters);
+			sqlStringBuilder.AppendLine(")");
+
+			Utilities.ExecuteNonQuery(sqlConnection, sqlStringBuilder, CommandType.Text, parameters.ToArray());
+		}
+
+		public void Delete(SqlConnection sqlConnection)
+		{
+			StringBuilder sqlStringBuilder = new StringBuilder();
+			sqlStringBuilder.AppendLine("DELETE FROM");
+			sqlStringBuilder.AppendLine("	" + TableName);
+			sqlStringBuilder.AppendLine("WHERE");
+			sqlStringBuilder.AppendLine("	AccountChangeId = @accountChangeId");
+			sqlStringBuilder.AppendLine("	AND");
+			sqlStringBuilder.AppendLine("	GroupId = @groupId");
+
+			Utilities.ExecuteNonQuery(sqlConnection, sqlStringBuilder, CommandType.Text,
+				new KeyValuePair<string, object>("accountChangeId", _accountChangeId),
+				new KeyValuePair<string, object>("groupId", _groupId));
+		}
+	}
+}
---
+++ DataLayer/DataLayer/SqlData/Group/AccountGroup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace DataLayer.SqlData.Group
+{
+	public class AccountGroup : AbstractData
+	{
+		private Guid _accountId;
+		private Guid _groupId;
+
+		public AccountGroup(Guid AccountId, Guid GroupId)
+		{
+			_accountId = AccountId;
+			_groupId = GroupId;
+		}
+
+		public static void MaintainTable(SqlConnection sqlConnection)
+		{
+			string tableName = typeof(AccountGroup).Name;
+
+			List<string> columnsInDatabase = Utilities.GetExistingColumns(sqlConnection, tableName);
+
+			if (columnsInDatabase.Any() == false)
+			{
+				Utilities.CreateCompositeTable2Tables(sqlConnection, tableName, "AccountId", "GroupId");
+			}
+
+			CreateKeyIfMissing(sqlConnection, tableName, "AccountId", typeof(Account.Account).Name, "id");
+			CreateKeyIfMissing(sqlConnection, tableName, "GroupId", typeof(Group).Name, "id");
+		}
+
+		public void Insert(SqlConnection sqlConnection)
+		{
+			StringBuilder sqlStringBuilderColumns = new StringBuilder();
+			StringBuilder sqlStringBuilderParameters = new StringBuilder();
+			List<KeyValuePair<string, object>> parameters = new List<KeyValuePair<string, object>>();
+			AddInsertParameterIfNotNull(_accountId, "AccountId", sqlStringBuilderColumns, sqlStringBuilderParameters, parameters);
+			AddInsertParameterIfNotNull(_groupId, "GroupId", sqlStringBuilderColumns, sqlStringBuilderParameters, parameters);
+
+			StringBuilder sqlStringBuilder = new StringBuilder();
+			sqlStringBuilder.AppendLine("INSERT INTO");
+			sqlStringBuilder.AppendLine($"	[{TableName}]");
+			sqlStringBuilder.AppendLine("(");
+			sqlStringBuilder.Append(sqlStringBuilderColumns);
+			sqlStringBuilder.AppendLine(")");
+			sqlStringBuilder.AppendLine("VALUES");
+			sqlStringBuilder.AppendLine("(");
+			sqlStringBuilder.Append(sqlStringBuilderParameters);
+			sqlStringBuilder.AppendLine(")");
+
+			Utilities.ExecuteNonQuery(sqlConnection, sqlStringBuilder, CommandType.Text, parameters.ToArray());
+		}
+
+		public void Delete(SqlConnection sqlConnection)
+		{
+			StringBuilder sqlStringBuilder = new StringBuilder();
+			sqlStringBuilder.AppendLine("DELETE FROM");
+			sqlStringBuilder.AppendLine($"	[{TableName}]");
+			sqlStringBuilder.AppendLine("WHERE");
+			sqlStringBuilder.AppendLine("	AccountId = @AccountId");
+			sqlStringBuilder.AppendLine("	AND");
+			sqlStringBuilder.AppendLine("	GroupId = @groupId");
+
+			Utilities.ExecuteNonQuery(sqlConnection, sqlStringBuilder, CommandType.Text,
+				new KeyValuePair<string, object>("AccountId", _accountId),
+				new KeyValuePair<string, object>("groupId", _groupId));
+		}
+	}
+}
