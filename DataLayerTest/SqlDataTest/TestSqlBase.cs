d4195b8cd501cf5b25835995af4c75f08b3135db
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-01-15 13:00:27 +0100

    (TestSqlBase) added InsertAccount and InsertAccountChange

--- DataLayerTest/SqlDataTest/TestSqlBase.cs
+++ DataLayerTest/SqlDataTest/TestSqlBase.cs
@@ -1,4 +1,5 @@
-﻿using DataLayer.SqlData.Contact;
+﻿using DataLayer.SqlData.Account;
+using DataLayer.SqlData.Contact;
 using System;
 using System.Data.SqlClient;
 
@@ -23,6 +24,22 @@ namespace DataLayerTest.SqlDataTest
 			return createdContact;
 		}
 
+		protected Account InsertAccount(SqlConnection sqlConnection)
+		{
+			DateTime creationDate = DateTime.Now;
+
+			Account createdContact = new Account
+			{
+				name = $"nameTest_{Guid.NewGuid()}",
+				ModifiedOn = creationDate,
+				CreatedOn = creationDate,
+			};
+
+			createdContact.Insert(sqlConnection);
+
+			return createdContact;
+		}
+
 		protected ContactChange InsertContactChange(SqlConnection sqlConnection, Guid contactId, Guid externalContactId, Guid changeProviderId, DateTime createdTime)
 		{
 			ContactChange contactChangeCreated = new ContactChange(sqlConnection, contactId, externalContactId, changeProviderId)
@@ -35,6 +52,20 @@ namespace DataLayerTest.SqlDataTest
 			contactChangeCreated.Insert();
 
 			return contactChangeCreated;
+		}
+
+		protected AccountChange InsertAccountChange(SqlConnection sqlConnection, Guid accountId, Guid externalAccountId, Guid changeProviderId, DateTime createdTime)
+		{
+			AccountChange accountChangeCreated = new AccountChange(sqlConnection, accountId, externalAccountId, changeProviderId)
+			{
+				name = $"name_{Guid.NewGuid()}",
+				CreatedOn = createdTime,
+				ModifiedOn = createdTime,
+			};
+
+			accountChangeCreated.Insert();
+
+			return accountChangeCreated;
 		}
 	}
 }
