5449625de46beb30557cc288f61620126e742cf1
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-01-21 14:46:08 +0100

    (Account) added Account conversion

---
+++ Administration/Administration/Conversion/Account.cs
@@ -0,0 +1,25 @@
+﻿using SystemInterfaceAccount = SystemInterface.Dynamics.Crm.Account;
+using DatabaseAccount = DataLayer.SqlData.Account.Account;
+using SystemInterface.Dynamics.Crm;
+
+namespace Administration.Conversion
+{
+	public static class Account
+	{
+		public static SystemInterfaceAccount Convert(DynamicsCrmConnection dynamicsCrmConnection, DatabaseAccount fromAccount)
+		{
+			SystemInterfaceAccount toAccount = new SystemInterfaceAccount(dynamicsCrmConnection);
+			return Convert(dynamicsCrmConnection, fromAccount, toAccount);
+		}
+
+		public static SystemInterfaceAccount Convert(DynamicsCrmConnection dynamicsCrmConnection, DatabaseAccount fromAccount, SystemInterfaceAccount toAccount)
+		{
+			SystemInterfaceAccount systemInterfaceAccount = new SystemInterfaceAccount(dynamicsCrmConnection)
+			{
+				name = fromAccount.name,
+			};
+
+			return systemInterfaceAccount;
+		}
+    }
+}
