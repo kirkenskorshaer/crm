8334fccdc3ca87dce5e71bd97dce58f0e0da9532
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-01-21 13:55:40 +0100

    (ModifiedReference) added modifiedReference

---
+++ Administration/Administration/Option/Options/Logic/SquashData/ModifiedReference.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Administration.Option.Options.Logic.SquashData
+{
+	public class ModifiedReference
+	{
+		public List<Guid> RelationIdsAdded;
+		public List<Guid> RelationIdsRemoved;
+		public Type RelationType;
+		public DateTime ModifiedOn;
+	}
+}
