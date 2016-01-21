9f3a1677651347bf855ccb3a20bf8d368fc905d5
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-01-21 14:06:56 +0100

    (ReferenceGetAndSet) adding simple holderclass

---
+++ Administration/Administration/Option/Options/Logic/SquashData/ReferenceGetAndSet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Administration.Option.Options.Logic.SquashData
+{
+	public class ReferenceGetAndSet
+	{
+		public Func<Guid, List<Guid>> GetReferences;
+		public Action<List<Guid>> SetReferences;
+	}
+}
