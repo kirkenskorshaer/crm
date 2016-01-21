a6e6b7ce3510aa50f809d239a420ff80969424a2
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-01-21 13:39:44 +0100

    (DatabaseArrangeResponse) added test helper class

---
+++ DataLayerTest/SqlDataTest/DatabaseArrangeResponse.cs
@@ -0,0 +1,23 @@
+﻿using DataLayer.SqlData;
+using DataLayer.SqlData.Account;
+using DataLayer.SqlData.Contact;
+using DataLayer.SqlData.Group;
+
+namespace DataLayerTest.SqlDataTest
+{
+	internal class DatabaseArrangeResponse
+	{
+		internal AccountChangeIndsamler AccountChangeIndsamler;
+		internal AccountChangeContact AccountChangeContact;
+		internal Account Account;
+		internal Contact Contact;
+		internal ExternalAccount ExternalAccount;
+		internal AccountChange AccountChange;
+		internal AccountChangeGroup AccountChangeGroup;
+		internal ChangeProvider ChangeProvider;
+		internal ExternalContact ExternalContact;
+		internal ContactChange ContactChange;
+		internal Group Group;
+		internal ContactChangeGroup ContactChangeGroup;
+	}
+}
