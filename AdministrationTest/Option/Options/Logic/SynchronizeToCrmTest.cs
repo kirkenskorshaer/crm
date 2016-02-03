720ce8b654e22b5edf7cc3637400e1587cde2cbb
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-02-03 11:07:51 +0100

    (SynchronizeToCrmTest) testing synchronize relations

--- AdministrationTest/Option/Options/Logic/SynchronizeToCrmTest.cs
+++ AdministrationTest/Option/Options/Logic/SynchronizeToCrmTest.cs
@@ -11,6 +11,12 @@ using DatabaseContactChange = DataLayer.SqlData.Contact.ContactChange;
 using DatabaseAccountChange = DataLayer.SqlData.Account.AccountChange;
 using DatabaseExternalContact = DataLayer.SqlData.Contact.ExternalContact;
 using DatabaseExternalAccount = DataLayer.SqlData.Account.ExternalAccount;
+using DatabaseGroup = DataLayer.SqlData.Group.Group;
+using DatabaseContactGroup = DataLayer.SqlData.Group.ContactGroup;
+using DatabaseAccountGroup = DataLayer.SqlData.Group.AccountGroup;
+using DatabaseAccountContact = DataLayer.SqlData.Account.AccountContact;
+using DatabaseAccountIndsamler = DataLayer.SqlData.Account.AccountIndsamler;
+using System.Collections.Generic;
 
 namespace AdministrationTest.Option.Options.Logic
 {
@@ -141,6 +147,178 @@ namespace AdministrationTest.Option.Options.Logic
 			account.Delete();
 
 			Assert.AreEqual(accountChange.name, accountRead.name);
+		}
+
+		[Test]
+		public void GroupsWillBeSynchronizedOnContact()
+		{
+			DatabaseSynchronizeToCrm databaseSynchronizeFromCrm = GetDatabaseSynchronizeToCrm();
+			SynchronizeToCrm synchronizeToCrm = new SynchronizeToCrm(Connection, databaseSynchronizeFromCrm);
+
+			DatabaseContact databaseContact = CreateContact();
+			AddGroupsToDatabaseContact(databaseContact, "group1", "group2");
+
+			MakeSureContactIsNextInProgressQueue(databaseContact);
+
+			synchronizeToCrm.Execute();
+
+			DatabaseExternalContact externalContact = DatabaseExternalContact.Read(_sqlConnection, _changeProvider.Id).Single();
+			Contact contact = Contact.Read(_dynamicsCrmConnection, externalContact.ExternalContactId);
+			List<string> readGroupNames = contact.Groups.Select(group => group.Name).ToList();
+			readGroupNames.Sort();
+
+			contact.Delete();
+			Assert.AreEqual("group1", readGroupNames.First());
+			Assert.AreEqual("group2", readGroupNames.Last());
+		}
+
+		[Test]
+		public void GroupsCanBeRemovedFromContact()
+		{
+			DatabaseSynchronizeToCrm databaseSynchronizeFromCrm = GetDatabaseSynchronizeToCrm();
+			SynchronizeToCrm synchronizeToCrm = new SynchronizeToCrm(Connection, databaseSynchronizeFromCrm);
+
+			DatabaseContact databaseContact = CreateContact();
+			AddGroupsToDatabaseContact(databaseContact, "group1", "group2");
+
+			MakeSureContactIsNextInProgressQueue(databaseContact);
+
+			synchronizeToCrm.Execute();
+
+			RemoveGroupFromDatabaseContact(databaseContact, "group1");
+
+			synchronizeToCrm.Execute();
+
+			DatabaseExternalContact externalContact = DatabaseExternalContact.Read(_sqlConnection, _changeProvider.Id).Single();
+			Contact contact = Contact.Read(_dynamicsCrmConnection, externalContact.ExternalContactId);
+			List<string> readGroupNames = contact.Groups.Select(group => group.Name).ToList();
+
+			contact.Delete();
+			Assert.AreEqual("group2", readGroupNames.Single());
+		}
+
+		[Test]
+		public void GroupsWillBeSynchronizedOnAccount()
+		{
+			DatabaseSynchronizeToCrm databaseSynchronizeFromCrm = GetDatabaseSynchronizeToCrm();
+			SynchronizeToCrm synchronizeToCrm = new SynchronizeToCrm(Connection, databaseSynchronizeFromCrm);
+
+			DatabaseAccount databaseAccount = CreateAccount();
+			AddGroupsToDatabaseAccount(databaseAccount, "group1", "group2");
+
+			MakeSureAccountIsNextInProgressQueue(databaseAccount);
+
+			synchronizeToCrm.Execute();
+
+			DatabaseExternalAccount externalAccount = DatabaseExternalAccount.Read(_sqlConnection, _changeProvider.Id).Single();
+			Account account = Account.Read(_dynamicsCrmConnection, externalAccount.ExternalAccountId);
+			List<string> readGroupNames = account.ReadGroups().Select(group => group.Name).ToList();
+
+			readGroupNames.Sort();
+
+			account.Delete();
+			Assert.AreEqual("group1", readGroupNames.First());
+			Assert.AreEqual("group2", readGroupNames.Last());
+		}
+
+		[Test]
+		public void AccountsWillBeSynchronized()
+		{
+			DatabaseSynchronizeToCrm databaseSynchronizeFromCrm = GetDatabaseSynchronizeToCrm();
+			SynchronizeToCrm synchronizeToCrm = new SynchronizeToCrm(Connection, databaseSynchronizeFromCrm);
+
+			DatabaseContact databaseContact = CreateContact();
+			MakeSureContactIsNextInProgressQueue(databaseContact);
+
+			DatabaseAccount databaseAccount = CreateAccount();
+			MakeSureAccountIsNextInProgressQueue(databaseAccount);
+
+			DatabaseAccountContact accountContact = new DatabaseAccountContact(databaseAccount.Id, databaseContact.Id);
+			accountContact.Insert(_sqlConnection);
+
+			synchronizeToCrm.Execute();
+
+			DatabaseExternalAccount externalAccount = DatabaseExternalAccount.Read(_sqlConnection, _changeProvider.Id).Single();
+			DatabaseExternalContact externalContact = DatabaseExternalContact.Read(_sqlConnection, _changeProvider.Id).Single();
+			Account account = Account.Read(_dynamicsCrmConnection, externalAccount.ExternalAccountId);
+			List<Contact> contacts = account.ReadContacts();
+
+			Assert.AreEqual(externalContact.ExternalContactId, contacts.Single().Id);
+		}
+
+		[Test]
+		public void IndsamlereWillBeSynchronized()
+		{
+			DatabaseSynchronizeToCrm databaseSynchronizeFromCrm = GetDatabaseSynchronizeToCrm();
+			SynchronizeToCrm synchronizeToCrm = new SynchronizeToCrm(Connection, databaseSynchronizeFromCrm);
+
+			DatabaseContact databaseContact = CreateContact();
+			MakeSureContactIsNextInProgressQueue(databaseContact);
+
+			DatabaseAccount databaseAccount = CreateAccount();
+			MakeSureAccountIsNextInProgressQueue(databaseAccount);
+
+			DatabaseAccountIndsamler accountIndsamler = new DatabaseAccountIndsamler(databaseAccount.Id, databaseContact.Id);
+			accountIndsamler.Insert(_sqlConnection);
+
+			synchronizeToCrm.Execute();
+
+			DatabaseExternalAccount externalAccount = DatabaseExternalAccount.Read(_sqlConnection, _changeProvider.Id).Single();
+			DatabaseExternalContact externalContact = DatabaseExternalContact.Read(_sqlConnection, _changeProvider.Id).Single();
+			Account account = Account.Read(_dynamicsCrmConnection, externalAccount.ExternalAccountId);
+			List<Contact> indsamlere = account.ReadIndsamlere();
+
+			Assert.AreEqual(externalContact.ExternalContactId, indsamlere.Single().Id);
+		}
+
+		private void AddGroupsToDatabaseContact(DatabaseContact databaseContact, params string[] groupNames)
+		{
+			foreach (string groupName in groupNames)
+			{
+				DatabaseGroup group = ReadOrCreateGroup(groupName);
+
+				DatabaseContactGroup contactGroup = new DatabaseContactGroup(databaseContact.Id, group.Id);
+				contactGroup.Insert(_sqlConnection);
+			}
+		}
+
+		private void AddGroupsToDatabaseAccount(DatabaseAccount databaseAccount, params string[] groupNames)
+		{
+			foreach (string groupName in groupNames)
+			{
+				DatabaseGroup group = ReadOrCreateGroup(groupName);
+
+				DatabaseAccountGroup accountGroup = new DatabaseAccountGroup(databaseAccount.Id, group.Id);
+				accountGroup.Insert(_sqlConnection);
+			}
+		}
+
+		private void RemoveGroupFromDatabaseContact(DatabaseContact databaseContact, params string[] groupNames)
+		{
+			foreach (string groupName in groupNames)
+			{
+				DatabaseGroup group = DatabaseGroup.ReadByName(_sqlConnection, groupName);
+				DatabaseContactGroup contactGroup = new DatabaseContactGroup(databaseContact.Id, group.Id);
+
+				contactGroup.Delete(_sqlConnection);
+			}
+		}
+
+		private DatabaseGroup ReadOrCreateGroup(string groupName)
+		{
+			if (DatabaseGroup.ExistsByName(_sqlConnection, groupName))
+			{
+				return DatabaseGroup.ReadByName(_sqlConnection, groupName);
+			}
+
+			DatabaseGroup group = new DatabaseGroup()
+			{
+				Name = groupName,
+			};
+
+			group.Insert(_sqlConnection);
+
+			return group;
 		}
 
 		private Contact InsertCrmContact()
