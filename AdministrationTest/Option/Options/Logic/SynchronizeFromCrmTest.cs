11c2628f146002b89a6f9bddda23617b115dff62
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-02-05 09:22:18 +0100

    (SynchronizeFromCrmTest) testing that AccountContacts can be added

--- AdministrationTest/Option/Options/Logic/SynchronizeFromCrmTest.cs
+++ AdministrationTest/Option/Options/Logic/SynchronizeFromCrmTest.cs
@@ -2,6 +2,8 @@
 using SystemInterface.Dynamics.Crm;
 using DatabaseChangeProvider = DataLayer.SqlData.ChangeProvider;
 using DatabaseSynchronizeFromCrm = DataLayer.MongoData.Option.Options.Logic.SynchronizeFromCrm;
+using DatabaseAccountChangeContact = DataLayer.SqlData.Account.AccountChangeContact;
+using DatabaseExternalContact = DataLayer.SqlData.Contact.ExternalContact;
 using System.Linq;
 using System.Collections.Generic;
 using Administration.Option.Options.Logic;
@@ -69,6 +71,31 @@ namespace AdministrationTest.Option.Options.Logic
 			Assert.IsTrue(contactChanges.Any(contactChange => contactChange.Firstname == firstname2));
 		}
 
+		[Test]
+		public void AccountContactsCanBeAdded()
+		{
+			string firstname1 = "firstname1";
+			string name2 = "name2";
+
+			Contact crmContact = CreateCrmContact(firstname1);
+			Account crmAccount = CreateCrmAccount(name2);
+
+			crmContact.Insert();
+			crmAccount.Insert();
+
+			crmAccount.SynchronizeContacts(new List<Contact>() { crmContact });
+
+			_synchronizeFromCrm.Execute();
+
+			DatabaseExternalContact databaseExternalContact = DatabaseExternalContact.Read(_sqlConnection, crmContact.Id, _changeProvider.Id);
+			List<DatabaseAccountChangeContact> accountChangeContacts = DatabaseAccountChangeContact.ReadFromContactId(_sqlConnection, databaseExternalContact.ContactId);
+
+			crmContact.Delete();
+			crmAccount.Delete();
+
+			Assert.AreEqual(1, accountChangeContacts.Count);
+		}
+
 		private Contact CreateCrmContact(string firstname1)
 		{
 			return new Contact(_dynamicsCrmConnection)
@@ -77,6 +104,14 @@ namespace AdministrationTest.Option.Options.Logic
 				firstname = firstname1,
 				lastname = "lastname1",
 				modifiedon = DateTime.Now,
+			};
+		}
+
+		private Account CreateCrmAccount(string name)
+		{
+			return new Account(_dynamicsCrmConnection)
+			{
+				name = name,
 			};
 		}
 	}
