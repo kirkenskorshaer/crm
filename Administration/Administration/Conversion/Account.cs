9fe11703e1429453a3d8692c17c77c39d2c90310
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-02-16 14:27:34 +0100

    (Account) now using reflection to convert data to crm Account

--- Administration/Administration/Conversion/Account.cs
+++ Administration/Administration/Conversion/Account.cs
@@ -1,6 +1,7 @@
 ﻿using SystemInterfaceAccount = SystemInterface.Dynamics.Crm.Account;
 using DatabaseAccount = DataLayer.SqlData.Account.Account;
 using SystemInterface.Dynamics.Crm;
+using System.Collections.Generic;
 
 namespace Administration.Conversion
 {
@@ -16,7 +17,14 @@ namespace Administration.Conversion
 
 		public static void Convert(DynamicsCrmConnection dynamicsCrmConnection, DatabaseAccount fromAccount, SystemInterfaceAccount toAccount)
 		{
-			toAccount.name = fromAccount.name;
+			List<string> exclusionList = new List<string>() { "Id" };
+			List<string> keys = Utilities.ReflectionHelper.GetFieldsAndProperties(typeof(DatabaseAccount), exclusionList);
+
+			foreach (string key in keys)
+			{
+				object value = Utilities.ReflectionHelper.GetValue(fromAccount, key);
+				Utilities.ReflectionHelper.SetValue(toAccount, key, value);
+			}
 		}
 	}
 }
