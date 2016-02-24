a731148cf236aa5c4e22c323f923fda1fa99a7f0
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-02-24 14:08:38 +0100

    added SystemUserTest

---
+++ SystemInterfaceTest/Dynamics/Crm/SystemUserTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using SystemInterface.Dynamics.Crm;
+
+namespace SystemInterfaceTest.Dynamics.Crm
+{
+	[TestFixture]
+	public class SystemUserTest : TestBase
+	{
+		[Test]
+		public void ContactIsCreatedByConnectingUser()
+		{
+			Contact contactInserted = CreateTestContact();
+			contactInserted.Insert();
+
+			Contact contactRead = Contact.Read(_dynamicsCrmConnection, contactInserted.Id);
+
+			contactInserted.Delete();
+
+			SystemUser user = SystemUser.Read(_dynamicsCrmConnection, contactRead.modifiedbyGuid.Value);
+
+			Assert.AreEqual("KAD\\" + _urlLogin.Username, user.domainname);
+		}
+
+		[Test]
+		public void UserCanBeRetreivedByDomainName()
+		{
+			string domainName = "KAD\\" + _urlLogin.Username;
+            SystemUser user = SystemUser.ReadByDomainname(_dynamicsCrmConnection, domainName);
+
+			Assert.IsNotNull(user);
+		}
+	}
+}
