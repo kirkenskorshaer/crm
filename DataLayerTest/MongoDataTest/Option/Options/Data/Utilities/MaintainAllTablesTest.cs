37ec65f4c09dabab0ff7a7658bc30a2d4c428689
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-02-25 09:30:33 +0100

    added MaintainAllTablesTest

---
+++ DataLayerTest/MongoDataTest/Option/Options/Data/Utilities/MaintainAllTablesTest.cs
@@ -0,0 +1,17 @@
+﻿using DataLayer.MongoData.Option.Options.Data.Utilities;
+using NUnit.Framework;
+
+namespace DataLayerTest.MongoDataTest.Option.Options.Data.Utilities
+{
+	[TestFixture]
+	public class MaintainAllTablesTest : TestBase
+	{
+		[Test]
+		public void InsertAndDelete()
+		{
+			MaintainAllTables maintainAllTables = MaintainAllTables.Create(_mongoConnection, "test", MongoTestUtilities.CreateOneTimeSimpleSchedule());
+
+			maintainAllTables.Delete(_mongoConnection);
+		}
+	}
+}
---
+++ DataLayerTest/TestBase.cs
@@ -0,0 +1,18 @@
+﻿using DataLayer;
+using NUnit.Framework;
+
+namespace DataLayerTest
+{
+	[TestFixture]
+	public class TestBase
+	{
+		protected MongoConnection _mongoConnection;
+
+		[TestFixtureSetUp]
+		public void SetUp()
+		{
+			_mongoConnection = MongoConnection.GetConnection("test");
+			_mongoConnection.CleanDatabase();
+		}
+	}
+}
