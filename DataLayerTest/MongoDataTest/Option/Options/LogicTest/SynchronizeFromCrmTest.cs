8ebc22b80c2f8f0aba1aa035f9b349bf5fc39ec9
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-02-25 10:46:46 +0100

    testing SynchronizeFromCrm

---
+++ DataLayerTest/MongoDataTest/Option/Options/LogicTest/SynchronizeFromCrmTest.cs
@@ -0,0 +1,26 @@
+﻿using DataLayer.MongoData.Option;
+using DataLayer.MongoData.Option.Options.Logic;
+using NUnit.Framework;
+using System;
+
+namespace DataLayerTest.MongoDataTest.Option.Options.LogicTest
+{
+	[TestFixture]
+	public class SynchronizeFromCrmTest : TestBase
+	{
+		[Test]
+		public void InsertAndDelete()
+		{
+			Schedule schedule = new Schedule()
+			{
+				NextAllowedExecution = DateTime.Now,
+				Recurring = true,
+				TimeBetweenAllowedExecutions = TimeSpan.FromMinutes(1),
+			};
+
+			SynchronizeFromCrm synchronizeFromCrm = SynchronizeFromCrm.Create(_mongoConnection, "test", schedule, "test", Guid.Parse("8C6A4E39-8350-4C6B-9085-6B6BE091BE6D"));
+
+			synchronizeFromCrm.Delete(_mongoConnection);
+		}
+	}
+}
