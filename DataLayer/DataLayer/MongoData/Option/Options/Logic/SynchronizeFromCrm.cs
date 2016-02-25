8ebf4dcb63c4eae48ee839ec08a364351b874b03
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-02-25 10:46:27 +0100

    (SynchronizeFromCrm) added Create and Delete methods

--- DataLayer/DataLayer/MongoData/Option/Options/Logic/SynchronizeFromCrm.cs
+++ DataLayer/DataLayer/MongoData/Option/Options/Logic/SynchronizeFromCrm.cs
@@ -7,6 +7,24 @@ namespace DataLayer.MongoData.Option.Options.Logic
 		public Guid changeProviderId { get; set; }
 		public string urlLoginName { get; set; }
 
+		public static SynchronizeFromCrm Create(MongoConnection connection, string name, Schedule schedule, string urlLoginName, Guid changeProviderId)
+		{
+			SynchronizeFromCrm synchronizeFromCrm = new SynchronizeFromCrm
+			{
+				changeProviderId = changeProviderId,
+				urlLoginName = urlLoginName,
+			};
+
+			Create(connection, synchronizeFromCrm, name, schedule);
+
+			return synchronizeFromCrm;
+		}
+
+		public void Delete(MongoConnection connection)
+		{
+			Delete<SynchronizeFromCrm>(connection);
+		}
+
 		protected override void Execute(MongoConnection connection, bool recurring)
 		{
 			if (recurring)
