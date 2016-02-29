0d8214b885a265e185013ba4641253f6a5731104
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-02-29 14:28:33 +0100

    (Account) added convert from SystemInterfaceAccount to DatabaseAccountChange

--- Administration/Administration/Conversion/Account.cs
+++ Administration/Administration/Conversion/Account.cs
@@ -1,5 +1,6 @@
 ﻿using SystemInterfaceAccount = SystemInterface.Dynamics.Crm.Account;
 using DatabaseAccount = DataLayer.SqlData.Account.Account;
+using DatabaseAccountChange = DataLayer.SqlData.Account.AccountChange;
 using DatabaseExternalContact = DataLayer.SqlData.Contact.ExternalContact;
 using SystemInterface.Dynamics.Crm;
 using System.Collections.Generic;
@@ -55,6 +56,47 @@ namespace Administration.Conversion
 		}
 
 		public static void Convert(SqlConnection sqlConnection, Guid changeProviderId, SystemInterfaceAccount fromAccount, DatabaseAccount toAccount)
+		{
+			List<string> exclusionList = new List<string>() {
+				"Id",
+				"new_bykoordinatorid", "bykoordinatorid",
+				"new_omraadekoordinatorid", "omraadekoordinatorid",
+				"new_kredsellerby", "kredsellerby"
+			};
+
+			List<string> keys = Utilities.ReflectionHelper.GetFieldsAndProperties(typeof(SystemInterfaceAccount), exclusionList);
+
+			foreach (string key in keys)
+			{
+				object value = Utilities.ReflectionHelper.GetValue(fromAccount, key);
+				Utilities.ReflectionHelper.SetValue(toAccount, key, value);
+			}
+
+			if (fromAccount.bykoordinatorid.HasValue)
+			{
+				List<DatabaseExternalContact> externalContacts = DatabaseExternalContact.ReadFromChangeProviderAndContact(sqlConnection, changeProviderId, fromAccount.bykoordinatorid.Value);
+				if (externalContacts.Any())
+				{
+					toAccount.bykoordinatorid = externalContacts.First().ContactId;
+				}
+			}
+
+			if (fromAccount.omraadekoordinatorid.HasValue)
+			{
+				List<DatabaseExternalContact> externalContacts = DatabaseExternalContact.ReadFromChangeProviderAndContact(sqlConnection, changeProviderId, fromAccount.omraadekoordinatorid.Value);
+				if (externalContacts.Any())
+				{
+					toAccount.omraadekoordinatorid = externalContacts.First().ContactId;
+				}
+			}
+
+			if (fromAccount.kredsellerby.HasValue)
+			{
+				toAccount.kredsellerby = (int)fromAccount.kredsellerby;
+			}
+		}
+
+		public static void Convert(SqlConnection sqlConnection, Guid changeProviderId, SystemInterfaceAccount fromAccount, DatabaseAccountChange toAccount)
 		{
 			List<string> exclusionList = new List<string>() {
 				"Id",
