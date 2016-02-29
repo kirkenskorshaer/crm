33d683f3a618c9507b8e55f5fd05b822181a48a6
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-02-29 14:28:00 +0100

    (Account) added convert from SystemInterfaceAccount to DatabaseAccount

--- Administration/Administration/Conversion/Account.cs
+++ Administration/Administration/Conversion/Account.cs
@@ -53,5 +53,46 @@ namespace Administration.Conversion
 				toAccount.kredsellerby = (SystemInterfaceAccount.kredsellerbyEnum)fromAccount.kredsellerby;
 			}
 		}
+
+		public static void Convert(SqlConnection sqlConnection, Guid changeProviderId, SystemInterfaceAccount fromAccount, DatabaseAccount toAccount)
+		{
+			List<string> exclusionList = new List<string>() {
+				"Id",
+				"new_bykoordinatorid", "bykoordinatorid",
+				"new_omraadekoordinatorid", "omraadekoordinatorid",
+				"new_kredsellerby", "kredsellerby"
+			};
+
+			List<string> keys = Utilities.ReflectionHelper.GetFieldsAndProperties(typeof(SystemInterfaceAccount), exclusionList);
+
+			foreach (string key in keys)
+			{
+				object value = Utilities.ReflectionHelper.GetValue(fromAccount, key);
+				Utilities.ReflectionHelper.SetValue(toAccount, key, value);
+			}
+
+			if (fromAccount.bykoordinatorid.HasValue)
+			{
+				List<DatabaseExternalContact> externalContacts = DatabaseExternalContact.ReadFromChangeProviderAndContact(sqlConnection, changeProviderId, fromAccount.bykoordinatorid.Value);
+				if (externalContacts.Any())
+				{
+					toAccount.bykoordinatorid = externalContacts.First().ContactId;
+				}
+			}
+
+			if (fromAccount.omraadekoordinatorid.HasValue)
+			{
+				List<DatabaseExternalContact> externalContacts = DatabaseExternalContact.ReadFromChangeProviderAndContact(sqlConnection, changeProviderId, fromAccount.omraadekoordinatorid.Value);
+				if (externalContacts.Any())
+				{
+					toAccount.omraadekoordinatorid = externalContacts.First().ContactId;
+				}
+			}
+
+			if (fromAccount.kredsellerby.HasValue)
+			{
+				toAccount.kredsellerby = (int)fromAccount.kredsellerby;
+			}
+		}
 	}
 }
