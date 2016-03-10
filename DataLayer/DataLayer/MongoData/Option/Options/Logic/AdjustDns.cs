6943bffc60b7cd34229c8ac4fed49f4db9c9d42b
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-03-10 13:44:15 +0100

    adding AdjustDns to datalayer

---
+++ DataLayer/DataLayer/MongoData/Option/Options/Logic/AdjustDns.cs
@@ -0,0 +1,20 @@
+﻿namespace DataLayer.MongoData.Option.Options.Logic
+{
+	public class AdjustDns : OptionBase
+	{
+		public string dnsIp { get; set; }
+		public string adapterName { get; set; }
+
+		protected override void Execute(MongoConnection connection, bool recurring)
+		{
+			if (recurring)
+			{
+				Update<AdjustDns>(connection);
+			}
+			else
+			{
+				Delete<AdjustDns>(connection);
+			}
+		}
+	}
+}
