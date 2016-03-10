82236e228b53637ae595b35fb07420b80ec4a9cd
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-03-10 09:45:27 +0100

    (KKAdminServiceTest) testing the interface

---
+++ SystemInterfaceTest/DsiNext/KKAdminServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using DataLayer.MongoData;
+using NUnit.Framework;
+using SystemInterface;
+
+namespace SystemInterfaceTest.DsiNext
+{
+	[TestFixture]
+	public class KKAdminServiceTest : TestBase
+	{
+		[Test]
+		public void Test()
+		{
+			UrlLogin urlLogin = UrlLogin.GetUrlLogin(_mongoConnection, "KKAdmin");
+			InterfaceHelper initializer = InterfaceHelper.GetInstance();
+			initializer.AllowThumbprint(urlLogin.CertificateThumbprint);
+
+			SystemInterface.KKAdminService.KKAdminServiceClient proxy = new SystemInterface.KKAdminService.KKAdminServiceClient();
+
+			string version = proxy.GetVersion();
+			Assert.AreEqual("1.00", version);
+		}
+	}
+}
