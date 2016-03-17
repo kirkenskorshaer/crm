b25db48b00eaa8558a00aefbe6d670fc9ffacf64
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-03-17 14:39:47 +0100

    (AbstractIdData) added Insert method

--- DataLayer/DataLayer/SqlData/AbstractIdData.cs
+++ DataLayer/DataLayer/SqlData/AbstractIdData.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
 using System.Text;
+using NonSqlUtilities = Utilities;
 
 namespace DataLayer.SqlData
 {
@@ -20,6 +22,39 @@ namespace DataLayer.SqlData
 			sqlStringBuilder.AppendLine("	id = @id");
 
 			Utilities.ExecuteNonQuery(sqlConnection, sqlStringBuilder, CommandType.Text, new KeyValuePair<string, object>("id", Id));
+		}
+
+		public void Insert(SqlConnection sqlConnection)
+		{
+			StringBuilder sqlStringBuilderColumns = new StringBuilder();
+			StringBuilder sqlStringBuilderParameters = new StringBuilder();
+			List<KeyValuePair<string, object>> parameters = new List<KeyValuePair<string, object>>();
+
+			List<SqlColumnInfo> dataColumns = _sqlColumnsInfo.Where(SqlColumnInfo.IsNotPrimaryKey).ToList();
+
+			foreach (SqlColumnInfo sqlColumn in dataColumns)
+			{
+				object value = NonSqlUtilities.ReflectionHelper.GetValue(this, sqlColumn.Name);
+				AddInsertParameterIfNotNull(value, sqlColumn.Name.ToLower(), sqlStringBuilderColumns, sqlStringBuilderParameters, parameters);
+			}
+
+			StringBuilder sqlStringBuilder = new StringBuilder();
+			sqlStringBuilder.AppendLine("INSERT INTO");
+			sqlStringBuilder.AppendLine("	" + TableName);
+			sqlStringBuilder.AppendLine("(");
+			sqlStringBuilder.Append(sqlStringBuilderColumns);
+			sqlStringBuilder.AppendLine(")");
+			sqlStringBuilder.AppendLine("OUTPUT");
+			sqlStringBuilder.AppendLine("	Inserted.id");
+			sqlStringBuilder.AppendLine("VALUES");
+			sqlStringBuilder.AppendLine("(");
+			sqlStringBuilder.Append(sqlStringBuilderParameters);
+			sqlStringBuilder.AppendLine(")");
+
+			DataTable dataTable = Utilities.ExecuteAdapterSelect(sqlConnection, sqlStringBuilder, parameters.ToArray());
+
+			DataRow row = dataTable.Rows[0];
+			Id = (Guid)row["id"];
 		}
 
 		public override bool Equals(object obj)
