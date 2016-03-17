d2959366ff6b25cc94243fc020a6579de8e7d838
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-03-17 11:59:14 +0100

    added Attribute SqlColumn

---
+++ DataLayer/DataLayer/SqlData/SqlColumn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DataLayer.SqlData
+{
+	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+	public class SqlColumn : Attribute
+	{
+		public enum PropertyEnum
+		{
+			None = 0,
+			PrimaryKey = 1,
+		}
+
+		public SqlColumn(PropertyEnum properties, Utilities.DataType dataType, bool allowNull)
+		{
+			Properties = properties;
+			DataType = dataType;
+			AllowNull = allowNull;
+		}
+
+		public PropertyEnum Properties { get; private set; }
+		public Utilities.DataType DataType { get; private set; }
+		public bool AllowNull { get; private set; }
+	}
+}
