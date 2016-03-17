12a2cba64439f11b843edb7371538309fbf1edb1
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-03-17 12:04:23 +0100

    added data class Byarbejde

---
+++ DataLayer/DataLayer/SqlData/Byarbejde/Byarbejde.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace DataLayer.SqlData.Byarbejde
+{
+	public class Byarbejde : AbstractIdData
+	{
+		[SqlColumn(SqlColumn.PropertyEnum.None, Utilities.DataType.NVARCHAR_MAX, false)]
+		public string new_name;
+
+		public static void MaintainTable(SqlConnection sqlConnection)
+		{
+			Type dataClassType = typeof(Byarbejde);
+
+			Utilities.MaintainTable(sqlConnection, dataClassType);
+		}
+	}
+}
