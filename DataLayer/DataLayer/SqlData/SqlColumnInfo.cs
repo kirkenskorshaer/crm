c432ca564752667c0ea95dee6e67702e9d6ba923
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-03-17 12:00:43 +0100

    added SqlColumnInfo

---
+++ DataLayer/DataLayer/SqlData/SqlColumnInfo.cs
@@ -0,0 +1,24 @@
+﻿namespace DataLayer.SqlData
+{
+	public class SqlColumnInfo
+	{
+		public SqlColumnInfo(string name, SqlColumn sqlField)
+		{
+			Name = name;
+			SqlColumn = sqlField;
+		}
+
+		public string Name;
+		public SqlColumn SqlColumn;
+
+		public static bool IsPrimaryKey(SqlColumnInfo info)
+		{
+			return info.SqlColumn.Properties.HasFlag(SqlColumn.PropertyEnum.PrimaryKey);
+		}
+
+		public static bool IsNotPrimaryKey(SqlColumnInfo info)
+		{
+			return info.SqlColumn.Properties.HasFlag(SqlColumn.PropertyEnum.PrimaryKey) == false;
+		}
+	}
+}
