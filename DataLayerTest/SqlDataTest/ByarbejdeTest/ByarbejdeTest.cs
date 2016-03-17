4a6997c25eee54ba443972900f060dd405b94925
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-03-17 14:43:48 +0100

    added ByarbejdeTest

---
+++ DataLayerTest/SqlDataTest/ByarbejdeTest/ByarbejdeTest.cs
@@ -0,0 +1,15 @@
+﻿using DataLayer.SqlData.Byarbejde;
+using NUnit.Framework;
+
+namespace DataLayerTest.SqlDataTest.ByarbejdeTest
+{
+	[TestFixture]
+	public class ByarbejdeTest: TestBase
+	{
+		[Test]
+		public void MaintainTable()
+		{
+			Byarbejde.MaintainTable(_sqlConnection);
+		}
+    }
+}
