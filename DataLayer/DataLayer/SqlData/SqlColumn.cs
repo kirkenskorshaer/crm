04434ad4d4831b8053dcfb0379cd231cbbac723c
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-03-18 11:06:43 +0100

    (SqlColumn) added optional parameters for definition of foreign keys

--- DataLayer/DataLayer/SqlData/SqlColumn.cs
+++ DataLayer/DataLayer/SqlData/SqlColumn.cs
@@ -9,17 +9,24 @@ namespace DataLayer.SqlData
 		{
 			None = 0,
 			PrimaryKey = 1,
+			ForeignKey = 2,
 		}
 
-		public SqlColumn(PropertyEnum properties, Utilities.DataType dataType, bool allowNull)
+		public SqlColumn(PropertyEnum properties, Utilities.DataType dataType, bool allowNull, Type foreignKeyTable = null, string foreignKeyIdName = null, bool foreignKeyCascade = false)
 		{
 			Properties = properties;
 			DataType = dataType;
 			AllowNull = allowNull;
+			ForeignKeyTable = foreignKeyTable;
+			ForeignKeyIdName = foreignKeyIdName;
+			ForeignKeyCascade = foreignKeyCascade;
 		}
 
 		public PropertyEnum Properties { get; private set; }
 		public Utilities.DataType DataType { get; private set; }
 		public bool AllowNull { get; private set; }
+		public Type ForeignKeyTable { get; private set; }
+		public string ForeignKeyIdName { get; private set; }
+		public bool ForeignKeyCascade { get; private set; }
 	}
 }
