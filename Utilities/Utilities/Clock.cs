182518d36a42cd89c5a568b9e07b64ecbfabe8a5
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-04-05 14:10:00 +0200

    adding Clock

---
+++ Utilities/Utilities/Clock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Utilities
+{
+	public class Clock
+	{
+		public static Func<DateTime> NowFunc = () => DateTime.Now;
+
+		public static DateTime Now
+		{
+			get
+			{
+				return NowFunc();
+			}
+		}
+	}
+}
