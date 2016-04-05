45f82f46921b0388c20b068c1a36dd949e7d63c8
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-04-05 11:03:27 +0200

    adding class to define foreign key information on a sql column

---
+++ DataLayer/DataLayer/SqlData/ForeignKeyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DataLayer.SqlData
+{
+	public class ForeignKeyInfo
+	{
+		public ForeignKeyInfo(string foreignKeyGroup, Type foreignKeyTable, string foreignKeyIdName, bool foreignKeyCascade, int foreignKeyIndex)
+		{
+			ForeignKeyGroup = foreignKeyGroup;
+			ForeignKeyTable = foreignKeyTable;
+			ForeignKeyIdName = foreignKeyIdName;
+			ForeignKeyCascade = foreignKeyCascade;
+			ForeignKeyIndex = foreignKeyIndex;
+        }
+
+		public string ForeignKeyGroup { get; private set; }
+		public Type ForeignKeyTable { get; private set; }
+		public string ForeignKeyIdName { get; private set; }
+		public bool ForeignKeyCascade { get; private set; }
+		public int ForeignKeyIndex { get; private set; }
+
+		public SqlColumnInfo SqlColumnInfo { get; set; }
+	}
+}
