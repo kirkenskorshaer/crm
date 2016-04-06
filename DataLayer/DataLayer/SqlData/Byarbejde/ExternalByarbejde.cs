256b88043fabd1b4e53ab0d4a968f919345c7eeb
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-04-06 15:08:30 +0200

    added ExternalByarbejde

---
+++ DataLayer/DataLayer/SqlData/Byarbejde/ExternalByarbejde.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace DataLayer.SqlData.Byarbejde
+{
+	public class ExternalByarbejde : AbstractData
+	{
+		[SqlColumn(SqlColumn.PropertyEnum.PrimaryKey, SqlUtilities.DataType.UNIQUEIDENTIFIER, false)]
+		public Guid ExternalByarbejdeId { get; private set; }
+
+		[SqlColumn(SqlColumn.PropertyEnum.PrimaryKey | SqlColumn.PropertyEnum.ForeignKey, SqlUtilities.DataType.UNIQUEIDENTIFIER, false, "byarbejde", typeof(Byarbejde), "id", true, 1)]
+		public Guid ByarbejdeId { get; private set; }
+
+		[SqlColumn(SqlColumn.PropertyEnum.PrimaryKey | SqlColumn.PropertyEnum.ForeignKey, SqlUtilities.DataType.UNIQUEIDENTIFIER, false, "changeprovider", typeof(ChangeProvider), "id", true, 1)]
+		public Guid ChangeProviderId { get; private set; }
+
+		public static void MaintainTable(SqlConnection sqlConnection)
+		{
+			ChangeProvider.MaintainTable(sqlConnection);
+			Byarbejde.MaintainTable(sqlConnection);
+
+			SqlUtilities.MaintainTable(sqlConnection, typeof(ExternalByarbejde));
+		}
+
+		public static List<ExternalByarbejde> ReadFromChangeProviderAndByarbejde(SqlConnection sqlConnection, Guid changeProviderId, Guid byarbejdeId)
+		{
+			return Read<ExternalByarbejde>(sqlConnection, new List<SqlCondition> { new SqlCondition("ChangeProviderId", "=", changeProviderId), new SqlCondition("ByarbejdeId", "=", byarbejdeId) });
+		}
+	}
+}
