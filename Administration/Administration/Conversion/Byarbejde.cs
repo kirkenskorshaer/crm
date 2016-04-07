1d0e86909d880cae63072e0c2ddf84f7c6dd7d57
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-04-07 10:11:43 +0200

    added conversion between byarbejde in systeminterface and byarbejde in datalayer

---
+++ Administration/Administration/Conversion/Byarbejde.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using SystemInterface.Dynamics.Crm;
+using SystemInterfaceByarbejde = SystemInterface.Dynamics.Crm.Byarbejde;
+using DatabaseByarbejde = DataLayer.SqlData.Byarbejde.Byarbejde;
+
+namespace Administration.Conversion
+{
+	public static class Byarbejde
+	{
+		public static SystemInterfaceByarbejde Convert(DynamicsCrmConnection dynamicsCrmConnection, DatabaseByarbejde fromByarbejde)
+		{
+			SystemInterfaceByarbejde toByarbejde = new SystemInterfaceByarbejde(dynamicsCrmConnection);
+			Convert(fromByarbejde, toByarbejde);
+
+			return toByarbejde;
+		}
+
+		public static void Convert(DatabaseByarbejde fromByarbejde, SystemInterfaceByarbejde toByarbejde)
+		{
+			List<string> exclusionList = new List<string>() { "Id" };
+			List<string> keys = Utilities.ReflectionHelper.GetFieldsAndProperties(typeof(DatabaseByarbejde), exclusionList);
+
+			foreach (string key in keys)
+			{
+				object value = Utilities.ReflectionHelper.GetValue(fromByarbejde, key);
+				Utilities.ReflectionHelper.SetValue(toByarbejde, key, value);
+			}
+		}
+
+		public static void Convert(SystemInterfaceByarbejde fromByarbejde, DatabaseByarbejde toByarbejde)
+		{
+			List<string> exclusionList = new List<string>() { "Id" };
+			List<string> keys = Utilities.ReflectionHelper.GetFieldsAndProperties(typeof(SystemInterfaceByarbejde), exclusionList);
+
+			foreach (string key in keys)
+			{
+				object value = Utilities.ReflectionHelper.GetValue(fromByarbejde, key);
+				Utilities.ReflectionHelper.SetValue(toByarbejde, key, value);
+			}
+		}
+	}
+}
