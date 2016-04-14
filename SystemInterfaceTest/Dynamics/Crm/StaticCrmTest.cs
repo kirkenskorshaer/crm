03678320078eb04a7fead8254b176a77bcc0e86b
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-04-14 12:43:37 +0200

    testing GetAllAttributeNames

---
+++ SystemInterfaceTest/Dynamics/Crm/StaticCrmTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using SystemInterface.Dynamics.Crm;
+
+namespace SystemInterfaceTest.Dynamics.Crm
+{
+	[TestFixture]
+	public class StaticCrmTest : TestBase
+	{
+		[Test]
+		public void GetAllAttributeNames()
+		{
+			List<string> attributeNames = StaticCrm.GetAllAttributeNames(_dynamicsCrmConnection, typeof(Annotation));
+
+			Assert.Greater(attributeNames.Count, 3);
+		}
+	}
+}
