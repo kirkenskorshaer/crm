713839b2f20ab7e0c718c1240db1ccdeed0eb049
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-05-06 11:20:47 +0200

    (WebInterface) adding stub

---
+++ WebInterface/Stub.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Web;
+using DatabaseStub = DataLayer.MongoData.Input.Stub;
+using DatabaseStubPusher = DataLayer.MongoData.Input.StubPusher;
+using DatabaseStubElement = DataLayer.MongoData.Input.StubElement;
+using DatabaseWebCampaign = DataLayer.MongoData.Input.WebCampaign;
+using System.Configuration;
+using DataLayer;
+
+public partial class Stub : System.Web.UI.Page
+{
+	protected void Page_Load(object sender, EventArgs e)
+	{
+		string errorRedirect = "error.html";
+
+		if (Request.HttpMethod != "POST")
+		{
+			Response.Redirect(errorRedirect);
+		}
+
+		string databaseName = ConfigurationManager.AppSettings["mongoDatabaseName"];
+		MongoConnection mongoConnection = MongoConnection.GetConnection(databaseName);
+
+		Guid formId = Guid.Empty;
+		Guid.TryParse(Request.Form["formId"], out formId);
+
+		DatabaseWebCampaign webCampaign = DatabaseWebCampaign.ReadSingleOrDefault(mongoConnection, formId);
+
+		if (webCampaign == null)
+		{
+			Response.Redirect(errorRedirect);
+		}
+
+		DatabaseStub stub = new DatabaseStub()
+		{
+			PostTime = DateTime.Now,
+			WebCampaignId = webCampaign._id,
+		};
+
+		foreach (string key in Request.Form)
+		{
+			if(key == "formId")
+			{
+				continue;
+			}
+
+			DatabaseStubElement element = new DatabaseStubElement()
+			{
+				Key = key,
+				//b = Request.Form[key],
+				Value = HttpUtility.HtmlDecode(Request.Form[key]),
+			};
+
+			stub.Contents.Add(element);
+		}
+
+		DatabaseStubPusher.GetInstance(mongoConnection).Push(stub);
+
+		string redirectTarget = "error.html";
+		if (webCampaign != null)
+		{
+			redirectTarget = webCampaign.RedirectTarget;
+		};
+
+		Response.Redirect(redirectTarget);
+	}
+}
