4676a0d22a120e1ed0126dba1c4ade9680386a55
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-05-13 13:01:30 +0200

    (Datalayer) adding SynchronizeFromStub

---
+++ DataLayer/DataLayer/MongoData/Option/Options/Logic/SynchronizeFromStub.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Bson;
+
+namespace DataLayer.MongoData.Option.Options.Logic
+{
+	public class SynchronizeFromStub : OptionBase
+	{
+		public ObjectId WebCampaignId { get; set; }
+
+		public byte[] WebCampaignIdValue()
+		{
+			return WebCampaignId.ToByteArray();
+		}
+
+		public static SynchronizeFromStub Create(MongoConnection mongoConnection, ObjectId webCampaignId, string name, Schedule schedule)
+		{
+			SynchronizeFromStub synchronizeFromStub = new SynchronizeFromStub()
+			{
+				WebCampaignId = webCampaignId,
+			};
+
+			Create(mongoConnection, synchronizeFromStub, name, schedule);
+
+			return synchronizeFromStub;
+		}
+
+		protected override void Execute(MongoConnection mongoConnection, bool recurring)
+		{
+			if (recurring)
+			{
+				Update<SynchronizeFromStub>(mongoConnection);
+			}
+			else
+			{
+				Delete<SynchronizeFromStub>(mongoConnection);
+			}
+		}
+	}
+}
