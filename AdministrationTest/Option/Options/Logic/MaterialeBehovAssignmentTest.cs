f2c591fa57ef0a49eee34dadc7f1b598d32d3bea
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-06-15 15:06:33 +0200

    (MaterialeBehovAssignmentTest) setting updateProgressFrequency

--- AdministrationTest/Option/Options/Logic/MaterialeBehovAssignmentTest.cs
+++ AdministrationTest/Option/Options/Logic/MaterialeBehovAssignmentTest.cs
@@ -14,6 +14,7 @@ namespace AdministrationTest.Option.Options.Logic
 			DatabaseMaterialeBehovAssignment databaseMaterialeBehovAssignment = new DatabaseMaterialeBehovAssignment()
 			{
 				urlLoginName = "test",
+				updateProgressFrequency = 1,
 				Schedule = CreateScheduleAlwaysOnDoOnce(),
 			};
 
