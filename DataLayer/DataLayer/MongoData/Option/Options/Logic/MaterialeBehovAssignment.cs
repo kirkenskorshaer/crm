c95a9be282689738ef88a60d414e1fd9fd4f965c
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-06-15 10:53:29 +0200

    (MaterialeBehovAssignment) added updateProgressFrequency

--- DataLayer/DataLayer/MongoData/Option/Options/Logic/MaterialeBehovAssignment.cs
+++ DataLayer/DataLayer/MongoData/Option/Options/Logic/MaterialeBehovAssignment.cs
@@ -3,6 +3,7 @@
 	public class MaterialeBehovAssignment : OptionBase
 	{
 		public string urlLoginName { get; set; }
+		public int updateProgressFrequency { get; set; }
 
 		public static MaterialeBehovAssignment Create(MongoConnection connection, string name, Schedule schedule, string urlLoginName)
 		{
