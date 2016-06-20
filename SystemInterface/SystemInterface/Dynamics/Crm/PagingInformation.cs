59b8ba1b40662f9900feb6f23d0b9e38c1a19043
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-06-20 13:58:05 +0200

    (SystemInterface) adding PagingInformation

---
+++ SystemInterface/SystemInterface/Dynamics/Crm/PagingInformation.cs
@@ -0,0 +1,17 @@
+﻿namespace SystemInterface.Dynamics.Crm
+{
+	public class PagingInformation
+	{
+		public bool MoreRecords;
+		public string PagingCookie;
+		public int Page = 1;
+		public bool FirstRun = true;
+
+		public void Reset()
+		{
+			Page = 1;
+			PagingCookie = string.Empty;
+			FirstRun = true;
+		}
+	}
+}
