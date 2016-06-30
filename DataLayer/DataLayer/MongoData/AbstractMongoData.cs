218b74704a1988b207e5b350a4acc81de2f980c5
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-06-30 14:31:09 +0200

    (DataLayer) refactor OptionBase to a common class AbstractMongoData

---
+++ DataLayer/DataLayer/MongoData/AbstractMongoData.cs
@@ -0,0 +1,96 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace DataLayer.MongoData
+{
+	[BsonIgnoreExtraElements]
+	public abstract class AbstractMongoData
+	{
+		public ObjectId _id { get; set; }
+
+		[BsonIgnore]
+		public string Id => _id.ToString();
+
+		protected static List<TDataType> ReadById<TDataType>(MongoConnection connection, ObjectId id)
+		where TDataType : AbstractMongoData
+		{
+			Expression<Func<TDataType, bool>> filter = data => data._id == id;
+
+			IMongoCollection<TDataType> datas = connection.Database.GetCollection<TDataType>(typeof(TDataType).Name);
+			IFindFluent<TDataType, TDataType> configFind = datas.Find(filter);
+			Task<List<TDataType>> dataTask = configFind.ToListAsync();
+
+			return dataTask.Result;
+		}
+
+		protected static TDataType ReadNextById<TDataType>(MongoConnection connection, ObjectId id)
+		where TDataType : AbstractMongoData
+		{
+			Expression<Func<TDataType, bool>> filter = data => data._id > id;
+
+			IMongoCollection<TDataType> datas = connection.Database.GetCollection<TDataType>(typeof(TDataType).Name);
+			IFindFluent<TDataType, TDataType> find = datas.Find(filter);
+			find.Limit(1);
+
+			SortDefinitionBuilder<TDataType> sortBuilder = new SortDefinitionBuilder<TDataType>();
+			SortDefinition<TDataType> sortDefinition = sortBuilder.Descending(document => document.Id);
+
+			find.Sort(sortDefinition);
+			Task<TDataType> dataTask = find.SingleOrDefaultAsync();
+
+			return dataTask.Result;
+		}
+
+		protected static TDataType First<TDataType>(MongoConnection connection)
+		where TDataType : AbstractMongoData
+		{
+			return ReadNextById<TDataType>(connection, ObjectId.Empty);
+		}
+
+		protected TDataType Next<TDataType>(MongoConnection connection)
+		where TDataType : AbstractMongoData
+		{
+			TDataType nextDocument = ReadNextById<TDataType>(connection, _id);
+
+			if (nextDocument == null)
+			{
+				nextDocument = First<TDataType>(connection);
+			}
+
+			return nextDocument;
+		}
+
+		protected static void Create<TDataType>(MongoConnection connection, TDataType data)
+		where TDataType : AbstractMongoData
+		{
+			data._id = ObjectId.GenerateNewId(DateTime.Now);
+			IMongoCollection<TDataType> datas = connection.Database.GetCollection<TDataType>(typeof(TDataType).Name);
+			Task insertTask = datas.InsertOneAsync(data);
+			MongoDataHelper.WaitForTaskOrThrowTimeout(insertTask);
+		}
+
+		protected void Update<TDataType>(MongoConnection connection)
+		where TDataType : AbstractMongoData
+		{
+			IMongoCollection<TDataType> datas = connection.Database.GetCollection<TDataType>(typeof(TDataType).Name);
+
+			FilterDefinition<TDataType> filter = Builders<TDataType>.Filter.Eq(data => data._id, _id);
+
+			Task<ReplaceOneResult> result = datas.ReplaceOneAsync(filter, (TDataType)this);
+			result.Wait();
+		}
+
+		protected void Delete<TDataType>(MongoConnection connection)
+		where TDataType : AbstractMongoData
+		{
+			IMongoCollection<TDataType> datas = connection.Database.GetCollection<TDataType>(typeof(TDataType).Name);
+			Task deleteTask = datas.DeleteOneAsync(data => data._id == _id);
+			deleteTask.Wait();
+		}
+	}
+}
--- DataLayer/DataLayer/MongoData/Option/OptionBase.cs
+++ DataLayer/DataLayer/MongoData/Option/OptionBase.cs
@@ -2,43 +2,22 @@
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
 
 namespace DataLayer.MongoData.Option
 {
-	public abstract class OptionBase
+	public abstract class OptionBase : AbstractMongoData
 	{
-		public ObjectId _id { get; set; }
 		public Schedule Schedule { get; set; }
 		public string Name { get; set; }
-
-		[BsonIgnore]
-		public string Id => _id.ToString();
 
 		protected static void Create<TOptionType>(MongoConnection connection, TOptionType option, string name, Schedule schedule)
 		where TOptionType : OptionBase
 		{
-			option._id = ObjectId.GenerateNewId(DateTime.Now);
 			option.Name = name;
 			option.Schedule = schedule;
 
-			IMongoCollection<TOptionType> options = connection.Database.GetCollection<TOptionType>(typeof(TOptionType).Name);
-			Task insertTask = options.InsertOneAsync(option);
-			insertTask.Wait();
-		}
-
-		protected static List<TOptionType> ReadById<TOptionType>(MongoConnection connection, ObjectId id)
-		where TOptionType : OptionBase
-		{
-			Expression<Func<TOptionType, bool>> filter = option => option._id == id;
-
-			IMongoCollection<TOptionType> options = connection.Database.GetCollection<TOptionType>(typeof(TOptionType).Name);
-			IFindFluent<TOptionType, TOptionType> configFind = options.Find(filter);
-			Task<List<TOptionType>> optionTask = configFind.ToListAsync();
-
-			return optionTask.Result;
+			Create(connection, option);
 		}
 
 		public static List<TOptionType> ReadAllowed<TOptionType>(MongoConnection connection)
@@ -53,25 +32,6 @@ namespace DataLayer.MongoData.Option
 			return optionTask.Result;
 		}
 
-		protected void Update<TOptionType>(MongoConnection connection)
-		where TOptionType : OptionBase
-		{
-			IMongoCollection<TOptionType> options = connection.Database.GetCollection<TOptionType>(typeof(TOptionType).Name);
-
-			FilterDefinition<TOptionType> filter = Builders<TOptionType>.Filter.Eq(option => option._id, _id);
-
-			Task<ReplaceOneResult> result = options.ReplaceOneAsync(filter, (TOptionType)this);
-			result.Wait();
-		}
-
-		protected void Delete<TOptionType>(MongoConnection connection)
-		where TOptionType : OptionBase
-		{
-			IMongoCollection<TOptionType> options = connection.Database.GetCollection<TOptionType>(typeof(TOptionType).Name);
-			Task deleteTask = options.DeleteOneAsync(option => option._id == _id);
-			deleteTask.Wait();
-		}
-
 		protected abstract void Execute(MongoConnection connection, bool recurring);
 
 		public void Execute(MongoConnection connection)
@@ -79,7 +39,7 @@ namespace DataLayer.MongoData.Option
 			if (Schedule.Recurring)
 			{
 				Schedule.MoveNext();
-				Execute(connection,true);
+				Execute(connection, true);
 			}
 			else
 			{
