a49ff8242c7306e4c835e9eb91757dda412d2731
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-06-30 14:46:41 +0200

    (StaticCrmTest) testing read and exists

--- SystemInterfaceTest/Dynamics/Crm/StaticCrmTest.cs
+++ SystemInterfaceTest/Dynamics/Crm/StaticCrmTest.cs
@@ -1,5 +1,7 @@
 ﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using SystemInterface.Dynamics.Crm;
 
 namespace SystemInterfaceTest.Dynamics.Crm
@@ -13,6 +15,67 @@ namespace SystemInterfaceTest.Dynamics.Crm
 			List<string> attributeNames = StaticCrm.GetAllAttributeNames(_dynamicsCrmConnection, typeof(Annotation));
 
 			Assert.Greater(attributeNames.Count, 3);
+		}
+
+		[Test]
+		public void ExistsReturnsTrueWhenContactExists()
+		{
+			Contact contact = CreateTestContact();
+			contact.emailaddress1 = $"email {Guid.NewGuid()}";
+			contact.firstname = $"firstname {Guid.NewGuid()}";
+			contact.Insert();
+
+			bool exists = StaticCrm.Exists<Contact>(_dynamicsCrmConnection, new Dictionary<string, string>()
+			{
+				{ "emailaddress1",contact.emailaddress1 },
+				{ "firstname",contact.firstname },
+			});
+
+			contact.Delete();
+
+			Assert.True(exists);
+		}
+
+		[Test]
+		public void ExistsReturnsFalseWhenContactDoesNotExists()
+		{
+			Contact contact = CreateTestContact();
+			contact.emailaddress1 = $"email {Guid.NewGuid()}";
+			contact.firstname = $"firstname {Guid.NewGuid()}";
+			contact.Insert();
+
+			bool exists = StaticCrm.Exists<Contact>(_dynamicsCrmConnection, new Dictionary<string, string>()
+			{
+				{ "emailaddress1",contact.emailaddress1 },
+				{ "firstname",$"firstname {Guid.NewGuid()}" },
+			});
+
+			contact.Delete();
+
+			Assert.False(exists);
+		}
+
+		[Test]
+		public void ReadFromFetchXml()
+		{
+			Contact contact = CreateTestContact();
+			contact.middlename = $"middlename {Guid.NewGuid()}";
+			contact.emailaddress1 = $"email {Guid.NewGuid()}";
+
+			contact.Insert();
+
+			List<string> fields = new List<string>() { "firstname", "emailaddress1" };
+			Dictionary<string, string> keyContent = new Dictionary<string, string>()
+			{
+				{ "middlename", contact.middlename },
+				{ "emailaddress1", contact.emailaddress1 }
+			};
+
+			Contact contactRead = Contact.ReadFromFetchXml(_dynamicsCrmConnection, fields, keyContent).Single();
+			contact.Delete();
+
+			Assert.AreEqual(contact.firstname, contactRead.firstname);
+			Assert.IsNull(contactRead.lastname);
 		}
 	}
 }
