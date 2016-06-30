271b80bdc27327a6ea6229fd78f9c072bc7b0331
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-06-30 14:43:47 +0200

    (DataLayer) adding importFromStub and CreateImportFromStub

---
+++ DataLayer/DataLayer/MongoData/Option/Options/Logic/CreateImportFromStub.cs
@@ -0,0 +1,33 @@
+﻿namespace DataLayer.MongoData.Option.Options.Logic
+{
+	public class CreateImportFromStub : OptionBase
+	{
+		public string urlLoginName { get; set; }
+		public Schedule ImportFromStubSchedule { get; set; }
+
+		public static CreateImportFromStub Create(MongoConnection mongoConnection, string urlLoginName, string name, Schedule schedule, Schedule importFromStubSchedule)
+		{
+			CreateImportFromStub createImportFromStub = new CreateImportFromStub()
+			{
+				urlLoginName = urlLoginName,
+				ImportFromStubSchedule = importFromStubSchedule,
+			};
+
+			Create(mongoConnection, createImportFromStub, name, schedule);
+
+			return createImportFromStub;
+		}
+
+		protected override void Execute(MongoConnection connection, bool recurring)
+		{
+			if (recurring)
+			{
+				Update<CreateImportFromStub>(connection);
+			}
+			else
+			{
+				Delete<CreateImportFromStub>(connection);
+			}
+		}
+	}
+}
---
+++ DataLayer/DataLayer/MongoData/Option/Options/Logic/ImportFromStub.cs
@@ -0,0 +1,62 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using DataLayer.MongoData.Input;
+
+namespace DataLayer.MongoData.Option.Options.Logic
+{
+	public class ImportFromStub : OptionBase
+	{
+		public string urlLoginName { get; set; }
+		public ObjectId WebCampaignId { get; set; }
+
+		public static ImportFromStub Create(MongoConnection mongoConnection, ObjectId webCampaignId, string urlLoginName, string name, Schedule schedule)
+		{
+			ImportFromStub importFromStub = new ImportFromStub()
+			{
+				WebCampaignId = webCampaignId,
+				urlLoginName = urlLoginName,
+			};
+
+			Create(mongoConnection, importFromStub, name, schedule);
+
+			return importFromStub;
+		}
+
+		protected override void Execute(MongoConnection connection, bool recurring)
+		{
+			if (recurring)
+			{
+				Update<ImportFromStub>(connection);
+			}
+			else
+			{
+				Delete<ImportFromStub>(connection);
+			}
+		}
+
+		public static List<ImportFromStub> ReadByWebCampaign(MongoConnection connection, WebCampaign webCampaign)
+		{
+			return ReadByWebCampaignId(connection, webCampaign._id);
+		}
+
+		private static List<ImportFromStub> ReadByWebCampaignId(MongoConnection connection, ObjectId WebCampaignId)
+		{
+			Expression<Func<ImportFromStub, bool>> filter = option => option.WebCampaignId == WebCampaignId;
+
+			IMongoCollection<ImportFromStub> options = connection.Database.GetCollection<ImportFromStub>(typeof(ImportFromStub).Name);
+			IFindFluent<ImportFromStub, ImportFromStub> configFind = options.Find(filter);
+			Task<List<ImportFromStub>> optionTask = configFind.ToListAsync();
+
+			return optionTask.Result;
+		}
+
+		public byte[] WebCampaignIdValue()
+		{
+			return WebCampaignId.ToByteArray();
+		}
+	}
+}
