bdb10be4114fef15fca4e54eba4a046dbf70b6b3
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-06-30 14:45:57 +0200

    (SystemInterfaceTest) testing campaign

---
+++ SystemInterfaceTest/Dynamics/Crm/CampaignTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using SystemInterface.Dynamics.Crm;
+
+namespace SystemInterfaceTest.Dynamics.Crm
+{
+	[TestFixture]
+	public class CampaignTest : TestBase
+	{
+		[Test]
+		public void ThereAreCampaigns()
+		{
+			List<Campaign> campaigns = Campaign.ReadAllCampaignIds(_dynamicsCrmConnection);
+
+			Assert.Greater(campaigns.Count, 0);
+		}
+	}
+}
