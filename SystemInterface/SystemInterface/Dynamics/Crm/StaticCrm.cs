a005556b63bfe8083b5fffd33e596e2adc1fb33c
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-06-30 14:00:35 +0200

    (StaticCrm) removed unused usings

--- SystemInterface/SystemInterface/Dynamics/Crm/StaticCrm.cs
+++ SystemInterface/SystemInterface/Dynamics/Crm/StaticCrm.cs
@@ -5,8 +5,6 @@ using Microsoft.Xrm.Sdk.Query;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Linq;
 
 namespace SystemInterface.Dynamics.Crm
