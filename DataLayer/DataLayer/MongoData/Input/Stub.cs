22104ff59d839a7eac75513d6eb0d24d76d825aa
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-01 13:15:50 +0200

    (DataLayer) adding dataclasses for web stub

---
+++ DataLayer/DataLayer/MongoData/Input/Stub.cs
@@ -0,0 +1,65 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace DataLayer.MongoData.Input
+{
+	public class Stub : AbstractMongoData
+	{
+		public List<StubElement> Contents = new List<StubElement>();
+		[BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+		public DateTime PostTime;
+		public ObjectId WebCampaignId;
+		public int ImportAttempt = 0;
+
+		public void Push(MongoConnection connection)
+		{
+			Create(connection, this);
+		}
+
+		public static Stub ReadFirst(MongoConnection connection)
+		{
+			SortDefinition<Stub> sortDefinition = Builders<Stub>.Sort.Ascending("ImportAttempt").Ascending("PostTime");
+
+			BsonDocument filter = new BsonDocument();
+
+			IMongoCollection<Stub> stubCollection = connection.Database.GetCollection<Stub>(typeof(Stub).Name);
+			IFindFluent<Stub, Stub> stubFind = stubCollection.Find(filter).Sort(sortDefinition).Limit(1);
+
+			Task<Stub> stubTask = stubFind.FirstOrDefaultAsync();
+			return MongoDataHelper.GetValueOrThrowTimeout(stubTask);
+		}
+
+		public static Stub ReadFirst(MongoConnection connection, WebCampaign webCampaign)
+		{
+			SortDefinition<Stub> sortDefinition = Builders<Stub>.Sort.Ascending("ImportAttempt").Ascending("PostTime");
+
+			Expression<Func<Stub, bool>> filter = option => option.WebCampaignId == webCampaign._id;
+
+			IMongoCollection<Stub> stubCollection = connection.Database.GetCollection<Stub>(typeof(Stub).Name);
+			IFindFluent<Stub, Stub> stubFind = stubCollection.Find(filter).Sort(sortDefinition).Limit(1);
+
+			Task<Stub> stubTask = stubFind.FirstOrDefaultAsync();
+			return MongoDataHelper.GetValueOrThrowTimeout(stubTask);
+		}
+
+		public static long Count(MongoConnection connection)
+		{
+			return Count<Stub>(connection);
+		}
+
+		public void Delete(MongoConnection connection)
+		{
+			Delete<Stub>(connection);
+		}
+
+		public void Update(MongoConnection connection)
+		{
+			Update<Stub>(connection);
+		}
+	}
+}
---
+++ DataLayer/DataLayer/MongoData/Input/WebCampaign.cs
@@ -0,0 +1,95 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataLayer.MongoData.Input
+{
+	public class WebCampaign : AbstractMongoData
+	{
+		[BsonRepresentation(BsonType.String)]
+		public Guid FormId;
+		[BsonRepresentation(BsonType.String)]
+		public Guid FormOwner;
+		public string RedirectTarget;
+		public List<string> KeyFields;
+		public CollectTypeEnum CollectType;
+
+		public enum CollectTypeEnum
+		{
+			Lead = 1,
+			LeadOgContactHvisContactIkkeFindes = 2,
+		}
+
+		public void Insert(MongoConnection connection)
+		{
+			Create(connection, this);
+		}
+
+		public static WebCampaign ReadSingleOrDefault(MongoConnection connection, Guid formId)
+		{
+			IMongoCollection<WebCampaign> WebCampaignCollection = connection.Database.GetCollection<WebCampaign>(typeof(WebCampaign).Name);
+			IFindFluent<WebCampaign, WebCampaign> WebCampaignFind = WebCampaignCollection.Find(WebCampaign =>
+				WebCampaign.FormId == formId);
+
+			Task<List<WebCampaign>> WebCampaignTask = WebCampaignFind.ToListAsync();
+
+			WebCampaignTask.Wait();
+
+			List<WebCampaign> WebCampaignFound = WebCampaignTask.Result;
+
+			return WebCampaignFound.SingleOrDefault();
+		}
+
+		public static WebCampaign ReadByIdBytesSingleOrDefault(MongoConnection connection, byte[] id)
+		{
+			ObjectId searchId = new ObjectId(id);
+			return ReadByIdSingleOrDefault(connection, searchId);
+		}
+
+		public static WebCampaign ReadByIdSingleOrDefault(MongoConnection connection, ObjectId id)
+		{
+			IMongoCollection<WebCampaign> WebCampaignCollection = connection.Database.GetCollection<WebCampaign>(typeof(WebCampaign).Name);
+			IFindFluent<WebCampaign, WebCampaign> WebCampaignFind = WebCampaignCollection.Find(WebCampaign =>
+				WebCampaign._id == id);
+
+			Task<List<WebCampaign>> WebCampaignTask = WebCampaignFind.ToListAsync();
+
+			WebCampaignTask.Wait();
+
+			List<WebCampaign> WebCampaignFound = WebCampaignTask.Result;
+
+			return WebCampaignFound.SingleOrDefault();
+		}
+
+		public void Update(MongoConnection connection)
+		{
+			Update<WebCampaign>(connection);
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (obj == null)
+			{
+				return false;
+			}
+
+			WebCampaign compareWebCampaign = obj as WebCampaign;
+
+			return
+				CollectType == compareWebCampaign.CollectType &&
+				FormId == compareWebCampaign.FormId &&
+				FormOwner == compareWebCampaign.FormOwner &&
+				Utilities.Comparer.ListCompare.ListEquals(KeyFields, compareWebCampaign.KeyFields, (a, b) => a.Equals(b)) &&
+				RedirectTarget == compareWebCampaign.RedirectTarget;
+		}
+
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
+	}
+}
