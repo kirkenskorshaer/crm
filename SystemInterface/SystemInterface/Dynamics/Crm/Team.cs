cdb215b5bfc20e9a1ee29a0f87a7d213336f31fc
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-07 13:50:43 +0200

    (SystemInterface) adding Team

---
+++ SystemInterface/SystemInterface/Dynamics/Crm/Team.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xrm.Client;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace SystemInterface.Dynamics.Crm
+{
+	public class Team : AbstractCrm
+	{
+		private static readonly ColumnSet ColumnSetTeam = new ColumnSet(
+			"teamid");
+
+		private static readonly ColumnSet ColumnSetTeamCrmGenerated = new ColumnSet("createdon", "modifiedon");
+
+		public Team(DynamicsCrmConnection connection) : base(connection)
+		{
+		}
+
+		public Team(DynamicsCrmConnection connection, Entity crmEntity) : base(connection, crmEntity)
+		{
+		}
+
+		protected override ColumnSet ColumnSetCrmGenerated { get { return ColumnSetTeamCrmGenerated; } }
+
+		protected override string entityName { get { return "team"; } }
+		protected override string idName { get { return "teamid"; } }
+
+		protected override CrmEntity GetAsEntity(bool includeContactId)
+		{
+			CrmEntity crmEntity = new CrmEntity("team");
+
+			if (includeContactId)
+			{
+				crmEntity.Attributes.Add(new KeyValuePair<string, object>(idName, Id));
+				crmEntity.Id = Id;
+			}
+
+			return crmEntity;
+		}
+
+		public static List<Team> ReadFromFetchXml(DynamicsCrmConnection dynamicsCrmConnection, List<string> fields, Dictionary<string, string> keyContent)
+		{
+			return StaticCrm.ReadFromFetchXml(dynamicsCrmConnection, fields, keyContent, null, (connection, contactEntity) => new Team(connection, contactEntity), new PagingInformation());
+		}
+	}
+}
