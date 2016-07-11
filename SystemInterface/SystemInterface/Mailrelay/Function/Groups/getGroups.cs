b35a851c57bb17a44d0444d4aa514ef12c015668
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-11 14:37:55 +0200

    (SystemInterface) adding getGroups

---
+++ SystemInterface/SystemInterface/Mailrelay/Function/Groups/getGroups.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using SystemInterface.Mailrelay.FunctionReply;
+
+namespace SystemInterface.Mailrelay.Function.Groups
+{
+	public class getGroups : AbstractFunction
+	{
+		public override Type ReturnType { get { return typeof(MailrelayArrayReply<getGroupsReply>); } }
+
+		/// <summery>Optional parameter to specify offset.</summery>
+		public int? offset;
+
+		/// <summery>Optional parameter to specify the number of records.</summery>
+		public int? count;
+
+		/// <summery>Optional parameter to search by id.</summery>
+		public int? id;
+
+		/// <summery>Optional parameter to search by name.</summery>
+		public string name;
+
+		/// <summery>Optional parameter to search by description.</summery>
+		public string description;
+
+		/// <summery>Optional parameter to search by position.</summery>
+		public int? position;
+
+		/// <summery>Optional parameter to search if group is enabled.</summery>
+		public bool? enable;
+
+		/// <summery>Optional parameter to search if group is visible.</summery>
+		public bool? visible;
+
+		/// <summery>Optional parameter to specify a search field.</summery>
+		public string sortField;
+
+		/// <summery>Optional parameter to specify a sort order. Accepted values are ASC and DESC (default).</summery>
+		public sortOrderEnum? sortOrder;
+	}
+}
---
+++ SystemInterface/SystemInterface/Mailrelay/Function/Groups/getGroupsReply.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace SystemInterface.Mailrelay.Function.Groups
+{
+	public class getGroupsReply
+	{
+		public int id;
+		public int position;
+		public string name;
+		public string description;
+		public string enable;
+		public string visible;
+		[JsonProperty(PropertyName = "internal")]
+		public string internalProperty;
+	}
+}
