0195a24fbe0c4eb641c295cab769e055003ecbb9
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-13 12:49:20 +0200

    (SystemInterface) adding addGroup

---
+++ SystemInterface/SystemInterface/Mailrelay/Function/Groups/addGroup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SystemInterface.Mailrelay.FunctionReply;
+
+namespace SystemInterface.Mailrelay.Function.Groups
+{
+	public class addGroup : AbstractFunction
+	{
+		public override Type ReturnType { get { return typeof(MailrelayIntReply); } }
+
+		///<summery>Name.</summery>
+		public string name;
+
+		///<summery>Description.</summery>
+		public string description;
+
+		///<summery>Position of the group on listing.</summery>
+		public int position;
+
+		///<summery>True if group is enabled.</summery>
+		public bool enable;
+
+		///<summery>True if group is visible.</summery>
+		public bool visible;
+	}
+}
