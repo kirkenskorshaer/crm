be3a8bb6ec2ce2922bc5dd43331c3062d0b6e4a1
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-13 14:11:38 +0200

    (SystemInterface) adding getStats

---
+++ SystemInterface/SystemInterface/Mailrelay/Function/Statistics/getStats.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SystemInterface.Mailrelay.FunctionReply;
+
+namespace SystemInterface.Mailrelay.Function.Statistics
+{
+	public class getStats : AbstractFunction
+	{
+		public override Type ReturnType { get { return typeof(MailrelayObjectReply<getStatsReply>); } }
+
+		/// <summary>Optional parameter if you want to get stats for a single mailing list.If not set, will return for all mailing lists.</summary>
+		public int? id;
+
+		/// <summary>Optional parameter for start date.Must be in the following format: YYYY-MM-DD HH:MM:SS.</summary>
+		public DateTime? startDate;
+
+		/// <summary>Optional parameter for end date.Must be in the following format: YYYY-MM-DD HH:MM:SS.</summary>
+		public DateTime? endDate;
+	}
+}
---
+++ SystemInterface/SystemInterface/Mailrelay/Function/Statistics/getStatsReply.cs
@@ -0,0 +1,16 @@
+﻿namespace SystemInterface.Mailrelay.Function.Statistics
+{
+	public class getStatsReply
+	{
+		public int impressions;
+		public bool unique_impressions;
+		public int clicks;
+		public int unique_clicks;
+		public int sent;
+		public int bounced;
+		public int reported_spam;
+		public int optouts;
+		public int forwarded;
+		public int delivered;
+	}
+}
