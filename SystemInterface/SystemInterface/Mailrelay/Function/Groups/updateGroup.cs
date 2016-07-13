420a1997d2787ca718411f36bd18a546a2d49bae
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-13 13:49:11 +0200

    (SystemInterface) adding updateGroup

---
+++ SystemInterface/SystemInterface/Mailrelay/Function/Groups/updateGroup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SystemInterface.Mailrelay.FunctionReply;
+
+namespace SystemInterface.Mailrelay.Function.Groups
+{
+	public class updateGroup : AbstractFunction
+	{
+		public override Type ReturnType { get { return typeof(MailrelayBoolReply); } }
+
+		///<summery>Id of the group that you want to update.</summery>
+		public int id;
+
+		///<summery>Name.</summery>
+		public string name;
+
+		///<summery>Description.</summery>
+		public string description;
+
+		///<summery>Position of the group on listing.</summery>
+		public int position;
+
+		///<summery>True if group is enabled.</summery>
+		public bool enable;
+
+		///<summery>True if group is visible.</summery>
+		public bool visible;
+	}
+}
