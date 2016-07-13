b9e37f3ed262a08d6997abe689198819fae32252
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-13 14:08:40 +0200

    (SystemInterface) adding mailrelay connection classes

---
+++ SystemInterface/SystemInterface/Mailrelay/Function/AbstractFunction.cs
@@ -0,0 +1,228 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Web;
+using SystemInterface.Mailrelay.Function;
+using SystemInterface.Mailrelay.FunctionReply;
+
+namespace SystemInterface.Mailrelay
+{
+	public abstract class AbstractFunction
+	{
+		public string apiKey;
+
+		[ToGet(type = ToGetAttribute.typeEnum.ignore)]
+		public abstract Type ReturnType { get; }
+
+		public string ToGet()
+		{
+			StringBuilder objectAsGetStringBuilder = new StringBuilder();
+
+			MemberInfo[] allMembers = GetType().GetMembers();
+			List<MemberInfo> members = allMembers.Where(member => member.MemberType == MemberTypes.Field || member.MemberType == MemberTypes.Property).ToList();
+
+			string function = GetType().Name;
+
+			AddUrlEncoded(objectAsGetStringBuilder, "function", function);
+
+			foreach (MemberInfo member in members)
+			{
+				ToGetAttribute toGetAttribute = (ToGetAttribute)member.GetCustomAttributes(typeof(ToGetAttribute)).SingleOrDefault();
+
+				if (toGetAttribute != null && toGetAttribute.type == ToGetAttribute.typeEnum.ignore)
+				{
+					continue;
+				}
+
+				string getName = FindGetName(member, toGetAttribute);
+
+				object value = Utilities.ReflectionHelper.GetValue(this, member);
+
+				if (value == null)
+				{
+					continue;
+				}
+
+				if (AddCollectionType(getName, value, objectAsGetStringBuilder))
+				{
+					continue;
+				}
+
+				string valueString = FindGetValue(value, toGetAttribute);
+
+				if (string.IsNullOrWhiteSpace(valueString))
+				{
+					continue;
+				}
+
+				AddUrlEncoded(objectAsGetStringBuilder, getName, valueString);
+			}
+
+			return objectAsGetStringBuilder.ToString();
+		}
+
+		private bool AddCollectionType(string getName, object value, StringBuilder objectAsGetStringBuilder)
+		{
+			if (value is IEnumerable<int>)
+			{
+				AddIEnumerableInt(value, getName, objectAsGetStringBuilder);
+				return true;
+			}
+
+			if (value is IEnumerable<string>)
+			{
+				AddIEnumerableString(value, getName, objectAsGetStringBuilder);
+				return true;
+			}
+
+			if (value.GetType() == typeof(Dictionary<string, string>))
+			{
+				AddDictionary(value, getName, objectAsGetStringBuilder);
+				return true;
+			}
+
+			return false;
+		}
+
+		private void AddDictionary(object value, string getName, StringBuilder objectAsGetStringBuilder)
+		{
+			Dictionary<string, string> valueDictionary = value as Dictionary<string, string>;
+
+			foreach (KeyValuePair<string, string> keyValuePair in valueDictionary)
+			{
+				string currentName = $"{getName}[{keyValuePair.Key}]";
+				AddUrlEncoded(objectAsGetStringBuilder, currentName, $"{keyValuePair.Value}");
+			}
+		}
+
+		private void AddIEnumerableInt(object value, string getName, StringBuilder objectAsGetStringBuilder)
+		{
+			IEnumerable<int> valueIEnumerable = value as IEnumerable<int>;
+
+			if (valueIEnumerable == null)
+			{
+				return;
+			}
+
+			int index = 0;
+			foreach (int currentInt in valueIEnumerable)
+			{
+				string currentName = $"{getName}[{index}]";
+				AddUrlEncoded(objectAsGetStringBuilder, currentName, currentInt.ToString());
+				index++;
+			}
+		}
+
+		private void AddIEnumerableString(object value, string getName, StringBuilder objectAsGetStringBuilder)
+		{
+			IEnumerable<string> valueIEnumerable = value as IEnumerable<string>;
+
+			if (valueIEnumerable == null)
+			{
+				return;
+			}
+
+			int index = 0;
+			foreach (string currentString in valueIEnumerable)
+			{
+				string currentName = $"{getName}[{index}]";
+				AddUrlEncoded(objectAsGetStringBuilder, currentName, $"{currentString}");
+				index++;
+			}
+		}
+
+		private string FindGetName(MemberInfo member, ToGetAttribute toGetAttribute)
+		{
+			if (toGetAttribute == null)
+			{
+				return member.Name;
+			}
+
+			if (string.IsNullOrWhiteSpace(toGetAttribute.name))
+			{
+				return member.Name;
+			}
+
+			return toGetAttribute.name;
+		}
+
+		private string FindGetValue(object value, ToGetAttribute toGetAttribute)
+		{
+			if (value == null)
+			{
+				return null;
+			}
+
+			if (toGetAttribute == null)
+			{
+				return ValueToString(value);
+			}
+
+			if (toGetAttribute.type == ToGetAttribute.typeEnum.intEnum)
+			{
+				return ((int)value).ToString();
+			}
+
+			if (toGetAttribute.type == ToGetAttribute.typeEnum.ShortDate)
+			{
+				return ((DateTime)value).ToString("yyyy-MM-dd");
+			}
+
+			return ValueToString(value);
+		}
+
+		private string ValueToString(object value)
+		{
+			string valueString;
+
+			Type valueType = value.GetType();
+
+			if (valueType == typeof(DateTime))
+			{
+				valueString = ((DateTime)value).ToString("yyyy-MM-dd HH:mm:ss");
+			}
+			else if(valueType == typeof(bool) || valueType == typeof(bool?))
+			{
+				if((bool?)value == true)
+				{
+					valueString = "1";
+				}
+				else
+				{
+					valueString = "0";
+				}
+			}
+			else
+			{
+				valueString = value.ToString();
+			}
+
+			return valueString;
+		}
+
+		private void AddUrlEncoded(StringBuilder objectAsGetStringBuilder, string name, string valueString)
+		{
+			objectAsGetStringBuilder.Append("&");
+			objectAsGetStringBuilder.Append(HttpUtility.UrlEncode(name));
+			objectAsGetStringBuilder.Append("=");
+			objectAsGetStringBuilder.Append(HttpUtility.UrlEncode(valueString));
+		}
+
+		public AbstractMailrelayReply GetMailrelayReply(string replyString)
+		{
+			AbstractMailrelayReply reply = JsonConvert.DeserializeObject(replyString, ReturnType, new DateTimeConverter()) as AbstractMailrelayReply;
+
+			return reply;
+		}
+
+		public enum sortOrderEnum
+		{
+			ASC = 1,
+			DESC = 2,
+		}
+	}
+}
---
+++ SystemInterface/SystemInterface/Mailrelay/MailrelayConnection.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Net;
+using SystemInterface.Mailrelay.FunctionReply;
+
+namespace SystemInterface.Mailrelay
+{
+	public class MailrelayConnection
+	{
+		private string mailrelayUrl;
+		private string mailrelayApiKey;
+
+		public MailrelayConnection(string url, string apiKey)
+		{
+			mailrelayUrl = url;
+			mailrelayApiKey = apiKey;
+		}
+
+		public AbstractMailrelayReply Send(AbstractFunction functionToSend)
+		{
+			functionToSend.apiKey = mailrelayApiKey;
+			string target = mailrelayUrl + functionToSend.ToGet();
+
+			HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(target);
+			httpWebRequest.ContentType = "text/xml; encoding='utf-8'";
+			httpWebRequest.Method = "GET";
+			HttpWebResponse httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+			string reply;
+			using (StreamReader streamReader = new StreamReader(httpResponse.GetResponseStream()))
+			{
+				reply = streamReader.ReadToEnd();
+			}
+
+			return functionToSend.GetMailrelayReply(reply);
+		}
+	}
+}
