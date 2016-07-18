5aa365467dca020440c5b11d9844e83424300561
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-18 10:08:10 +0200

    (SystemInterfaceTest) testing that an entity can now be created from invalid data

---
+++ SystemInterfaceTest/Dynamics/Crm/AbstractCrmTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using SystemInterface.Dynamics.Crm;
+
+namespace SystemInterfaceTest.Dynamics.Crm
+{
+	[TestFixture]
+	public class AbstractCrmTest : TestBase
+	{
+		[Test]
+		public void CreateFromContentCanCreateFromInvalidContent()
+		{
+			string firstname = $"firstname {Guid.NewGuid()}";
+
+			Dictionary<string, string> contactDefinition = new Dictionary<string, string>()
+			{
+				{ "contactid", "test" },
+				{ "nonExistingFieldOrPropertyName", "test" },
+				{ "firstname", firstname },
+			};
+
+			Contact contact = Contact.Create(_dynamicsCrmConnection, contactDefinition);
+
+			Assert.AreEqual(firstname, contact.firstname);
+		}
+	}
+}
