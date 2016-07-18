71763ed83380833a91c0930fe7463643ca1c2e04
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-18 14:36:38 +0200

    (SystemInterface) adding ToGetAttribute

---
+++ SystemInterface/SystemInterface/Mailrelay/Function/ToGetAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SystemInterface.Mailrelay.Function
+{
+	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, Inherited = true)]
+	public class ToGetAttribute : Attribute
+	{
+		public string name { get; set; }
+		public typeEnum type { get; set; }
+
+		public enum typeEnum
+		{
+			defaultType = 1,
+			intEnum = 2,
+			ignore = 3,
+			ShortDate = 4,
+		}
+	}
+}
