0801bec1ca4ef1e2d4af05c14ecee538327b567b
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-18 08:19:27 +0200

    (Datalayer) adding ExposeData option

---
+++ DataLayer/DataLayer/MongoData/Option/Options/Logic/ExposeData.cs
@@ -0,0 +1,34 @@
+﻿namespace DataLayer.MongoData.Option.Options.Logic
+{
+	public class ExposeData : OptionBase
+	{
+		public string urlLoginName { get; set; }
+		public string fetchXmlPath { get; set; }
+		public string exposePath { get; set; }
+		public string exposeName { get; set; }
+
+		public static ExposeData Create(MongoConnection mongoConnection, string urlLoginName, string name, Schedule schedule, Schedule importFromStubSchedule)
+		{
+			ExposeData exposeIndsamlingssteder = new ExposeData()
+			{
+				urlLoginName = urlLoginName,
+			};
+
+			Create(mongoConnection, exposeIndsamlingssteder, name, schedule);
+
+			return exposeIndsamlingssteder;
+		}
+
+		protected override void Execute(MongoConnection connection, bool recurring)
+		{
+			if (recurring)
+			{
+				Update<ExposeData>(connection);
+			}
+			else
+			{
+				Delete<ExposeData>(connection);
+			}
+		}
+	}
+}
