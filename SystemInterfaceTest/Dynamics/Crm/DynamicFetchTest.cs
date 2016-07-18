fea7199302bcd3219dd7ec5fa1c7e7f55b1d2ad7
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-18 08:26:21 +0200

    (SystemInterfaceTest) testing dynamicFetch

---
+++ SystemInterfaceTest/Dynamics/Crm/DynamicFetchTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SystemInterface.Dynamics.Crm;
+
+namespace SystemInterfaceTest.Dynamics.Crm
+{
+	[TestFixture]
+	public class DynamicFetchTest : TestBase
+	{
+		[Test]
+		public void DynamicFetchReturnsData()
+		{
+			List<dynamic> dynamicEntities = DynamicFetch.ReadFromFetchXml(_dynamicsCrmConnection, "contact", new List<string>() { "contactid" }, new Dictionary<string, string>(), 1, new PagingInformation());
+
+			Assert.AreNotEqual(Guid.Empty, (Guid)dynamicEntities.Single().contactid);
+		}
+	}
+}
