2db1a4ce5f098645e81ab6f9006a4de8a7a00ba9
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-18 08:28:02 +0200

    (AdministrationTest) testing ExposeData

---
+++ AdministrationTest/Option/Options/Logic/ExposeDataTest.cs
@@ -0,0 +1,29 @@
+﻿using Administration.Option.Options.Logic;
+using NUnit.Framework;
+using DatabaseExposeData = DataLayer.MongoData.Option.Options.Logic.ExposeData;
+
+namespace AdministrationTest.Option.Options.Logic
+{
+	[TestFixture]
+	public class ExposeDataTest : TestBase
+	{
+		[Test]
+		[Ignore]
+		public void ExposeDataExposesDataInFile()
+		{
+			DatabaseExposeData databaseExposeData = new DatabaseExposeData()
+			{
+				Name = "test",
+				Schedule = CreateScheduleAlwaysOnDoOnce(),
+				urlLoginName = "test",
+				fetchXmlPath = "Dynamics/Crm/FetchXml/Account/plant.xml",
+				exposePath = "webinterface",
+				exposeName = "test.txt",
+			};
+
+			ExposeData exposeData = new ExposeData(Connection, databaseExposeData);
+
+			exposeData.Execute();
+		}
+	}
+}
