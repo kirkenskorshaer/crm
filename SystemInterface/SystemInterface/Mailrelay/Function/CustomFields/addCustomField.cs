a54e9349709c1579e5dd2dc1f54394ca6875dc39
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-18 14:39:41 +0200

    (SystemInterface) adding addCustomField

---
+++ SystemInterface/SystemInterface/Mailrelay/Function/CustomFields/addCustomField.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using SystemInterface.Mailrelay.FunctionReply;
+
+namespace SystemInterface.Mailrelay.Function.CustomFields
+{
+	public class addCustomField : AbstractCustomField
+	{
+		public override Type ReturnType { get { return typeof(MailrelayIntReply); } }
+
+		///<summery>Name.</summery>
+		public string name;
+
+		///<summery>Field type.</summery>
+		[ToGet(type = ToGetAttribute.typeEnum.intEnum)]
+		public CustomFieldTypeEnum fieldType;
+
+		///<summery>Position of the custom field on listing.</summery>
+		public int position;
+
+		///<summery>Default field value.</summery>
+		[ToGet(name = "default")]
+		public string defaultField;
+
+		///<summery>True if field is enabled.</summery>
+		public bool enable;
+	}
+}
