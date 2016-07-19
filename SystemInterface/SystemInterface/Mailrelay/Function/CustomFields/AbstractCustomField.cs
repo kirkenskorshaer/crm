9be16d328e0c0a3825428983357ee6259d83b27f
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-19 11:10:37 +0200

    (SystemInterface) adding abstract for CustomFields

---
+++ SystemInterface/SystemInterface/Mailrelay/Function/CustomFields/AbstractCustomField.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SystemInterface.Mailrelay.Function.CustomFields
+{
+	public abstract class AbstractCustomField : AbstractFunction
+	{
+		public abstract override Type ReturnType { get; }
+
+		public enum CustomFieldTypeEnum
+		{
+			TextField = 1,
+			PasswordField = 2,
+			Textarea = 3,
+			SelectField = 4,
+			Checkbox = 5,
+			RadioButton = 6,
+			Date = 7,
+			BIrthday = 8,
+		}
+	}
+}
