ebd622764158bdb3d8dd0e633a7cd87bf316387e
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-19 12:00:14 +0200

    (SystemInterface) adding deleteCustomField

---
+++ SystemInterface/SystemInterface/Mailrelay/Function/CustomFields/deleteCustomField.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SystemInterface.Mailrelay.FunctionReply;
+
+namespace SystemInterface.Mailrelay.Function.CustomFields
+{
+	public class deleteCustomField : AbstractCustomField
+	{
+		public override Type ReturnType { get { return typeof(MailrelayBoolReply); } }
+
+		/// <summary> Id of the custom field that you want to delete.</summary>
+		public int id;
+	}
+}
