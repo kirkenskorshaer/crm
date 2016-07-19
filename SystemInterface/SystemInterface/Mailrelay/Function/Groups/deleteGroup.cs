c252a14266be6903952d05e6b0f4f6bf1a8deed3
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-19 11:43:28 +0200

    (SystemInterface) adding deleteGroup

---
+++ SystemInterface/SystemInterface/Mailrelay/Function/Groups/deleteGroup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SystemInterface.Mailrelay.FunctionReply;
+
+namespace SystemInterface.Mailrelay.Function.Groups
+{
+	public class deleteGroup : AbstractFunction
+	{
+		public override Type ReturnType { get { return typeof(MailrelayBoolReply); } }
+
+		/// <summary>Id of the group that you want to delete.</summary>
+		public int id;
+	}
+}
