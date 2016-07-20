3ba387fe7a9af47f96401fb4f322ec1ad6a370af
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-20 14:45:19 +0200

    (SystemInterface) adding addSubscriber

---
+++ SystemInterface/SystemInterface/Mailrelay/Function/Subscribers/addSubscriber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using SystemInterface.Mailrelay.FunctionReply;
+
+namespace SystemInterface.Mailrelay.Function.Subscribers
+{
+	public class addSubscriber : AbstractFunction
+	{
+		public override Type ReturnType { get { return typeof(MailrelayIntReply); } }
+
+		/// <summary>Email.</summary>
+		public string email;
+
+		/// <summary>Name.</summary>
+		public string name;
+
+		/// <summary>Array with the IDs of the groups that this subscriber belongs.
+		/// Example for groups 10 and 14:
+		/// array( 10, 14 );</summary>
+		public List<int> groups;
+
+		/// <summary>Optional parameter for custom fields. Use f_FieldId as key.
+		/// Example with two fields:
+		/// array( 'f_1' => 'Madrid', 'f_2' => '5555-5555' )</summary>
+		public Dictionary<string, string> customFields;
+	}
+}
