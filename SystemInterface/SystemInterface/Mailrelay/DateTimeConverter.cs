6a85ca834fc3f66cc9c89d64c0bf895211a19738
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-20 14:42:31 +0200

    (SystemInterface) adding DateTimeConverter so invalid datetimes will be handled as null

---
+++ SystemInterface/SystemInterface/Mailrelay/DateTimeConverter.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json.Converters;
+using System;
+using Newtonsoft.Json;
+
+namespace SystemInterface.Mailrelay
+{
+	public class DateTimeConverter : DateTimeConverterBase
+	{
+		private const string format = "yyyy-MM-dd hh:mm:ss";
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			DateTime parsedDateTime;
+
+			if (reader.Value == null)
+			{
+				return null;
+			}
+
+			if (DateTime.TryParse(reader.Value.ToString(), out parsedDateTime))
+			{
+				return parsedDateTime;
+			}
+			return existingValue;
+		}
+
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			writer.WriteValue(((DateTime)value).ToString(format));
+		}
+	}
+}
