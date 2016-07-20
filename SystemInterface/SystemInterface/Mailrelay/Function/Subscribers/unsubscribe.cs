5ffcc659664191dcf029ea0cb4a48b9178e69345
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-20 14:46:54 +0200

    (SystemInterface) adding unsubscribe

---
+++ SystemInterface/SystemInterface/Mailrelay/Function/Subscribers/unsubscribe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SystemInterface.Mailrelay.FunctionReply;
+
+namespace SystemInterface.Mailrelay.Function.Subscribers
+{
+	public class unsubscribe : AbstractFunction
+	{
+		public override Type ReturnType { get { return typeof(MailrelayBoolReply); } }
+
+		/// <summary>Email of the subscriber that you want to unsubscribe.</summary>
+		public string email;
+	}
+}
