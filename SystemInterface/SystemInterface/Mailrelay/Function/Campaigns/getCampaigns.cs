16554690b368f6739de7e4498b3aebf73fefd0be
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-21 13:36:01 +0200

    (SystemInterface) adding getCampaigns

---
+++ SystemInterface/SystemInterface/Mailrelay/Function/Campaigns/getCampaigns.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using SystemInterface.Mailrelay.FunctionReply;
+
+namespace SystemInterface.Mailrelay.Function.Campaigns
+{
+	public class getCampaigns : AbstractFunction
+	{
+		public override Type ReturnType { get { return typeof(MailrelayArrayReply<getCampaignsReply>); } }
+
+		///<summery>Optional parameter to specify offset.</summery>
+		public int? offset;
+
+		///<summery>Optional parameter to specify the number of records.</summery>
+		public int? count;
+
+		///<summery>Optional parameter to search by id.</summery>
+		public int? id;
+
+		///<summery>Optional parameter to search by subject.</summery>
+		public string subject;
+
+		///<summery>Optional parameter to search deleted campaigns</summery>
+		public bool? deleted;
+
+		///<summery>Optional parameter to specify a search field.</summery>
+		public string sortField;
+
+		///<summery>Optional parameter to specify a sort order. Accepted values are ASC and DESC (default).</summery>
+		public sortOrderEnum sortOrder;
+	}
+}
---
+++ SystemInterface/SystemInterface/Mailrelay/Function/Campaigns/getCampaignsReply.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SystemInterface.Mailrelay.Function.Campaigns
+{
+	public class getCampaignsReply
+	{
+		public int id;
+		public string subject;
+		public int mailbox_from_id;
+		public int mailbox_reply_id;
+		public int mailbox_report_id;
+		public string groups;
+		public string text;
+		public string html;
+		public string attachs;
+		public DateTime date;
+		public DateTime created;
+		public DateTime? last_sent;
+		public string deleted;
+		public DateTime? send_date;
+		public int subscribers_total;
+		public int package_id;
+		public int id_campaign_folder;
+		public string analytics_utm_campaign;
+		public List<int> mailing_lists;
+	}
+}
