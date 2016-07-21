5f27621e568ad018ab647fc6304ed1d7dd9d71a2
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-21 13:36:41 +0200

    (SystemInterface) adding getMailboxes

---
+++ SystemInterface/SystemInterface/Mailrelay/Function/Mailboxes/getMailboxes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using SystemInterface.Mailrelay.FunctionReply;
+
+namespace SystemInterface.Mailrelay.Function.Mailboxes
+{
+	public class getMailboxes : AbstractFunction
+	{
+		public override Type ReturnType { get { return typeof(MailrelayArrayReply<getMailboxesReply>); } }
+
+		///<summery>Optional parameter to specify offset.</summery>
+		public int? offset;
+
+		///<summery>Optional parameter to specify the number of records.</summery>
+		public int? count;
+
+		///<summery>Optional parameter to search by id.</summery>
+		public int? id;
+
+		///<summery>Optional parameter to search by mailbox name.</summery>
+		public string mailboxName;
+
+		///<summery>Optional parameter to search by name.</summery>
+		public string name;
+
+		///<summery>Optional parameter to search by email.</summery>
+		public string email;
+
+		///<summery>Optional parameter to specify a search field.</summery>
+		public string sortField;
+
+		///<summery>Optional parameter to specify a sort order. Accepted values are ASC and DESC (default).</summery>
+		public string sortOrder;
+	}
+}
---
+++ SystemInterface/SystemInterface/Mailrelay/Function/Mailboxes/getMailboxesReply.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace SystemInterface.Mailrelay.Function.Mailboxes
+{
+	public class getMailboxesReply
+	{
+		public int id;
+		public string mailbox_name;
+		public string name;
+		public string email;
+		public string check;
+		public string username;
+		public string password;
+		public string hostname;
+		public string imap;
+		public string apop;
+		public string delete;
+		public string enable;
+		public string confirmed;
+		[JsonProperty(PropertyName = "internal")]
+		public string internalProperty;
+	}
+}
