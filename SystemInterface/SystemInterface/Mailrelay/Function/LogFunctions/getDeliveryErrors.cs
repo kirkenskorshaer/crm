3bcbaa3c1cecaab3d708a97e58293b27387e9c4f
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-21 13:42:19 +0200

    (SystemInterface) adding getDeliveryErrors

---
+++ SystemInterface/SystemInterface/Mailrelay/Function/LogFunctions/getDeliveryErrors.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Dynamic;
+using SystemInterface.Mailrelay.FunctionReply;
+
+namespace SystemInterface.Mailrelay.Function.LogFunctions
+{
+	public class getDeliveryErrors : AbstractFunction
+	{
+		public override Type ReturnType { get { return typeof(MailrelayArrayReply<ExpandoObject>); } }
+
+		///<summery>Date in the following format: YYYY-MM-DD.</summery>
+		[ToGet(type = ToGetAttribute.typeEnum.ShortDate)]
+		public DateTime date;
+
+		///<summery>Optional bounce category. Check Appendix 1 for all bounce message codes.</summery>
+		public string bounceCategory;
+
+		///<summery>Optional parameter to specify offset. If not specified, it's 0.</summery>
+		public int? offset;
+
+		///<summery>Optional parameter to specify the number of records. If not specified, it's 100. The maximum allowed value is 1000. Numbers bigger than 1000 are automatically reduced to 1000.</summery>
+		public int? count;
+	}
+}
