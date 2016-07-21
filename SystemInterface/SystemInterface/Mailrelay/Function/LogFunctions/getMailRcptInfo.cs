310258b3f108720cc776b070e21e1e214de45009
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-21 13:42:56 +0200

    (SystemInterface) adding getMailRcptInfo

---
+++ SystemInterface/SystemInterface/Mailrelay/Function/LogFunctions/getMailRcptInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Dynamic;
+using SystemInterface.Mailrelay.FunctionReply;
+
+namespace SystemInterface.Mailrelay.Function.LogFunctions
+{
+	public class getMailRcptInfo : AbstractFunction
+	{
+		public override Type ReturnType { get { return typeof(MailrelayObjectReply<getMailRcptInfoReply>); } }
+
+		///<summery>Email address of the recipient.</summery>
+		public string email;
+
+		///<summery>Date to search in the following format: YYYY-MM-DD.</summery>
+		[ToGet(type = ToGetAttribute.typeEnum.ShortDate)]
+		public DateTime date;
+
+		///<summery>ID of the message.</summery>
+		public int id;
+	}
+}
---
+++ SystemInterface/SystemInterface/Mailrelay/Function/LogFunctions/getMailRcptInfoReply.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace SystemInterface.Mailrelay.Function.LogFunctions
+{
+	public class getMailRcptInfoReply
+	{
+		public string type;
+		public string timeLogged;
+		public string timeQueued;
+		public string timeImpinted;
+		public string orig;
+		public string rcpt;
+		public string orcpt;
+		public string dsnAction;
+		public string dsnStatus;
+		public string dsnDiag;
+		public string dsnMta;
+		public string bounceCat;
+		public string srcType;
+		public string srcMta;
+		public string dlvType;
+		public string dlvSourceIp;
+		public string dlvDestinationIp;
+		public string dlvEsmtpAvailable;
+		public string dlvSize;
+		public string vmta;
+		public int? jobId;
+		public int? envId;
+		public int? sentId;
+		public int? mailingListId;
+	}
+}
