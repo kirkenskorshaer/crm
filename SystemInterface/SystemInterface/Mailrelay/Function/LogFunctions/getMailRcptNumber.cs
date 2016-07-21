e3ab748b63d380a448a0aea13d2daba2deeb3dee
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-21 13:43:25 +0200

    (SystemInterface) adding getMailRcptNumber

---
+++ SystemInterface/SystemInterface/Mailrelay/Function/LogFunctions/getMailRcptNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SystemInterface.Mailrelay.FunctionReply;
+
+namespace SystemInterface.Mailrelay.Function.LogFunctions
+{
+	public class getMailRcptNumber : AbstractFunction
+	{
+		public override Type ReturnType { get { return typeof(MailrelayIntReply); } }
+
+		///<summery>Email address of the recipient.</summery>
+		public string email;
+
+		///<summery>Date to search in the following format: YYYY-MM-DD.</summery>
+		[ToGet(type = ToGetAttribute.typeEnum.ShortDate)]
+		public DateTime date;
+	}
+}
