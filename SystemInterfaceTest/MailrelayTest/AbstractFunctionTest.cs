b04b898962d8e951eec62262cac70ea46466f8b3
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-21 13:34:13 +0200

    (SystemInterfaceTest) testing mailrelay functions

---
+++ SystemInterfaceTest/MailrelayTest/AbstractFunctionTest.cs
@@ -0,0 +1,182 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using SystemInterface.Mailrelay;
+using SystemInterface.Mailrelay.Function.CustomFields;
+using SystemInterface.Mailrelay.Function.Groups;
+using SystemInterface.Mailrelay.Function.LogFunctions;
+using SystemInterface.Mailrelay.Function.Statistics;
+using SystemInterface.Mailrelay.Function.Subscribers;
+
+namespace SystemInterfaceTest.MailrelayTest
+{
+	[TestFixture]
+	public class AbstractFunctionTest : TestBase
+	{
+		[Test]
+		public void ToGetCreatesUrlEncodedRepresentationOfValues()
+		{
+			string expectedUrlEncoded = $"&function=getStats&id=1&startDate=2000-01-01+00%3a00%3a00&endDate=2001-01-01+00%3a00%3a00";
+
+			getStats function = new getStats()
+			{
+				startDate = new DateTime(2000, 1, 1),
+				endDate = new DateTime(2001, 1, 1),
+				id = 1,
+			};
+
+			AssertToGet(function, expectedUrlEncoded);
+		}
+
+		[Test]
+		public void functionWithEnumCanBeUrlEncoded()
+		{
+			string expectedUrlEncoded = $"&function=getGroups&sortOrder=ASC";
+
+			getGroups function = new getGroups()
+			{
+				sortOrder = AbstractFunction.sortOrderEnum.ASC,
+			};
+
+			AssertToGet(function, expectedUrlEncoded);
+		}
+
+		[Test]
+		public void functionWithToGetAttributeCanBeUrlEncoded()
+		{
+			string expectedUrlEncoded = $"&function=addCustomField&name=test&fieldType=3&position=1&default=defaultValue&enable=1";
+
+			addCustomField function = new addCustomField()
+			{
+				fieldType = AbstractCustomField.CustomFieldTypeEnum.Textarea,
+				position = 1,
+				name = "test",
+				defaultField = "defaultValue",
+				enable = true,
+			};
+
+			AssertToGet(function, expectedUrlEncoded);
+		}
+
+		[Test]
+		public void functionWithIntArrayCanBeUrlEncoded()
+		{
+			string expectedUrlEncoded = $"&function=getSubscribers&groups%5b0%5d=1&groups%5b1%5d=4";
+
+			getSubscribers function = new getSubscribers()
+			{
+				groups = new List<int>() { 1, 4 },
+			};
+
+			AssertToGet(function, expectedUrlEncoded);
+		}
+
+		[Test]
+		public void functionWithStringArrayCanBeUrlEncoded()
+		{
+			string expectedUrlEncoded = $"&function=assignSubscribersToGroups&groups%5b0%5d=1&groups%5b1%5d=4&subscribers%5b0%5d=test";
+
+			assignSubscribersToGroups function = new assignSubscribersToGroups()
+			{
+				groups = new List<int>() { 1, 4 },
+				subscribers = new List<string>() { "test" },
+			};
+
+			AssertToGet(function, expectedUrlEncoded);
+		}
+
+		[Test]
+		public void functionWithSingleElementArrayCanBeUrlEncoded()
+		{
+			string expectedUrlEncoded = $"&function=getSubscribers&groups%5b0%5d=1";
+
+			getSubscribers function = new getSubscribers()
+			{
+				groups = new List<int>() { 1 },
+			};
+
+			AssertToGet(function, expectedUrlEncoded);
+		}
+
+		[Test]
+		public void functionWithEmptyArrayCanBeUrlEncoded()
+		{
+			string expectedUrlEncoded = $"&function=getSubscribers";
+
+			getSubscribers function = new getSubscribers()
+			{
+			};
+
+			AssertToGet(function, expectedUrlEncoded);
+		}
+
+		[Test]
+		public void functionWithDictionaryCanBeUrlEncoded()
+		{
+			string expectedUrlEncoded = $"&function=addSubscriber&customFields%5bf_1%5d=test+1&customFields%5bf_2%5d=test+2";
+
+			addSubscriber function = new addSubscriber()
+			{
+				customFields = new Dictionary<string, string>()
+				{
+					{ "f_1", "test 1" },
+					{ "f_2", "test 2" },
+				},
+			};
+
+			AssertToGet(function, expectedUrlEncoded);
+		}
+
+		[Test]
+		public void functionWithSingleElementDictionaryCanBeUrlEncoded()
+		{
+			string expectedUrlEncoded = $"&function=addSubscriber&customFields%5bf_1%5d=test+1";
+
+			addSubscriber function = new addSubscriber()
+			{
+				customFields = new Dictionary<string, string>()
+				{
+					{ "f_1", "test 1" },
+				},
+			};
+
+			AssertToGet(function, expectedUrlEncoded);
+		}
+
+		[Test]
+		public void functionWithEmptyDictionaryCanBeUrlEncoded()
+		{
+			string expectedUrlEncoded = $"&function=addSubscriber";
+
+			addSubscriber function = new addSubscriber()
+			{
+			};
+
+			AssertToGet(function, expectedUrlEncoded);
+		}
+
+		[Test]
+		public void functionWithShortDateCanBeUrlEncoded()
+		{
+			string expectedUrlEncoded = $"&function=getDeliveryErrors&date=2000-01-01";
+
+			getDeliveryErrors function = new getDeliveryErrors()
+			{
+				date = new DateTime(2000, 1, 1),
+			};
+
+			AssertToGet(function, expectedUrlEncoded);
+		}
+
+		private void AssertToGet(AbstractFunction function, string expectedEncoded)
+		{
+			string apiKey = "testApiKey";
+			string expectedUrlEncoded = $"{expectedEncoded}&apiKey={apiKey}";
+			function.apiKey = apiKey;
+
+			string output = function.ToGet();
+
+			Assert.AreEqual(expectedUrlEncoded, output);
+		}
+	}
+}
---
+++ SystemInterfaceTest/MailrelayTest/MailrelayConnectionTest.cs
@@ -0,0 +1,300 @@
+﻿using NUnit.Framework;
+using System;
+using SystemInterface.Mailrelay;
+using SystemInterface.Mailrelay.FunctionReply;
+using SystemInterface.Mailrelay.Function.Groups;
+using SystemInterface.Mailrelay.Function.Statistics;
+using SystemInterface.Mailrelay.Function.CustomFields;
+using SystemInterface.Mailrelay.Function.Subscribers;
+using System.Collections.Generic;
+using SystemInterface.Mailrelay.Function.SMTP;
+using SystemInterface.Mailrelay.Function.LogFunctions;
+using SystemInterface.Mailrelay.Function.Mailboxes;
+using SystemInterface.Mailrelay.Function.Campaigns;
+
+namespace SystemInterfaceTest.MailrelayTest
+{
+	[TestFixture]
+	public class MailrelayConnectionTest : TestBase
+	{
+		[Test]
+		public void getStats()
+		{
+			getStats function = new getStats() { startDate = DateTime.Now.AddDays(1), endDate = DateTime.Now };
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		[Test]
+		public void getGroups()
+		{
+			getGroups function = new getGroups() { sortOrder = AbstractFunction.sortOrderEnum.ASC };
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		[Test]
+		[Ignore]
+		public void addGroup()
+		{
+			addGroup function = new addGroup() { description = "this is a test group", enable = false, name = "test", position = 3, visible = true };
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		[Test]
+		[Ignore]
+		public void updateGroup()
+		{
+			updateGroup function = new updateGroup() { id = 2, description = "this is test group 2", enable = false, name = "test", visible = true, position = 4 };
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		[Test]
+		[Ignore]
+		public void deleteGroup()
+		{
+			deleteGroup function = new deleteGroup() { id = 2 };
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		[Test]
+		[Ignore]
+		public void getCustomFields()
+		{
+			getCustomFields function = new getCustomFields() { };
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		[Test]
+		[Ignore]
+		public void addCustomField()
+		{
+			addCustomField function = new addCustomField() { defaultField = "testDefault", enable = true, fieldType = AbstractCustomField.CustomFieldTypeEnum.TextField, name = "test", position = 1 };
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		[Test]
+		[Ignore]
+		public void updateCustomField()
+		{
+			updateCustomField function = new updateCustomField() { id = 1, defaultField = "testDefault", enable = true, fieldType = AbstractCustomField.CustomFieldTypeEnum.TextField, name = "test", position = 1 };
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		[Test]
+		[Ignore]
+		public void deleteCustomField()
+		{
+			deleteCustomField function = new deleteCustomField() { id = 2 };
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		[Test]
+		[Ignore]
+		public void addSubscriber()
+		{
+			addSubscriber function = new addSubscriber()
+			{
+				email = "svend.l.kirkenskorshaer@gmail.com",
+				groups = new List<int>() { 1 },
+				name = "test_name",
+				customFields = new Dictionary<string, string>()
+				{
+					{ "f_1", "f1" },
+					{ "f_3", "f3" },
+					{ "f_4", "f4" },
+					{ "f_5", "f5" },
+				},
+			};
+
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		[Test]
+		public void getSubscribers()
+		{
+			getSubscribers function = new getSubscribers() { };
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		[Test]
+		[Ignore]
+		public void updateSubscribers()
+		{
+			updateSubscribers function = new updateSubscribers() { ids = new List<int>() { 2 }, activated = true, banned = false, deleted = false };
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		[Test]
+		[Ignore]
+		public void updateSubscriber()
+		{
+			updateSubscriber function = new updateSubscriber()
+			{
+				id = 4,
+				email = "svend.l.kirkenskorshaer@gmail.com_test",
+				name = "test_name",
+				activated = true,
+				groups = new List<int>() { 1 },
+				customFields = new Dictionary<string, string>()
+				{
+					{ "f_1", "f1" },
+					{ "f_3", "f3" },
+					{ "f_4", "f4" },
+					{ "f_5", "f5" },
+				},
+			};
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		[Test]
+		[Ignore]
+		public void assignSubscribersToGroups()
+		{
+			assignSubscribersToGroups function = new assignSubscribersToGroups() { groups = new List<int>() { 3 }, subscribers = new List<string>() { "svend.l.kirkenskorshaer@gmail.com_test2" } };
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		[Test]
+		[Ignore]
+		public void unsubscribe()
+		{
+			unsubscribe function = new unsubscribe() { email = "svend.l.kirkenskorshaer@gmail.com" };
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		[Test]
+		[Ignore]
+		public void deleteSubscriber()
+		{
+			deleteSubscriber function = new deleteSubscriber() { email = "svend.l.kirkenskorshaer@gmail.com" };
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		[Test]
+		[Ignore]
+		public void getSmtpTags()
+		{
+			getSmtpTags function = new getSmtpTags() { count = 10, offset = 0 };
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		[Test]
+		[Ignore]
+		public void getSends()
+		{
+			getSends function = new getSends() { count = 10 };
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		[Test]
+		[Ignore]
+		public void getDeliveryErrors()
+		{
+			getDeliveryErrors function = new getDeliveryErrors() { count = 10 };
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		[Test]
+		[Ignore]
+		public void getDayLog()
+		{
+			getDayLog function = new getDayLog() { count = 10 };
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		[Test]
+		[Ignore]
+		public void getMailRcptNumber()
+		{
+			getMailRcptNumber function = new getMailRcptNumber() { email = "svend.l.kirkenskorshaer@gmail.com", date = DateTime.Now };
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		[Test]
+		[Ignore]
+		public void getMailRcptInfo()
+		{
+			getMailRcptInfo function = new getMailRcptInfo() { id = 1, email = "svend.l.kirkenskorshaer@gmail.com", date = DateTime.Now };
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		[Test]
+		[Ignore]
+		public void getPackages()
+		{
+			getPackages function = new getPackages() { sortOrder = AbstractFunction.sortOrderEnum.DESC };
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		[Test]
+		[Ignore]
+		public void sendMail()
+		{
+			sendMail function = new sendMail()
+			{
+				emails = new Dictionary<string, string>()
+				{
+					{ "Svend", "svend.l.kirkenskorshaer@gmail.com" }
+				},
+				subject = "test",
+				html = "hej [name] vi tester igen",
+				mailboxFromId = 1,
+				mailboxReplyId = 1,
+				mailboxReportId = 1,
+				packageId = 6,
+			};
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		[Test]
+		[Ignore]
+		public void getMailboxes()
+		{
+			getMailboxes function = new getMailboxes() { };
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		[Test]
+		[Ignore]
+		public void getCampaigns()
+		{
+			getCampaigns function = new getCampaigns() { };
+			AbstractMailrelayReply reply = TestCall(function);
+			Assert.AreEqual(1, reply.status);
+		}
+
+		private AbstractMailrelayReply TestCall(AbstractFunction functionToCall)
+		{
+			AbstractMailrelayReply reply = _mailrelayConnection.Send(functionToCall);
+
+			Console.Out.WriteLine(functionToCall.ToGet());
+			Console.Out.WriteLine(reply.ToJson());
+
+			return reply;
+		}
+	}
+}
