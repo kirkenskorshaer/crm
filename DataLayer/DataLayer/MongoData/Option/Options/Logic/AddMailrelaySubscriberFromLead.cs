b7ecd3b443464c06e7c970b2e41f8d9f686987c3
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-25 14:37:18 +0200

    (DataLayer) added AddMailrelaySubscriberFromLead

---
+++ DataLayer/DataLayer/MongoData/Option/Options/Logic/AddMailrelaySubscriberFromLead.cs
@@ -0,0 +1,48 @@
+﻿using DataLayer.MongoData.Input;
+using MongoDB.Bson;
+
+namespace DataLayer.MongoData.Option.Options.Logic
+{
+	public class AddMailrelaySubscriberFromLead : OptionBase
+	{
+		public string urlLoginName { get; set; }
+		public ObjectId WebCampaignId { get; set; }
+		public string email { get; set; }
+
+		public static AddMailrelaySubscriberFromLead Create(MongoConnection mongoConnection, WebCampaign webCampaign, string urlLoginName, string email, string name, Schedule schedule)
+		{
+			return Create(mongoConnection, webCampaign._id, urlLoginName, email, name, schedule);
+		}
+
+		public static AddMailrelaySubscriberFromLead Create(MongoConnection mongoConnection, ObjectId webCampaignId, string urlLoginName, string email, string name, Schedule schedule)
+		{
+			AddMailrelaySubscriberFromLead addMailrelaySubscriberFromLead = new AddMailrelaySubscriberFromLead()
+			{
+				WebCampaignId = webCampaignId,
+				urlLoginName = urlLoginName,
+				email = email,
+			};
+
+			Create(mongoConnection, addMailrelaySubscriberFromLead, name, schedule);
+
+			return addMailrelaySubscriberFromLead;
+		}
+
+		protected override void Execute(MongoConnection connection, bool recurring)
+		{
+			if (recurring)
+			{
+				Update<AddMailrelaySubscriberFromLead>(connection);
+			}
+			else
+			{
+				Delete<AddMailrelaySubscriberFromLead>(connection);
+			}
+		}
+
+		public byte[] WebCampaignIdValue()
+		{
+			return WebCampaignId.ToByteArray();
+		}
+	}
+}
