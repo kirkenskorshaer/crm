1a0f313d75531307028564209fcf2b864160f8c8
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-25 12:48:25 +0200

    (TestUtilities) adding TestUtilities project

---
+++ TestUtilities/TestUtilities/MailrelayConnectionTester.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using SystemInterface.Mailrelay;
+using SystemInterface.Mailrelay.FunctionReply;
+
+namespace TestUtilities
+{
+	public class MailrelayConnectionTester : IMailrelayConnection
+	{
+		public Queue<AbstractMailrelayReply> replies = new Queue<AbstractMailrelayReply>();
+		public List<AbstractFunction> sendFunctions = new List<AbstractFunction>();
+
+		public AbstractMailrelayReply Send(AbstractFunction functionToSend)
+		{
+			sendFunctions.Add(functionToSend);
+
+			AbstractMailrelayReply reply = replies.Dequeue();
+
+			return reply;
+		}
+
+		public override string ToString()
+		{
+			StringBuilder raportBuilder = new StringBuilder();
+
+			foreach (AbstractFunction function in sendFunctions)
+			{
+				raportBuilder.AppendLine(HttpUtility.UrlDecode(function.ToGet()));
+			}
+
+			return raportBuilder.ToString();
+		}
+	}
+}
