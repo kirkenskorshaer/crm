cb89edccbe84dde74edc1902fa72fd08581edad3
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-25 14:50:22 +0200

    (SystemInterface) adding MailrelayInformation

---
+++ SystemInterface/SystemInterface/Dynamics/Crm/MailrelayInformation.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.Xrm.Client;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SystemInterface.Dynamics.Crm
+{
+	public class MailrelayInformation : AbstractCrm
+	{
+		public string fullname;
+		public DateTime createdon;
+		public int? campaign_new_mailrelaygroupid;
+		public string Indsamlingssted2016_emailaddress1;
+		public string Indsamlingssted2016_address1_composite;
+		public string Indsamlingssted2016_name;
+		public string Indsamlingskoordinator_fullname;
+		public string Indsamlingskoordinator_emailaddress1;
+		public string Indsamlingskoordinator_mobilephone;
+
+		protected override string entityName { get { return "lead"; } }
+
+		protected override string idName { get { return "leadid"; } }
+
+		protected override ColumnSet ColumnSetCrmGenerated { get { throw new NotImplementedException(); } }
+
+		public MailrelayInformation(DynamicsCrmConnection connection) : base(connection)
+		{
+		}
+
+		public MailrelayInformation(DynamicsCrmConnection connection, Entity crmEntity) : base(connection, crmEntity)
+		{
+		}
+
+		public static MailrelayInformation GetMailrelayFromLead(DynamicsCrmConnection dynamicsCrmConnection, Func<string, string> getResourcePath, string email, Guid campaignId)
+		{
+			string path = getResourcePath("Dynamics/Crm/FetchXml/Mailrelay/GetMailrelayFromLead.xml");
+
+			XDocument xDocument = XDocument.Load(path);
+
+			xDocument.Element("fetch").Element("entity").Element("filter").Element("condition").Attribute("value").Value = email;
+
+			xDocument.Element("fetch").Element("entity").Elements("link-entity").Single(element => element.Attribute("name").Value == "campaign").Element("filter").Element("condition").Attribute("value").Value = campaignId.ToString();
+
+			List<MailrelayInformation> informations = StaticCrm.ReadFromFetchXml(dynamicsCrmConnection, xDocument, (connection, entity) => new MailrelayInformation(connection, entity), new PagingInformation());
+
+			MailrelayInformation information = informations.OrderByDescending(lInformation => lInformation.createdon).FirstOrDefault();
+
+			return information;
+		}
+
+		protected override CrmEntity GetAsEntity(bool includeId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Dictionary<string, string> GetCustomFields()
+		{
+			Dictionary<string, string> customFields = new Dictionary<string, string>();
+
+			AddIfValueNotEmpty(customFields, "f_1", Indsamlingssted2016_name);
+			AddIfValueNotEmpty(customFields, "f_3", Indsamlingssted2016_emailaddress1);
+			AddIfValueNotEmpty(customFields, "f_4", Indsamlingssted2016_address1_composite);
+			AddIfValueNotEmpty(customFields, "f_5", Indsamlingskoordinator_fullname);
+			AddIfValueNotEmpty(customFields, "f_6", Indsamlingskoordinator_emailaddress1);
+			AddIfValueNotEmpty(customFields, "f_7", Indsamlingskoordinator_mobilephone);
+
+			return customFields;
+		}
+
+		private void AddIfValueNotEmpty(Dictionary<string, string> customFields, string key, string value)
+		{
+			if (string.IsNullOrWhiteSpace(value))
+			{
+				return;
+			}
+
+			customFields.Add(key, value);
+		}
+	}
+}
