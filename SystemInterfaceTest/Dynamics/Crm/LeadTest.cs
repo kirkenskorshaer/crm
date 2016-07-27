31f1ccf3990f6f4fd7e9a5dd3aba952e1019f9c9
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-07-27 09:20:10 +0200

    (SystemInterfaceTest) testing SubscriberId on lead

---
+++ SystemInterfaceTest/Dynamics/Crm/LeadTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SystemInterface.Dynamics.Crm;
+
+namespace SystemInterfaceTest.Dynamics.Crm
+{
+	[TestFixture]
+	public class LeadTest : TestBase
+	{
+		[Test]
+		public void LeadCanHaveSubscriberIdUpdated()
+		{
+			string firstname = $"firstname {Guid.NewGuid()}";
+			int subscriberId = new Random().Next(1, int.MaxValue);
+
+			Lead lead = Lead.Create(_dynamicsCrmConnection, new Dictionary<string, string>()
+			{
+				{ "firstname", firstname}
+			});
+
+			lead.InsertWithoutRead();
+
+			Lead.UpdateSubscriberId(_dynamicsCrmConnection, lead.Id, subscriberId);
+
+			Lead leadRead = Lead.ReadFromFetchXml(_dynamicsCrmConnection, new List<string>() { "leadid", "firstname", "new_mailrelaysubscriberid" }, new Dictionary<string, string>() { { "leadid", lead.Id.ToString() } }).Single();
+
+			lead.Delete();
+
+			Assert.AreEqual(firstname, leadRead.firstname);
+			Assert.AreEqual(subscriberId, leadRead.new_mailrelaysubscriberid);
+		}
+	}
+}
