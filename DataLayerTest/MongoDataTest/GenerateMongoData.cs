d15c2517d2ce62bfabcb1610832be17cc4ba8f91
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-08-03 13:02:50 +0200

    (DataLayerTest) added a GenerateMongoData class

---
+++ DataLayerTest/MongoDataTest/GenerateMongoData.cs
@@ -0,0 +1,27 @@
+﻿using DataLayer.MongoData.Option;
+using DataLayer.MongoData.Option.Options.Logic;
+using NUnit.Framework;
+using System;
+
+namespace DataLayerTest.MongoDataTest
+{
+	[TestFixture]
+	public class GenerateMongoData : TestBase
+	{
+		[Test]
+		public void CreateUpdateMailrelayFromContact()
+		{
+			UpdateMailrelayFromContact.Create(_mongoConnection, "test", "test", GreateSchedule());
+		}
+
+		private Schedule GreateSchedule()
+		{
+			return new Schedule()
+			{
+				NextAllowedExecution = new DateTime(2000, 1, 1),
+				Recurring = true,
+				TimeBetweenAllowedExecutions = TimeSpan.FromMinutes(10),
+			};
+		}
+	}
+}
