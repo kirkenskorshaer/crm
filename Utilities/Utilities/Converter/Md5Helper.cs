b88b4ca5e270b08aeb70726bb7e65d35f5aab505
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-08-03 15:23:05 +0200

    (Utilities) adding Md5Helper

---
+++ Utilities/Utilities/Converter/Md5Helper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Utilities.Converter
+{
+	public static class Md5Helper
+	{
+		public static string MakeMd5(string input)
+		{
+			string hash;
+			using (MD5 md5 = MD5.Create())
+			{
+				hash = BitConverter.ToString(md5.ComputeHash(Encoding.UTF8.GetBytes(input)));
+			}
+			return hash;
+		}
+	}
+}
