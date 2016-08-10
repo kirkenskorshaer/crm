6b28e72cfc502a5a2a65129b3c924b380f47dd2d
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-08-10 08:32:08 +0200

    (DataLayer) adding option UpdateMailrelayGroup

---
+++ DataLayer/DataLayer/MongoData/Option/Options/Logic/UpdateMailrelayGroup.cs
@@ -0,0 +1,39 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace DataLayer.MongoData.Option.Options.Logic
+{
+	public class UpdateMailrelayGroup : OptionBase
+	{
+		public string urlLoginName { get; set; }
+
+		[BsonRepresentation(BsonType.String)]
+		public Guid? listId { get; set; }
+
+		public static UpdateMailrelayGroup Create(MongoConnection mongoConnection, string urlLoginName, string name, Guid? listId, Schedule schedule)
+		{
+			UpdateMailrelayGroup updateMailrelayGroup = new UpdateMailrelayGroup()
+			{
+				urlLoginName = urlLoginName,
+				listId = listId,
+			};
+
+			Create(mongoConnection, updateMailrelayGroup, name, schedule);
+
+			return updateMailrelayGroup;
+		}
+
+		protected override void Execute(MongoConnection connection, bool recurring)
+		{
+			if (recurring)
+			{
+				Update<UpdateMailrelayGroup>(connection);
+			}
+			else
+			{
+				Delete<UpdateMailrelayGroup>(connection);
+			}
+		}
+	}
+}
