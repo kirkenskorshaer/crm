504edda097b22850986761ee595a8e56b414d031
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-08-10 09:19:45 +0200

    (SubscriberTest) testing subscriber.GetMailrelaySubscribers

--- SystemInterfaceTest/MailrelayTest/LogicTest/SubscriberTest.cs
+++ SystemInterfaceTest/MailrelayTest/LogicTest/SubscriberTest.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using SystemInterface.Mailrelay.Function.Subscribers;
 using SystemInterface.Mailrelay.FunctionReply;
 using SystemInterface.Mailrelay.Logic;
@@ -108,6 +109,48 @@ namespace SystemInterfaceTest.MailrelayTest.LogicTest
 			subscriber.UpdateIfNeeded(id, fullname, email, customFields, groupInCrm);
 
 			Console.Out.WriteLine(_mailrelayConnectionTester);
+		}
+
+		[Test]
+		public void SubscribersEnumerationReturnSubscribers()
+		{
+			Dictionary<string, string> customFields = new Dictionary<string, string>();
+			List<string> groups12 = new List<string>() { "1", "2" };
+
+			EnqueueSubscribersReply(customFields, groups12, 2);
+			EnqueueSubscribersReply(customFields, groups12, 2);
+			EnqueueSubscribersReply(customFields, groups12, 2);
+			EnqueueSubscribersReply(customFields, groups12, 1);
+
+			Subscriber subscriber = new Subscriber(_mailrelayConnectionTester);
+
+			getSubscribers getSubscribersFunction = new getSubscribers();
+
+			IEnumerable<getSubscribersReply> replies = subscriber.GetMailrelaySubscribers(getSubscribersFunction, 2);
+
+			Assert.AreEqual(0, _mailrelayConnectionTester.sendFunctions.Count);
+
+			replies.ToList();
+
+			Assert.AreEqual(4, _mailrelayConnectionTester.sendFunctions.Count);
+
+			Console.Out.WriteLine(_mailrelayConnectionTester);
+		}
+
+		private void EnqueueSubscribersReply(Dictionary<string, string> customFields, List<string> groups, int replies)
+		{
+			List<getSubscribersReply> data = new List<getSubscribersReply>();
+
+			for (int replyIndex = 0; replyIndex < replies; replyIndex++)
+			{
+				data.Add(new getSubscribersReply() { email = "email", name = "name", fields = customFields, groups = groups, });
+			}
+
+			_mailrelayConnectionTester.replies.Enqueue(new MailrelayArrayReply<getSubscribersReply>()
+			{
+				status = 1,
+				data = data,
+			});
 		}
 	}
 }
