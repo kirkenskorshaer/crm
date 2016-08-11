f68426f9f2adaec2309140a5d86c16c7b9b4baca
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-08-11 13:49:23 +0200

    (SystemInterface) adding SingleValueEntity

---
+++ SystemInterface/SystemInterface/Dynamics/Crm/SingleValueEntity.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xrm.Sdk;
+
+namespace SystemInterface.Dynamics.Crm
+{
+	public class SingleValueEntity<ValueType> : AbstractValueEntity
+	{
+		public ValueType value;
+
+		public SingleValueEntity(DynamicsCrmConnection connection, Entity crmEntity, string entityName, string idName) : base(connection, crmEntity, entityName, idName)
+		{
+		}
+	}
+}
