d72751fe43c9a5439afece92ccd5e8b2219299bc
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-08-11 13:43:44 +0200

    (DataLayer) adding ExportContactToMailrelay

---
+++ DataLayer/DataLayer/MongoData/Option/Options/Logic/ExportContactToMailrelay.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace DataLayer.MongoData.Option.Options.Logic
+{
+	public class ExportContactToMailrelay : OptionBase
+	{
+		public string urlLoginName { get; set; }
+
+		[BsonRepresentation(BsonType.String)]
+		public Guid? listId { get; set; }
+
+		protected override void Execute(MongoConnection connection, bool recurring)
+		{
+			if (recurring)
+			{
+				Update<ExportContactToMailrelay>(connection);
+			}
+			else
+			{
+				Delete<ExportContactToMailrelay>(connection);
+			}
+		}
+	}
+}
