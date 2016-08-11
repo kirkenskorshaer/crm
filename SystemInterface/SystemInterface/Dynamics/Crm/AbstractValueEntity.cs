edeeceb88d7ba8b5fbe617b5faeea5cd17341eff
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-08-11 13:48:06 +0200

    (SystemInterface) moving KeyValueEntity functionality to new class AbstractValueEntity

---
+++ SystemInterface/SystemInterface/Dynamics/Crm/AbstractValueEntity.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xrm.Client;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+
+namespace SystemInterface.Dynamics.Crm
+{
+	public abstract class AbstractValueEntity : AbstractCrm
+	{
+		private string _entityName;
+		private string _idName;
+
+		public AbstractValueEntity(DynamicsCrmConnection connection, Entity crmEntity, string entityName, string idName) : base(connection)
+		{
+			_idName = idName;
+			_entityName = entityName;
+
+			InitializeFromEntity(crmEntity);
+		}
+
+		public AbstractValueEntity(DynamicsCrmConnection connection) : base(connection)
+		{
+			throw new NotImplementedException();
+		}
+
+		public AbstractValueEntity(DynamicsCrmConnection connection, Entity crmEntity) : base(connection, crmEntity)
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override ColumnSet ColumnSetCrmGenerated { get { throw new NotImplementedException(); } }
+
+		protected override string entityName { get { return _entityName; } }
+
+		protected override string idName { get { return _idName; } }
+
+		protected override CrmEntity GetAsEntity(bool includeId) { throw new NotImplementedException(); }
+	}
+}
--- SystemInterface/SystemInterface/Dynamics/Crm/KeyValueEntity.cs
+++ SystemInterface/SystemInterface/Dynamics/Crm/KeyValueEntity.cs
@@ -1,41 +1,14 @@
-﻿using Microsoft.Xrm.Client;
-using Microsoft.Xrm.Sdk;
-using Microsoft.Xrm.Sdk.Query;
-using System;
+﻿using Microsoft.Xrm.Sdk;
 
 namespace SystemInterface.Dynamics.Crm
 {
-	public class KeyValueEntity<KeyType, ValueType> : AbstractCrm
+	public class KeyValueEntity<KeyType, ValueType> : AbstractValueEntity
 	{
-		private string _entityName;
-		private string _idName;
 		public KeyType key;
 		public ValueType value;
 
-		public KeyValueEntity(DynamicsCrmConnection connection, Entity crmEntity, string entityName, string idName) : base(connection)
+		public KeyValueEntity(DynamicsCrmConnection connection, Entity crmEntity, string entityName, string idName) : base(connection, crmEntity, entityName, idName)
 		{
-			_idName = idName;
-			_entityName = entityName;
-
-			InitializeFromEntity(crmEntity);
 		}
-
-		public KeyValueEntity(DynamicsCrmConnection connection) : base(connection)
-		{
-			throw new NotImplementedException();
-		}
-
-		public KeyValueEntity(DynamicsCrmConnection connection, Entity crmEntity) : base(connection, crmEntity)
-		{
-			throw new NotImplementedException();
-		}
-
-		protected override ColumnSet ColumnSetCrmGenerated { get { throw new NotImplementedException(); } }
-
-		protected override string entityName { get { return _entityName; } }
-
-		protected override string idName { get { return _idName; } }
-
-		protected override CrmEntity GetAsEntity(bool includeId) { throw new NotImplementedException(); }
 	}
 }
