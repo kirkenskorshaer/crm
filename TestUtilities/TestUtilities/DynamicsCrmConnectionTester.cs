7de88103eb55558e9c17109b89bad391f6fef460
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-08-22 15:21:59 +0200

    (TestUtilities) added DynamicsCrmTester

---
+++ TestUtilities/TestUtilities/DynamicsCrmConnectionTester.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using SystemInterface.Dynamics.Crm;
+
+namespace TestUtilities
+{
+	public class DynamicsCrmConnectionTester : IDynamicsCrmConnection
+	{
+		public DynamicsCrmConnectionTester()
+		{
+			Service = new OrganizationServiceTester();
+		}
+
+		public OrganizationServiceContext Context { get; }
+		public IOrganizationService Service { get; }
+
+		public void EnqueueRetrieveMultiple(string entityName, List<Dictionary<string, object>> entityDictionaryList)
+		{
+			List<Entity> entities = new List<Entity>();
+
+			foreach (Dictionary<string, object> entityDictionary in entityDictionaryList)
+			{
+				Entity entity = new Entity(entityName);
+				foreach (KeyValuePair<string, object> entityAttribute in entityDictionary)
+				{
+					entity.Attributes.Add(entityAttribute);
+				}
+				entities.Add(entity);
+			}
+			EntityCollection collection = new EntityCollection(entities);
+
+			((OrganizationServiceTester)Service).RetrieveMultipleQueue.Enqueue(collection);
+		}
+	}
+}
---
+++ TestUtilities/TestUtilities/OrganizationServiceTester.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using Microsoft.Xrm.Sdk.Query;
+using System.Collections.Generic;
+
+namespace TestUtilities
+{
+	public class OrganizationServiceTester : IOrganizationService
+	{
+		public void Associate(string entityName, Guid entityId, Relationship relationship, EntityReferenceCollection relatedEntities)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Guid Create(Entity entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Delete(string entityName, Guid id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Disassociate(string entityName, Guid entityId, Relationship relationship, EntityReferenceCollection relatedEntities)
+		{
+			throw new NotImplementedException();
+		}
+
+		public OrganizationResponse Execute(OrganizationRequest request)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Entity Retrieve(string entityName, Guid id, ColumnSet columnSet)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Queue<EntityCollection> RetrieveMultipleQueue = new Queue<EntityCollection>();
+		public List<QueryBase> RetrieveMultipleQueries = new List<QueryBase>();
+		public EntityCollection RetrieveMultiple(QueryBase query)
+		{
+			if (RetrieveMultipleQueue.Count == 0)
+			{
+				string errorString = "";
+
+				if (query is FetchExpression)
+				{
+					errorString = ((FetchExpression)query).Query;
+				}
+
+				throw new Exception($"RetrieveMultipleQueue queue was empty calling: {errorString}");
+			}
+
+			RetrieveMultipleQueries.Add(query);
+
+			EntityCollection reply = RetrieveMultipleQueue.Dequeue();
+
+			return reply;
+		}
+
+		public void Update(Entity entity)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
