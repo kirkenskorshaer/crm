961c575d91b0d755c9fdfa3dc0b97e23187dda94
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-08-22 15:20:51 +0200

    (XmlHelper) made helper public

--- SystemInterface/SystemInterface/Dynamics/Crm/XmlHelper.cs
+++ SystemInterface/SystemInterface/Dynamics/Crm/XmlHelper.cs
@@ -3,19 +3,19 @@ using System.Xml.Linq;
 
 namespace SystemInterface.Dynamics.Crm
 {
-	internal static class XmlHelper
+	public static class XmlHelper
 	{
-		internal static void RemoveAllAttributes(XDocument xDocument)
+		public static void RemoveAllAttributes(XDocument xDocument)
 		{
 			xDocument.Descendants().Where(descendant => descendant.Name == "attribute").Remove();
 		}
 
-		internal static void AddAliasedValue(XDocument xDocument, string crmName, string aliasName)
+		public static void AddAliasedValue(XDocument xDocument, string crmName, string aliasName)
 		{
 			xDocument.Element("fetch").Element("entity").Add(new XElement("attribute", new XAttribute("name", crmName), new XAttribute("alias", aliasName)));
 		}
 
-		internal static void AddCondition(XDocument xDocument, string fieldName, string operatorString, string valueString = null)
+		public static void AddCondition(XDocument xDocument, string fieldName, string operatorString, string valueString = null)
 		{
 			XElement filterElement = xDocument.Element("fetch").Element("entity").Element("filter");
 
@@ -34,7 +34,7 @@ namespace SystemInterface.Dynamics.Crm
 			}
 		}
 
-		internal static void SetCount(XDocument xDocument, int count)
+		public static void SetCount(XDocument xDocument, int count)
 		{
 			if (xDocument.Element("fetch").Attribute("count") == null)
 			{
