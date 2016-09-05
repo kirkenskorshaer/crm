6191505b3b5f417f6ee9438d12591e5c837cf0f5
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-09-05 14:07:24 +0200

    (Utilities) added DateTimeHelper

---
+++ Utilities/Utilities/DateTimeHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Utilities
+{
+	public static class DateTimeHelper
+	{
+		public static bool Between(DateTime evaluateDateTime, DateTime from, DateTime to)
+		{
+			if (from > to)
+			{
+				return Between(evaluateDateTime, to, from);
+			}
+
+			return from <= evaluateDateTime && evaluateDateTime <= to;
+		}
+	}
+}
