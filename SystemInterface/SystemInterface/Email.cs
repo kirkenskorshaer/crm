23cb34cd3bb88f99064f81786ec58a3237e87fa6
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-10-05 10:34:55 +0200

    (Email) added delevery method for test

--- SystemInterface/SystemInterface/Email.cs
+++ SystemInterface/SystemInterface/Email.cs
@@ -4,6 +4,9 @@ namespace SystemInterface
 {
 	public class Email
 	{
+		public static System.Net.Mail.SmtpDeliveryMethod DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+		public static string PickupDirectoryLocation = string.Empty;
+
 		public void Send(string messageBody, bool isBodyHtml, string messageSubject, string messageFrom, string messageTo, string smtpHost, int smtpPort, string userName, string password)
 		{
 			System.Net.Mail.MailMessage message = new System.Net.Mail.MailMessage();
@@ -15,6 +18,8 @@ namespace SystemInterface
 
 			System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient(smtpHost, smtpPort);
 			smtp.Credentials = new NetworkCredential(userName, password);
+			smtp.DeliveryMethod = DeliveryMethod;
+			smtp.PickupDirectoryLocation = PickupDirectoryLocation;
 			smtp.Send(message);
 		}
 	}
--- SystemInterfaceTest/EmailTest.cs
+++ SystemInterfaceTest/EmailTest.cs
@@ -20,5 +20,15 @@ namespace SystemInterfaceTest
 
 			email.Send("test", false, "test", config.Email, "svend.l@kirkenskorshaer.dk", config.EmailSmtpHost, config.EmailSmtpPort, config.Email, config.EmailPassword);
 		}
+
+		[Test]
+		public void SendingTestEmailsWillNotSendActualEmails()
+		{
+			Email.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.SpecifiedPickupDirectory;
+			Email.PickupDirectoryLocation = "c:\\test\\email";
+
+			Email email = new Email();
+			email.Send("test", false, "test", "svend.l@kirkenskorshaer.dk", "svend.l@kirkenskorshaer.dk", string.Empty, 0, string.Empty, string.Empty);
+		}
 	}
 }
