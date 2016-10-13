611729ab48338903c0c5d7d464fc29a202302adb
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-10-13 14:28:04 +0200

    (DataLayer) added ImportDanskeBank

---
+++ DataLayer/DataLayer/MongoData/Option/Options/Logic/ImportDanskeBank.cs
@@ -0,0 +1,20 @@
+﻿namespace DataLayer.MongoData.Option.Options.Logic
+{
+	public class ImportDanskeBank : OptionBase
+	{
+		public string urlLoginName { get; set; }
+		public string importFolder { get; set; }
+
+		protected override void Execute(MongoConnection connection, bool recurring)
+		{
+			if (recurring)
+			{
+				Update<ImportDanskeBank>(connection);
+			}
+			else
+			{
+				Delete<ImportDanskeBank>(connection);
+			}
+		}
+	}
+}
