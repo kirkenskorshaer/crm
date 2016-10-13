6b664ea75a67dac804d4bfbbd030a6750b65f363
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-10-13 14:28:46 +0200

    (Administration) added classes to handle iso20022 xml

---
+++ Administration/Administration/Option/Options/Logic/ImportDanskeBankData/Iso20022Document.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Administration.Option.Options.Logic.ImportDanskeBankData
+{
+	public class Iso20022Document
+	{
+		public string IBAN { get; private set; }
+		public decimal TtlNetNtryAmt { get; private set; }
+
+		public List<Iso20022Ntry> Ntries = new List<Iso20022Ntry>();
+
+		private XNamespace _namespace;
+		public Iso20022Document(XDocument bankXml)
+		{
+			_namespace = bankXml.Root.GetDefaultNamespace();
+
+			IEnumerable<XElement> Ntrys;
+
+			if (_namespace.NamespaceName == "urn:iso:std:iso:20022:tech:xsd:camt.053.001.02")
+			{
+				XElement Stmt = bankXml.Element(_namespace + "Document").Element(_namespace + "BkToCstmrStmt").Element(_namespace + "Stmt");
+				IBAN = Stmt.Element(_namespace + "Acct").Element(_namespace + "Id").Element(_namespace + "IBAN").Value;
+
+				Ntrys = Stmt.Elements(_namespace + "Ntry");
+
+				TtlNetNtryAmt = decimal.Parse(Stmt.Element(_namespace + "TxsSummry").Element(_namespace + "TtlNtries").Element(_namespace + "TtlNetNtryAmt").Value);
+			}
+			else
+			{
+				XElement Ntfctn = bankXml.Element(_namespace + "Document").Element(_namespace + "BkToCstmrDbtCdtNtfctn").Element(_namespace + "Ntfctn");
+				IBAN = Ntfctn.Element(_namespace + "Acct").Element(_namespace + "Id").Element(_namespace + "IBAN").Value;
+
+				Ntrys = Ntfctn.Elements(_namespace + "Ntry");
+
+				TtlNetNtryAmt = decimal.Parse(Ntfctn.Element(_namespace + "TxsSummry").Element(_namespace + "TtlNtries").Element(_namespace + "TtlNetNtryAmt").Value);
+			}
+
+			foreach (XElement Ntry in Ntrys)
+			{
+				Iso20022Ntry ntry = new Iso20022Ntry()
+				{
+					BankId = Ntry.Element(_namespace + "AddtlInfInd").Element(_namespace + "MsgNmId").Value,
+					ValDt = XmlConvert.ToDateTime(Ntry.Element(_namespace + "ValDt").Element(_namespace + "Dt").Value, XmlDateTimeSerializationMode.Unspecified),
+					Amt = decimal.Parse(Ntry.Element(_namespace + "Amt").Value, new CultureInfo("en-US")),
+					Ccy = Ntry.Element(_namespace + "Amt").Attribute("Ccy").Value,
+					Prtry = Ntry.Element(_namespace + "NtryDtls")?.Element(_namespace + "TxDtls")?.Element(_namespace + "Purp")?.Element(_namespace + "Prtry")?.Value,
+					BkTxCdDomnCd = Ntry.Element(_namespace + "BkTxCd")?.Element(_namespace + "Domn")?.Element(_namespace + "Cd")?.Value,
+					BkTxCdDomnFmlyCd = Ntry.Element(_namespace + "BkTxCd")?.Element(_namespace + "Domn")?.Element(_namespace + "Fmly")?.Element(_namespace + "Cd")?.Value,
+					BkTxCdDomnFmlySubFmlyCd = Ntry.Element(_namespace + "BkTxCd")?.Element(_namespace + "Domn")?.Element(_namespace + "Fmly")?.Element(_namespace + "SubFmlyCd")?.Value,
+				};
+
+				Ntries.Add(ntry);
+			}
+		}
+	}
+}
---
+++ Administration/Administration/Option/Options/Logic/ImportDanskeBankData/Iso20022Ntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Administration.Option.Options.Logic.ImportDanskeBankData
+{
+	public class Iso20022Ntry
+	{
+		public string BankId;
+		public DateTime ValDt;
+		public decimal Amt;
+		public string Ccy;
+		public string Prtry;
+		public string BkTxCdDomnCd;
+		public string BkTxCdDomnFmlyCd;
+		public string BkTxCdDomnFmlySubFmlyCd;
+    }
+}
