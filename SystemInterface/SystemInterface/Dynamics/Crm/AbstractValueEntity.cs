1aba415a1404924f38a20689d51dacde07ed8074
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-10-13 14:25:12 +0200

    (AbstractValueEntity) implemented GetAsEntity

--- SystemInterface/SystemInterface/Dynamics/Crm/AbstractValueEntity.cs
+++ SystemInterface/SystemInterface/Dynamics/Crm/AbstractValueEntity.cs
@@ -2,6 +2,10 @@
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Query;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Utilities;
 
 namespace SystemInterface.Dynamics.Crm
 {
@@ -40,6 +44,77 @@ namespace SystemInterface.Dynamics.Crm
 
 		protected override string idName { get { return _idName; } }
 
-		protected override CrmEntity GetAsEntity(bool includeId) { throw new NotImplementedException(); }
+		protected override CrmEntity GetAsEntity(bool includeId)
+		{
+			return GetAsEntity(includeId, false);
+		}
+
+		protected CrmEntity GetAsEntity(bool includeId, bool includeNulls)
+		{
+			CrmEntity crmEntity = new CrmEntity(_entityName);
+
+			if (includeId)
+			{
+				crmEntity.Attributes.Add(new KeyValuePair<string, object>(idName, Id));
+				crmEntity.Id = Id;
+			}
+
+			Dictionary<string, object> values = GetEntityValues();
+
+			foreach (KeyValuePair<string, object> keyValue in values)
+			{
+				if (includeNulls == false && ReflectionHelper.IsNullOrWhiteSpace(keyValue.Value))
+				{
+					continue;
+				}
+
+				crmEntity.Attributes.Add(new KeyValuePair<string, object>(keyValue.Key, keyValue.Value));
+			}
+
+			return crmEntity;
+		}
+
+		private Dictionary<string, object> GetEntityValues()
+		{
+			Dictionary<string, object> values = new Dictionary<string, object>();
+
+			List<Type> allowedTypes = new List<Type>()
+			{
+				typeof(EntityReference),
+				typeof(OptionSetValue),
+				typeof(Money),
+				typeof(int?),
+				typeof(string),
+				typeof(decimal?),
+				typeof(bool?),
+				typeof(DateTime?),
+			};
+
+			MemberInfo[] allMembers = GetType().GetMembers();
+			List<MemberInfo> members = allMembers.Where
+			(member =>
+				(
+					member.MemberType == MemberTypes.Field ||
+					member.MemberType == MemberTypes.Property
+				) &&
+				allowedTypes.Any(allowedType => allowedType == ReflectionHelper.GetType(member))
+			).ToList();
+
+			foreach (MemberInfo member in members)
+			{
+				string name = member.Name;
+
+				if (name == "ownerid" || name == "owningbusinessunit")
+				{
+					continue;
+				}
+
+				object value = ReflectionHelper.GetValue(this, member);
+
+				values.Add(name, value);
+			}
+
+			return values;
+		}
 	}
 }
