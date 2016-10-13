0328cfe84dffb3d3a65e81c33375b7e0e584511a
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-10-13 14:30:37 +0200

    (AdministrationTest) testing ImportDanskeBank

---
+++ AdministrationTest/Option/Options/Logic/ImportDanskeBankTest.cs
@@ -0,0 +1,35 @@
+﻿using Administration.Option.Options.Logic;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatabaseImportDanskeBank = DataLayer.MongoData.Option.Options.Logic.ImportDanskeBank;
+
+namespace AdministrationTest.Option.Options.Logic
+{
+	[TestFixture]
+	public class ImportDanskeBankTest : TestBase
+	{
+		[Test]
+		public void test()
+		{
+			DatabaseImportDanskeBank databaseImportDanskeBank = CreateDatabaseImportDanskeBank();
+
+			ImportDanskeBank importDanskeBank = new ImportDanskeBank(Connection, databaseImportDanskeBank);
+
+			importDanskeBank.Execute();
+		}
+
+		private DatabaseImportDanskeBank CreateDatabaseImportDanskeBank()
+		{
+			return new DatabaseImportDanskeBank()
+			{
+				importFolder = "danskebank",
+				Schedule = CreateScheduleAlwaysOnDoOnce(),
+				urlLoginName = "test",
+			};
+		}
+	}
+}
