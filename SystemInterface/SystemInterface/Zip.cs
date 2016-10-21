0b68257cd524db0b37728118ae5329e057b78abf
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-10-21 10:23:24 +0200

    (Zip) strings can now be compressed without saving them to a file

--- SystemInterface/SystemInterface/Zip.cs
+++ SystemInterface/SystemInterface/Zip.cs
@@ -1,4 +1,7 @@
 ﻿using Ionic.Zip;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
 
 namespace SystemInterface
 {
@@ -19,6 +22,51 @@ namespace SystemInterface
 			{
 				zipFile.ExtractAll(targetFolderName);
 			}
+		}
+
+		public byte[] CompressString(string uncompressed)
+		{
+			byte[] output;
+
+			using (MemoryStream memoryStreamInput = new MemoryStream())
+			{
+				StreamWriter writer = new StreamWriter(memoryStreamInput);
+				writer.Write(uncompressed);
+				writer.Flush();
+
+				memoryStreamInput.Position = 0;
+
+				using (MemoryStream memoryStreamOutput = new MemoryStream())
+				{
+					using (GZipStream gZipStream = new GZipStream(memoryStreamOutput, CompressionMode.Compress))
+					{
+						memoryStreamInput.CopyTo(gZipStream);
+					}
+					output = memoryStreamOutput.ToArray();
+				}
+			}
+
+			return output;
+		}
+
+		public string DecompressString(byte[] compressed)
+		{
+			string output;
+
+			using (MemoryStream memoryStreamInput = new MemoryStream(compressed))
+			{
+				using (GZipStream gZipStream = new GZipStream(memoryStreamInput, CompressionMode.Decompress))
+				{
+					using (MemoryStream memoryStreamOutput = new MemoryStream())
+					{
+						gZipStream.CopyTo(memoryStreamOutput);
+						output = Encoding.UTF8.GetString(memoryStreamOutput.ToArray());
+					}
+
+				}
+			}
+
+			return output;
 		}
 	}
 }
