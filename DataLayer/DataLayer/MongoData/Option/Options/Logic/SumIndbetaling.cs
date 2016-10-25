32ac18cd4353d842764ed26fc5151acc0c22a54a
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-10-25 15:03:29 +0200

    (DataLayer) adding SumIndbetaling

---
+++ DataLayer/DataLayer/MongoData/Option/Options/Logic/SumIndbetaling.cs
@@ -0,0 +1,31 @@
+﻿namespace DataLayer.MongoData.Option.Options.Logic
+{
+	public class SumIndbetaling : OptionBase
+	{
+		public string urlLoginName { get; set; }
+
+		public static SumIndbetaling Create(MongoConnection mongoConnection, string urlLoginName, string name, Schedule schedule)
+		{
+			SumIndbetaling sumIndbetaling = new SumIndbetaling()
+			{
+				urlLoginName = urlLoginName,
+			};
+
+			Create(mongoConnection, sumIndbetaling, name, schedule);
+
+			return sumIndbetaling;
+		}
+
+		protected override void Execute(MongoConnection connection, bool recurring)
+		{
+			if (recurring)
+			{
+				Update<SumIndbetaling>(connection);
+			}
+			else
+			{
+				Delete<SumIndbetaling>(connection);
+			}
+		}
+	}
+}
