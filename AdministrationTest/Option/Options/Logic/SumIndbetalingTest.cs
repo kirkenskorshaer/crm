20c0804cb98aa02309130ff668835d365cc611b2
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-10-26 09:58:23 +0200

    (AdministrationTest) calling SumIndbetaling from test

---
+++ AdministrationTest/Option/Options/Logic/SumIndbetalingTest.cs
@@ -0,0 +1,29 @@
+﻿using Administration.Option.Options.Logic;
+using NUnit.Framework;
+using DatabaseSumIndbetaling = DataLayer.MongoData.Option.Options.Logic.SumIndbetaling;
+
+namespace AdministrationTest.Option.Options.Logic
+{
+	[TestFixture]
+	public class SumIndbetalingTest : TestBase
+	{
+		[Test]
+		public void test()
+		{
+			DatabaseSumIndbetaling databaseSumIndbetaling = CreateDatabaseSumIndbetaling();
+
+			SumIndbetaling sumIndbetaling = new SumIndbetaling(Connection, databaseSumIndbetaling);
+
+			sumIndbetaling.Execute();
+		}
+
+		private DatabaseSumIndbetaling CreateDatabaseSumIndbetaling()
+		{
+			return new DatabaseSumIndbetaling()
+			{
+				Schedule = CreateScheduleAlwaysOnDoOnce(),
+				urlLoginName = "test",
+			};
+		}
+	}
+}
