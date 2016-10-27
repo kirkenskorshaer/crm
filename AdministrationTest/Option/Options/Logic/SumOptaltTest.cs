6290f4daf210f3c3f99d3a8859ec2cedc043f086
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-10-27 10:16:00 +0200

    (AdministrationTest) testing SumOptalt

---
+++ AdministrationTest/Option/Options/Logic/SumOptaltTest.cs
@@ -0,0 +1,31 @@
+﻿using Administration.Option.Options.Logic;
+using NUnit.Framework;
+using System;
+using DatabaseSumOptalt = DataLayer.MongoData.Option.Options.Logic.SumOptalt;
+
+namespace AdministrationTest.Option.Options.Logic
+{
+	[TestFixture]
+	public class SumOptaltTest : TestBase
+	{
+		[Test]
+		public void test()
+		{
+			DatabaseSumOptalt databaseSumOptalt = CreateDatabaseSumOptalt();
+
+			SumOptalt sumOptalt = new SumOptalt(Connection, databaseSumOptalt);
+
+			sumOptalt.Execute();
+		}
+
+		private DatabaseSumOptalt CreateDatabaseSumOptalt()
+		{
+			return new DatabaseSumOptalt()
+			{
+				Schedule = CreateScheduleAlwaysOnDoOnce(),
+				urlLoginName = "test",
+				campaignid = Guid.Parse("ff052597-5538-e611-80ef-001c4215c4a0"),
+			};
+		}
+	}
+}
