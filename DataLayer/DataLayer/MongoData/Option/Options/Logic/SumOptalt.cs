d2a5408757c47436bf93a3be050e886926ae2367
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-10-27 10:09:41 +0200

    (DataLayer) added SumOptalt

---
+++ DataLayer/DataLayer/MongoData/Option/Options/Logic/SumOptalt.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace DataLayer.MongoData.Option.Options.Logic
+{
+	public class SumOptalt : OptionBase
+	{
+		public string urlLoginName { get; set; }
+		public decimal? kreds { get; set; }
+		public decimal? by { get; set; }
+		public decimal? total { get; set; }
+
+		[BsonRepresentation(BsonType.String)]
+		public Guid campaignid { get; set; }
+
+		protected override void Execute(MongoConnection connection, bool recurring)
+		{
+			if (recurring)
+			{
+				Update<SumOptalt>(connection);
+			}
+			else
+			{
+				Delete<SumOptalt>(connection);
+			}
+		}
+	}
+}
