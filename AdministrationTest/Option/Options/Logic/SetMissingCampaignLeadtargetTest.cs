6c0819916641797966b241359bbd9bdbeef9ff72
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-11-03 11:43:24 +0100

    (AdministrationTest) testing option SetMissingCampaignLeadtarget

---
+++ AdministrationTest/Option/Options/Logic/SetMissingCampaignLeadtargetTest.cs
@@ -0,0 +1,30 @@
+﻿using Administration.Option.Options.Logic;
+using NUnit.Framework;
+using DatabaseSetMissingCampaignLeadtarget = DataLayer.MongoData.Option.Options.Logic.SetMissingCampaignLeadtarget;
+
+namespace AdministrationTest.Option.Options.Logic
+{
+	[TestFixture]
+	public class SetMissingCampaignLeadtargetTest : TestBase
+	{
+		[Test]
+		public void test()
+		{
+			DatabaseSetMissingCampaignLeadtarget databaseSetMissingCampaignLeadtarget = CreateDatabaseSetMissingCampaignLeadtarget();
+
+			SetMissingCampaignLeadtarget setMissingCampaignLeadtarget = new SetMissingCampaignLeadtarget(Connection, databaseSetMissingCampaignLeadtarget);
+
+			setMissingCampaignLeadtarget.Execute();
+		}
+
+		private DatabaseSetMissingCampaignLeadtarget CreateDatabaseSetMissingCampaignLeadtarget()
+		{
+			return new DatabaseSetMissingCampaignLeadtarget()
+			{
+				Schedule = CreateScheduleAlwaysOnDoOnce(),
+				urlLoginName = "test",
+				baseUrl = "https://webinterfacetest.korsnet.dk/stub.aspx",
+			};
+		}
+	}
+}
