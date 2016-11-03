ca955ad7ccff5e04afb561033ae9e534779c478e
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-11-03 11:33:01 +0100

    (DataLayer) added SetMissingCampaignLeadtarget

---
+++ DataLayer/DataLayer/MongoData/Option/Options/Logic/SetMissingCampaignLeadtarget.cs
@@ -0,0 +1,20 @@
+﻿namespace DataLayer.MongoData.Option.Options.Logic
+{
+	public class SetMissingCampaignLeadtarget : OptionBase
+	{
+		public string urlLoginName { get; set; }
+		public string baseUrl { get; set; }
+
+		protected override void Execute(MongoConnection connection, bool recurring)
+		{
+			if (recurring)
+			{
+				Update<SetMissingCampaignLeadtarget>(connection);
+			}
+			else
+			{
+				Delete<SetMissingCampaignLeadtarget>(connection);
+			}
+		}
+	}
+}
