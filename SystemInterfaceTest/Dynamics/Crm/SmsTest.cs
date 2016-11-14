aa89027f740e9e5295edcb9f21d6b2daab4ceb5d
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-11-14 13:37:19 +0100

    (SystemInterfaceTest) adding simple sms test

---
+++ SystemInterfaceTest/Dynamics/Crm/SmsTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SystemInterface.Dynamics.Crm;
+using Utilities;
+
+namespace SystemInterfaceTest.Dynamics.Crm
+{
+	[TestFixture]
+	public class SmsTest : TestBase
+	{
+		[Test]
+		public void GetTemplate()
+		{
+			SmsTemplate template = SmsTemplate.GetWaitingTemplate(_dynamicsCrmConnection);
+
+			Console.Out.WriteLine(template.new_text);
+			Console.Out.WriteLine(template.Id);
+			Console.Out.WriteLine(template.new_fetchxml);
+
+			Sms sms = Sms.GetWaitingSmsOnTemplate(_dynamicsCrmConnection, template.Id).Single();
+
+			//Console.Out.WriteLine(sms.mobilephone);
+			//Console.Out.WriteLine(sms.Id);
+			//Console.Out.WriteLine(sms.toid);
+
+			IDictionary<string, object> fields = template.GetFields(sms.contactid.Value);
+
+			/*
+			foreach (KeyValuePair<string, object> content in fields)
+			{
+				Console.Out.WriteLine($"{content.Key} : {content.Value.ToString()}");
+			}
+			*/
+
+			TextMerger textMerger = new TextMerger(template.new_text);
+			string output = textMerger.GetMerged(fields, "yyyy-MM-dd");
+
+			Console.Out.WriteLine(output);
+		}
+	}
+}
