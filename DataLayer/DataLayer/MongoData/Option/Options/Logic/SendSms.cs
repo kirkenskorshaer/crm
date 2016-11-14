5af51b8f5ba1bd3908b60ca4703d3638e44a0780
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-11-14 13:39:08 +0100

    (DataLayer) adding SendSms

---
+++ DataLayer/DataLayer/MongoData/Option/Options/Logic/SendSms.cs
@@ -0,0 +1,19 @@
+﻿namespace DataLayer.MongoData.Option.Options.Logic
+{
+	public class SendSms : OptionBase
+	{
+		public string urlLoginName { get; set; }
+
+		protected override void Execute(MongoConnection connection, bool recurring)
+		{
+			if (recurring)
+			{
+				Update<SendSms>(connection);
+			}
+			else
+			{
+				Delete<SendSms>(connection);
+			}
+		}
+	}
+}
