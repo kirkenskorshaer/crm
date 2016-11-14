cf9b287dcd7d9106b245de4f1263919cca5eb31c
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-11-14 13:16:48 +0100

    (SystemInterface) adding Sms and SmsTemplate

---
+++ SystemInterface/SystemInterface/Dynamics/Crm/Sms.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xrm.Sdk;
+using System.Xml.Linq;
+
+namespace SystemInterface.Dynamics.Crm
+{
+	public class Sms : AbstractValueEntity
+	{
+		public string mobilephone;
+		public DateTime? new_sendtime;
+		public DateTime? new_operatorsendtime;
+
+		public EntityReference new_contactid;
+		public Guid? contactid { get { return GetEntityReferenceId(new_contactid); } set { new_contactid = SetEntityReferenceId(value, "contact"); } }
+
+		public Sms(IDynamicsCrmConnection connection, Entity crmEntity) : base(connection, crmEntity, "new_sms", "new_smsid")
+		{
+		}
+
+		public static IEnumerable<Sms> GetWaitingSmsOnTemplate(IDynamicsCrmConnection _dynamicsCrmConnection, Guid templateId)
+		{
+			XDocument xDocument = new XDocument(
+				new XElement("fetch",
+					new XElement("entity", new XAttribute("name", "new_sms"),
+						new XElement("attribute", new XAttribute("name", "new_smsid")),
+						new XElement("attribute", new XAttribute("name", "new_sendtime")),
+						new XElement("attribute", new XAttribute("name", "new_operatorsendtime")),
+						new XElement("attribute", new XAttribute("name", "new_contactid")),
+						new XElement("filter", new XAttribute("type", "and"),
+							new XElement("condition", new XAttribute("attribute", "new_direction"), new XAttribute("operator", "eq"), new XAttribute("value", "0")),
+							new XElement("condition", new XAttribute("attribute", "new_sendstatus"), new XAttribute("operator", "eq"), new XAttribute("value", "100000000")),
+							new XElement("condition", new XAttribute("attribute", "new_smstemplateid"), new XAttribute("operator", "eq"), new XAttribute("value", templateId)),
+							new XElement("filter", new XAttribute("type", "or"),
+								new XElement("condition", new XAttribute("attribute", "new_sendtime"), new XAttribute("operator", "null")),
+								new XElement("condition", new XAttribute("attribute", "new_sendtime"), new XAttribute("operator", "le"), new XAttribute("value", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")))
+							)
+						),
+						new XElement("link-entity", new XAttribute("name", "contact"), new XAttribute("from", "contactid"), new XAttribute("to", "new_contactid"), new XAttribute("link-type", "inner"),
+							new XElement("attribute", new XAttribute("name", "mobilephone"), new XAttribute("alias", "mobilephone")),
+							new XElement("filter", new XAttribute("type", "and"),
+								new XElement("condition", new XAttribute("attribute", "mobilephone"), new XAttribute("operator", "not-null"))
+							)
+						)
+					)
+				)
+			);
+
+			IEnumerable<Sms> smsCollection = StaticCrm.ReadFromFetchXml(_dynamicsCrmConnection, xDocument, (dynamicsCrmConnection, entity) => new Sms(dynamicsCrmConnection, entity));
+
+			return smsCollection;
+		}
+
+		public void MarkAsSent(string text, string externalId)
+		{
+			Update(Connection, entityName, idName, Id, new Dictionary<string, object>()
+			{
+				{ "new_text", text },
+				{ "new_externalid", externalId },
+				{ "new_sendtime", new_sendtime },
+				{ "new_operatorsendtime", new_operatorsendtime },
+				{ "new_sendstatus", new OptionSetValue(100000002) },
+			});
+		}
+
+		public void UpdateStatus(string operatorstatus)
+		{
+			Update(Connection, entityName, idName, Id, new Dictionary<string, object>()
+			{
+				{ "new_operatorstatus", operatorstatus },
+			});
+		}
+	}
+}
---
+++ SystemInterface/SystemInterface/Dynamics/Crm/SmsTemplate.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace SystemInterface.Dynamics.Crm
+{
+	public class SmsTemplate : AbstractValueEntity
+	{
+		public string new_text;
+		public string new_fetchxml;
+
+		public SmsTemplate(IDynamicsCrmConnection connection, Entity crmEntity) : base(connection, crmEntity, "new_smstemplate", "new_smstemplateid")
+		{
+		}
+
+		public static SmsTemplate GetWaitingTemplate(IDynamicsCrmConnection _dynamicsCrmConnection)
+		{
+			XDocument xDocument = new XDocument(
+				new XElement("fetch", new XAttribute("count", 1),
+					new XElement("entity", new XAttribute("name", "new_smstemplate"),
+						new XElement("attribute", new XAttribute("name", "new_text")),
+						new XElement("attribute", new XAttribute("name", "new_smstemplateid")),
+						new XElement("attribute", new XAttribute("name", "new_fetchxml")),
+						new XElement("link-entity", new XAttribute("name", "new_sms"), new XAttribute("from", "new_smstemplateid"), new XAttribute("to", "new_smstemplateid"), new XAttribute("link-type", "inner"),
+							new XElement("filter", new XAttribute("type", "and"),
+								new XElement("condition", new XAttribute("attribute", "new_direction"), new XAttribute("operator", "eq"), new XAttribute("value", "0")),
+								new XElement("condition", new XAttribute("attribute", "new_sendstatus"), new XAttribute("operator", "eq"), new XAttribute("value", "100000000")),
+								new XElement("filter", new XAttribute("type", "or"),
+									new XElement("condition", new XAttribute("attribute", "new_sendtime"), new XAttribute("operator", "null")),
+									new XElement("condition", new XAttribute("attribute", "new_sendtime"), new XAttribute("operator", "le"), new XAttribute("value", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")))
+								)
+							),
+							new XElement("link-entity", new XAttribute("name", "contact"), new XAttribute("from", "contactid"), new XAttribute("to", "new_contactid"), new XAttribute("link-type", "inner"),
+								new XElement("filter", new XAttribute("type", "and"),
+									new XElement("condition", new XAttribute("attribute", "mobilephone"), new XAttribute("operator", "not-null"))
+								)
+							)
+						)
+					)
+				)
+			);
+
+			SmsTemplate template = StaticCrm.ReadFromFetchXml(_dynamicsCrmConnection, xDocument, (dynamicsCrmConnection, entity) => new SmsTemplate(dynamicsCrmConnection, entity)).SingleOrDefault();
+
+			return template;
+		}
+
+		public IDictionary<string, object> GetFields(Guid contactId)
+		{
+			if (string.IsNullOrWhiteSpace(new_fetchxml))
+			{
+				return new Dictionary<string, object>();
+			}
+
+			XDocument xDocument = XDocument.Parse(new_fetchxml);
+
+			XmlHelper.AddCondition(xDocument, "contactid", "eq", contactId.ToString());
+
+			List<dynamic> receivers = DynamicFetch.ReadFromFetchXml(Connection, xDocument, new PagingInformation());
+
+			if (receivers == null)
+			{
+				return new Dictionary<string, object>();
+			}
+
+			if (receivers.Count != 1)
+			{
+				throw new Exception($"sms template error for template {Id}");
+			}
+
+			IDictionary<string, object> receiverDictionary = (IDictionary<string, object>)receivers.Single();
+
+			return receiverDictionary;
+		}
+	}
+}
