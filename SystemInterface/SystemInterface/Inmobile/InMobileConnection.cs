5785db9c6d73b8081f1e6a29196a879c102adaf0
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-11-14 13:15:41 +0100

    (SystemInterface) implementing inmobile connection

---
+++ SystemInterface/SystemInterface/Inmobile/InMobileConnection.cs
@@ -0,0 +1,49 @@
+﻿using Sms.ApiClient.V2;
+using Sms.ApiClient.V2.GetMessageStatuses;
+using Sms.ApiClient.V2.SendMessages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SystemInterface.Inmobile
+{
+	public class InMobileConnection : AbstractInMobileConnection
+	{
+		public InMobileConnection(string apiKey, string getMessagesGetUrl, string messageStatusCallbackUrl, string postUrl) : base(apiKey, getMessagesGetUrl, messageStatusCallbackUrl, postUrl)
+		{
+		}
+
+		public override void Send(Dictionary<string, InMobileSms> inMobileSmsMessages)
+		{
+			List<ISmsMessage> messages = inMobileSmsMessages.Select(message => message.Value.ToInmobileSms(message.Key)).ToList();
+
+			SendMessagesRequestBuilder requestBuilder = new SendMessagesRequestBuilder();
+			SendMessagesClient client = new SendMessagesClient(ApiKey, PostUrl, requestBuilder);
+
+			SendMessagesResponse response = client.SendMessages(messages, MessageStatusCallbackUrl);
+
+			List<MsisdnAndMessageId> responseIds = response.MessageIds;
+
+			responseIds.ForEach(msisdnAndId => inMobileSmsMessages[msisdnAndId.Msisdn].MessageId = msisdnAndId.MessageId);
+		}
+
+		public override void GetMessageStatus(Action<string, int, string> receiveStatus)
+		{
+			GetMessageStatusClient client = new GetMessageStatusClient(ApiKey, GetMessagesGetUrl);
+			GetMessageStatusesResponse response = client.GetMessageStatus();
+
+			foreach (MessageStatus status in response.MessageStatuses)
+			{
+				receiveStatus(status.MessageId, status.StatusCode, status.StatusDescription);
+			}
+		}
+
+		public override string Version
+		{
+			get
+			{
+				return ClientUtils.VersionNumber;
+			}
+		}
+	}
+}
---
+++ SystemInterface/SystemInterface/Inmobile/InMobileSms.cs
@@ -0,0 +1,47 @@
+﻿using Sms.ApiClient.V2.SendMessages;
+using System;
+using System.Text;
+
+namespace SystemInterface.Inmobile
+{
+	public class InMobileSms
+	{
+		public string Text { get; private set; }
+		public string SenderName { get; private set; }
+		public string MessageId { get; set; }
+		public DateTime? SendTime { get; private set; }
+
+		public object LocalSms;
+
+		public InMobileSms(string text, string senderName, DateTime? sendTime)
+		{
+			Text = text;
+			SenderName = senderName;
+			SendTime = sendTime;
+		}
+
+		public static string GetMsisdn(string crmNumber)
+		{
+			string trimmedNumber = crmNumber.Replace(" ", "");
+
+			if (trimmedNumber.Length == 8)
+			{
+				return "45" + trimmedNumber;
+			}
+
+			return trimmedNumber;
+		}
+
+		internal ISmsMessage ToInmobileSms(string msisdn)
+		{
+			ISmsMessage message = new SmsMessage(msisdn, Text, SenderName, SmsEncoding.Utf8, MessageId, SendTime, false);
+
+			return message;
+		}
+
+		public override string ToString()
+		{
+			return $"Text:{Text} SenderName:{SenderName} MessageId:{MessageId} SendTime:{SendTime?.ToString("yyyy-MM-dd HH:mm:ss")}";
+		}
+	}
+}
