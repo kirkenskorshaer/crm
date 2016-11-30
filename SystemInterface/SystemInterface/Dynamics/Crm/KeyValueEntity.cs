6813464f52287803ddc7cf5c91fa5bdf0742965f
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-11-30 08:28:04 +0100

    (SystemInterface) adding GetAll methods to KeyValueEntity and SingleValueEntity

--- SystemInterface/SystemInterface/Dynamics/Crm/KeyValueEntity.cs
+++ SystemInterface/SystemInterface/Dynamics/Crm/KeyValueEntity.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Xrm.Sdk;
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+using Microsoft.Xrm.Sdk;
+using System.Linq;
 
 namespace SystemInterface.Dynamics.Crm
 {
@@ -9,6 +12,13 @@ namespace SystemInterface.Dynamics.Crm
 
 		public KeyValueEntity(IDynamicsCrmConnection connection, Entity crmEntity, string entityName, string idName) : base(connection, crmEntity, entityName, idName)
 		{
+		}
+
+		public static List<KeyValuePair<KeyType, ValueType>> GetAll(IDynamicsCrmConnection dynamicsCrmConnection, XDocument xDocument, string entityName, string entityIdName)
+		{
+			List<KeyValueEntity<KeyType, ValueType>> keyValueEntities = StaticCrm.ReadFromFetchXml(dynamicsCrmConnection, xDocument, (LDynamicsCrmConnection, entity) => new KeyValueEntity<KeyType, ValueType>(dynamicsCrmConnection, entity, entityName, entityIdName), new PagingInformation());
+
+			return keyValueEntities.Select(keyValue => new KeyValuePair<KeyType, ValueType>(keyValue.key, keyValue.value)).ToList();
 		}
 	}
 }
--- SystemInterface/SystemInterface/Dynamics/Crm/SingleValueEntity.cs
+++ SystemInterface/SystemInterface/Dynamics/Crm/SingleValueEntity.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Xrm.Sdk;
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+using Microsoft.Xrm.Sdk;
+using System.Linq;
 
 namespace SystemInterface.Dynamics.Crm
 {
@@ -8,6 +11,13 @@ namespace SystemInterface.Dynamics.Crm
 
 		public SingleValueEntity(IDynamicsCrmConnection connection, Entity crmEntity, string entityName, string idName) : base(connection, crmEntity, entityName, idName)
 		{
+		}
+
+		public static List<ValueType> GetAll(IDynamicsCrmConnection dynamicsCrmConnection, XDocument xDocument, string entityName, string entityIdName)
+		{
+			IEnumerable<ValueType> entities = StaticCrm.ReadFromFetchXml(dynamicsCrmConnection, xDocument, (lDynamicsCrmConnection, entity) => new SingleValueEntity<ValueType>(lDynamicsCrmConnection, entity, entityName, entityIdName)).Select(valueEntity => valueEntity.value);
+
+			return entities.ToList();
 		}
 	}
 }
