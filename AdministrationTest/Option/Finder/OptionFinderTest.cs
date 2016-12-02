a87c1eebb238952cd61db1ecb1173236e7868074
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-12-02 13:27:42 +0100

    (AdministrationTest) testing OptionFinder

---
+++ AdministrationTest/Option/Finder/OptionFinderTest.cs
@@ -0,0 +1,29 @@
+﻿using Administration.Option;
+using Administration.Option.Finder;
+using Administration.Option.Options;
+using Administration.Option.Options.Logic;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using DatabaseExposeData = DataLayer.MongoData.Option.Options.Logic.ExposeData;
+
+namespace AdministrationTest.Option.Finder
+{
+	[TestFixture]
+	public class OptionFinderTest : TestBase
+	{
+		[Test]
+		public void FindFindsOption()
+		{
+			OptionFinder finder = new OptionFinder(Connection);
+
+			DatabaseExposeData.Create(Connection, "test", "test", CreateScheduleAlwaysOnDoOnce(), CreateScheduleAlwaysOnDoOnce());
+
+			List<OptionBase> options = finder.Find();
+
+			Assert.True(options.Count == 2);
+			Assert.True(options.Any(option => option.GetType() == typeof(ExposeData)));
+			Assert.True(options.Any(option => option.GetType() == typeof(Sleep)));
+		}
+	}
+}
