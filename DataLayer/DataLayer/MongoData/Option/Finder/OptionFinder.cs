8964068173a4e34eb4fe900aafb8fb01e5655dca
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-12-02 13:22:44 +0100

    (DataLayer) adding OptionFinder

---
+++ DataLayer/DataLayer/MongoData/Option/Finder/OptionFinder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using Utilities;
+
+namespace DataLayer.MongoData.Option.Finder
+{
+	public class OptionFinder
+	{
+		private readonly MongoConnection _connection;
+		public OptionFinder(MongoConnection connection)
+		{
+			_connection = connection;
+		}
+
+		public List<OptionBase> Find()
+		{
+			List<Type> optionTypes = ReflectionHelper.GetChildTypes(typeof(OptionBase));
+
+			List<OptionBase> options = new List<OptionBase>();
+
+			foreach (Type optionType in optionTypes)
+			{
+				MethodInfo method = typeof(OptionBase).GetMethod("ReadAllowed");
+				MethodInfo generic = method.MakeGenericMethod(optionType);
+				IEnumerable optionsOnCurrentType = (IEnumerable)generic.Invoke(null, new object[] { _connection });
+
+				foreach (object option in optionsOnCurrentType)
+				{
+					options.Add((OptionBase)option);
+				}
+			}
+
+			return options;
+		}
+	}
+}
