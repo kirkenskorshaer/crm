62ccacd9dc0eb2e24ca103516ee6d248a11e2fb5
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-12-02 13:23:43 +0100

    (DataLayer) testing OptionFinder

---
+++ DataLayerTest/MongoDataTest/Option/Finder/OptionFinderTest.cs
@@ -0,0 +1,25 @@
+﻿using DataLayer.MongoData.Option;
+using DataLayer.MongoData.Option.Finder;
+using DataLayer.MongoData.Option.Options;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataLayerTest.MongoDataTest.Option.Finder
+{
+	[TestFixture]
+	public class OptionFinderTest : TestBase
+	{
+		[Test]
+		public void FindFindsOption()
+		{
+			OptionFinder finder = new OptionFinder(_mongoConnection);
+
+			Email.Create(_mongoConnection, "test", new Schedule(), "", "");
+
+			List<OptionBase> options = finder.Find();
+
+			Assert.True(options.Single().GetType() == typeof(Email));
+		}
+	}
+}
