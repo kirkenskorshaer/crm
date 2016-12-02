fb514545e08cd96f6a523ae37ace6ad8eb290e91
kkkSvend <svend.l@kirkenskorshaer.dk>
2016-12-02 13:26:44 +0100

    (Administration) OptionFinder now uses reflection

--- Administration/Administration/Option/Finder/OptionFinder.cs
+++ Administration/Administration/Option/Finder/OptionFinder.cs
@@ -1,57 +1,49 @@
 ﻿using System.Collections.Generic;
 using Administration.Option.Options;
 using DataLayer;
-using Administration.Option.Options.Logic;
+using Utilities;
+using DatabaseOptionBase = DataLayer.MongoData.Option.OptionBase;
+using DatabaseOptionFinder = DataLayer.MongoData.Option.Finder.OptionFinder;
+using System;
+using System.Linq;
 
 namespace Administration.Option.Finder
 {
 	public class OptionFinder
 	{
 		private readonly MongoConnection _connection;
+		private readonly DatabaseOptionFinder _databaseOptionFinder;
+
 		public OptionFinder(MongoConnection connection)
 		{
 			_connection = connection;
+			_databaseOptionFinder = new DatabaseOptionFinder(_connection);
 		}
 
 		public List<OptionBase> Find()
 		{
 			List<OptionBase> options = new List<OptionBase>();
-			/*
-			options.AddRange(Email.Find(_connection));
 
-			options.AddRange(ServiceCreate.Find(_connection));
-			options.AddRange(ServiceDelete.Find(_connection));
-			options.AddRange(ServiceStart.Find(_connection));
-			options.AddRange(ServiceStop.Find(_connection));
+			List<DatabaseOptionBase> databaseOptions = _databaseOptionFinder.Find();
 
-			options.AddRange(CsvWriteLine.Find(_connection));
-			options.AddRange(CsvRead.Find(_connection));
-			options.AddRange(CsvUpdate.Find(_connection));
-			options.AddRange(CsvDelete.Find(_connection));
+			List<Type> optionTypes = ReflectionHelper.GetChildTypes(typeof(OptionBase));
 
-			options.AddRange(MaintainProgress.Find(_connection));
-			options.AddRange(Squash.Find(_connection));
-			options.AddRange(StressTestCrm.Find(_connection));
-			options.AddRange(SynchronizeFromCrm.Find(_connection));
-			options.AddRange(SynchronizeFromCsv.Find(_connection));
-			options.AddRange(SynchronizeToCrm.Find(_connection));
-			options.AddRange(AdjustDns.Find(_connection));
-			*/
-			options.AddRange(ImportFromStub.Find(_connection));
-			options.AddRange(CreateImportFromStub.Find(_connection));
-			options.AddRange(MaterialeBehovAssignment.Find(_connection));
-			options.AddRange(ExposeData.Find(_connection));
-			options.AddRange(AddMailrelaySubscriberFromLead.Find(_connection));
-			options.AddRange(UpdateMailrelayFromContact.Find(_connection));
-			options.AddRange(UpdateMailrelayGroup.Find(_connection));
-			options.AddRange(ExportContactToMailrelay.Find(_connection));
-			options.AddRange(SendTableFromMailrelay.Find(_connection));
-			options.AddRange(ImportDanskeBank.Find(_connection));
-			options.AddRange(SumIndbetaling.Find(_connection));
-			options.AddRange(SumOptalt.Find(_connection));
-			options.AddRange(SetMissingCampaignLeadtarget.Find(_connection));
-			options.AddRange(AggregateSubTable.Find(_connection));
-			//options.AddRange(MaintainAllTables.Find(_connection));
+			foreach (DatabaseOptionBase databaseOption in databaseOptions)
+			{
+				string optionName = databaseOption.GetType().Name;
+
+				Type optionType = optionTypes.SingleOrDefault(type => type.Name == optionName);
+
+				if (optionType == null)
+				{
+					Log.WriteLocation(_connection, $"Unknown option type {optionType}", "OptionFinder", DataLayer.MongoData.Config.LogLevelEnum.OptionError);
+					continue;
+				}
+
+				OptionBase option = (OptionBase)Activator.CreateInstance(optionType, _connection, databaseOption);
+
+				options.Add(option);
+			}
 
 			Sleep sleep = new Sleep(_connection);
 			options.Add(sleep);
