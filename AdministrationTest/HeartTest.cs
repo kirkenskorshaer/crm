47344bb296d24921ad2d373b11cfdf875d6a2035
kkkSvend <svend.l@kirkenskorshaer.dk>
2017-01-19 14:44:35 +0100

    (AdministrationTest) adding ignored test for HeartBeat with several options

--- AdministrationTest/HeartTest.cs
+++ AdministrationTest/HeartTest.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Configuration;
 using System.Diagnostics;
 using Administration;
-using DataLayer;
-using DataLayer.MongoData;
 using DataLayer.MongoData.Option;
 using DataLayer.MongoData.Option.Options;
 using NUnit.Framework;
+using DataLayer.MongoData.Option.Options.Logic;
+using System.Threading;
 
 namespace AdministrationTest
 {
@@ -38,11 +37,66 @@ namespace AdministrationTest
 		{
 			Heart heart = new Heart();
 
+			heart.Begin();
+
 			Stopwatch stopwatch = Stopwatch.StartNew();
 			heart.HeartBeat();
 			stopwatch.Stop();
 
+			heart.End();
+
 			Assert.Greater(stopwatch.ElapsedMilliseconds, 100);
+		}
+
+		[Test]
+		[Ignore]
+		public void HeartBeatWithOptions()
+		{
+			Heart heart = new Heart();
+
+			heart.Begin();
+
+			AddExposeDataOption(Schedule.ActionOnFailEnum.Disable);
+			AddExposeDataOption(Schedule.ActionOnFailEnum.TryAgain);
+			AddExposeDataOption(Schedule.ActionOnFailEnum.TryAgain);
+			AddExposeDataOption(Schedule.ActionOnFailEnum.TryAgain);
+			AddExposeDataOption(Schedule.ActionOnFailEnum.TryAgain);
+
+			AddTestOption(Schedule.ActionOnFailEnum.TryAgain, 10, true);
+			AddTestOption(Schedule.ActionOnFailEnum.TryAgain, 10, false);
+			AddTestOption(Schedule.ActionOnFailEnum.TryAgain, 10, false);
+			AddTestOption(Schedule.ActionOnFailEnum.TryAgain, 10, true);
+
+			do
+			{
+				heart.HeartBeat();
+
+				Thread.Sleep(TimeSpan.FromSeconds(5));
+			}
+			while (heart.IsAnyThreadHolderBusy() || heart.IsWorkQueued());
+
+			heart.End();
+		}
+
+		public void AddExposeDataOption(Schedule.ActionOnFailEnum actionOnFail)
+		{
+			ExposeData exposeData = ExposeData.Create(Connection, "test", "test", CreateScheduleAlwaysOnDoOnce());
+
+			exposeData.exposeName = "test.json";
+			exposeData.exposePath = "heartTest";
+			exposeData.fetchXmlPath = "Dynamics/Crm/FetchXml/Account/plant.xml";
+			exposeData.Schedule.ActionOnFail = actionOnFail;
+
+			exposeData.UpdateOption(Connection);
+		}
+
+		public void AddTestOption(Schedule.ActionOnFailEnum actionOnFail, int sleepForSeconds, bool throwException)
+		{
+			Test test = Test.Create(Connection, "test", CreateScheduleAlwaysOnDoOnce(), sleepForSeconds, throwException);
+
+			test.Schedule.ActionOnFail = actionOnFail;
+
+			test.UpdateOption(Connection);
 		}
 
 		[Test]
