70f10930e1e1fa56b4df07c919667c2e7647b245
kkkSvend <svend.l@kirkenskorshaer.dk>
2017-01-19 14:14:38 +0100

    (DataLayerTest) testing assign workers

--- DataLayerTest/MongoDataTest/Option/ScheduleTest.cs
+++ DataLayerTest/MongoDataTest/Option/ScheduleTest.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using DataLayer.MongoData.Option;
 using NUnit.Framework;
+using DataLayer.MongoData.Option.Options.Logic;
+using DataLayer.MongoData;
+using System.Linq;
 
 namespace DataLayerTest.MongoDataTest.Option
 {
 	[TestFixture]
-	public class ScheduleTest
+	public class ScheduleTest : TestBase
 	{
 		[TestCase(25, 0, 67, 25, 1, 7)]
 		[TestCase(22, 0, 67, 24, 0, 0)]
@@ -29,6 +32,24 @@ namespace DataLayerTest.MongoDataTest.Option
 			schedule.MoveNext();
 
 			Assert.AreEqual(new DateTime(2015, 8, dayExpected, hourExpected, minuteExpected, 0).ToString("s"), schedule.NextAllowedExecution.ToString("s"));
+		}
+
+		[Test]
+		public void AssignWorkerAssignsWorkerToAll()
+		{
+			SumIndbetaling option1 = SumIndbetaling.Create(_mongoConnection, "test", "test", CreateSchedule());
+			SumIndbetaling option2 = SumIndbetaling.Create(_mongoConnection, "test", "test", CreateSchedule());
+			SumIndbetaling option3 = SumIndbetaling.Create(_mongoConnection, "test", "test", CreateSchedule());
+
+			Worker worker = new Worker();
+
+			worker.Create(_mongoConnection);
+
+			OptionBase.AssignWorkerToAllUnassigned<SumIndbetaling>(worker, _mongoConnection);
+
+			List<SumIndbetaling> optionsRead = OptionBase.ReadAllowed<SumIndbetaling>(_mongoConnection, null);
+
+			Assert.IsTrue(optionsRead.All(option => option.Schedule.WorkerId == worker._id));
 		}
 	}
 }
