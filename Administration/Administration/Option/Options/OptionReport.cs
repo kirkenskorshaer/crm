4eeb64051c3b6eb408405323ce35e61e17e74b9f
kkkSvend <svend.l@kirkenskorshaer.dk>
2017-01-19 14:35:02 +0100

    (Administration) adding more information properties to OptionReport

--- Administration/Administration/Option/Options/OptionReport.cs
+++ Administration/Administration/Option/Options/OptionReport.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Text;
 
 namespace Administration.Option.Options
@@ -10,10 +11,15 @@ namespace Administration.Option.Options
 		public int SubWorkload;
 		public bool Success = false;
 		public StringBuilder TextBuilder = new StringBuilder();
+		public DateTime BeginTime;
+		public DateTime EndTime;
+		public int ProcessId;
+		public long VirtualMemorySize64;
 
 		public OptionReport(string name)
 		{
 			Name = name;
+			BeginTime = DateTime.Now;
 		}
 
 		public void WriteLog(DataLayer.MongoConnection mongoConnection)
@@ -28,6 +34,16 @@ namespace Administration.Option.Options
 			string finalReport = $"S:{Success} W:{Workload} SW:{SubWorkload}{Environment.NewLine}{TextBuilder.ToString()}";
 
 			Log.WriteLocation(mongoConnection, finalReport, Name, logLevel);
+		}
+
+		public void FinishReport()
+		{
+			EndTime = DateTime.Now;
+
+			Process currentProcess = Process.GetCurrentProcess();
+
+			ProcessId = currentProcess.Id;
+			VirtualMemorySize64 = currentProcess.VirtualMemorySize64;
 		}
 	}
 }
