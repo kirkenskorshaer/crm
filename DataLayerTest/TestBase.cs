10c57416faa1e801d987f3f69607db75873cc8fb
kkkSvend <svend.l@kirkenskorshaer.dk>
2017-01-19 14:09:44 +0100

    (DataLayerTest) adding CreateSchedule to TestBase

--- DataLayerTest/TestBase.cs
+++ DataLayerTest/TestBase.cs
@@ -1,5 +1,7 @@
 ﻿using DataLayer;
+using DataLayer.MongoData.Option;
 using NUnit.Framework;
+using System;
 
 namespace DataLayerTest
 {
@@ -7,12 +9,23 @@ namespace DataLayerTest
 	public class TestBase
 	{
 		protected MongoConnection _mongoConnection;
+		protected Random _random = new Random();
 
 		[TestFixtureSetUp]
 		public void SetUp()
 		{
 			_mongoConnection = MongoConnection.GetConnection("test");
 			_mongoConnection.CleanDatabase();
+		}
+
+		protected Schedule CreateSchedule()
+		{
+			Schedule schedule = new Schedule()
+			{
+				NextAllowedExecution = DateTime.Now,
+			};
+
+			return schedule;
 		}
 	}
 }
