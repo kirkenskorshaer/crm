20ad03893d749da7126106781402be8fc41c6d55
kkkSvend <svend.l@kirkenskorshaer.dk>
2017-01-19 14:43:33 +0100

    (AdministrationTest) testing OptionStatus

---
+++ AdministrationTest/Option/Status/OptionStatusTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministrationTest.Option.Status
+{
+	[TestFixture]
+	public class OptionStatusTest// : TestBase
+	{
+		[Test]
+		public void test()
+		{
+			System.Diagnostics.Process currentProcess = System.Diagnostics.Process.GetCurrentProcess();
+			//long totalBytesOfMemoryUsed = currentProcess.WorkingSet64;
+
+			Console.Out.WriteLine($"Id: {currentProcess.Id}");
+			Console.Out.WriteLine($"VirtualMemorySize64: {currentProcess.VirtualMemorySize64 / (1024 * 1024)} MB");
+		}
+	}
+}
