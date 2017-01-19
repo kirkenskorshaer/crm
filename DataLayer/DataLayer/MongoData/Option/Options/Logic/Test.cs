87afdd108287946f62f8a3d095b80c88355bfdca
kkkSvend <svend.l@kirkenskorshaer.dk>
2017-01-19 14:05:50 +0100

    (DataLayer) adding test option

---
+++ DataLayer/DataLayer/MongoData/Option/Options/Logic/Test.cs
@@ -0,0 +1,21 @@
+﻿namespace DataLayer.MongoData.Option.Options.Logic
+{
+	public class Test : OptionBase
+	{
+		public int SleepForSeconds { get; set; }
+		public bool ThrowException { get; set; }
+
+		public static Test Create(MongoConnection mongoConnection, string name, Schedule schedule, int sleepForSeconds, bool throwException)
+		{
+			Test test = new Test()
+			{
+				SleepForSeconds = sleepForSeconds,
+				ThrowException = throwException,
+			};
+
+			Create(mongoConnection, test, name, schedule);
+
+			return test;
+		}
+	}
+}
