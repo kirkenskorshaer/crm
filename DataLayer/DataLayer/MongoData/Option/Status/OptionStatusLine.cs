f402f05fec315621ac089c663d9b6c647e1fabd2
kkkSvend <svend.l@kirkenskorshaer.dk>
2017-01-19 14:01:26 +0100

    (DataLayer) adding status classes to datalayer

---
+++ DataLayer/DataLayer/MongoData/Option/Status/OptionStatusLine.cs
@@ -0,0 +1,17 @@
+﻿namespace DataLayer.MongoData.Option.Status
+{
+	public class OptionStatusLine
+	{
+		public int FailTotal { get; set; }
+		public int Fail24Hour { get; set; }
+		public int Fail1Hour { get; set; }
+		public int Fail10Minute { get; set; }
+
+		public int SuccessTotal { get; set; }
+		public int Success24Hour { get; set; }
+		public int Success1Hour { get; set; }
+		public int Success10Minute { get; set; }
+
+		public int ExecutionTotal { get; set; }
+	}
+}
