9d75ef98ad5cf7805360c48eb32fa8743455d822
kkkSvend <svend.l@kirkenskorshaer.dk>
2017-02-13 13:29:08 +0100

    (WebInterface) adding Backend.aspx

---
+++ WebInterface/Backend.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Backend.aspx.cs" Inherits="Backend" %>
---
+++ WebInterface/Backend.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Web;
+using System.Collections.Specialized;
+using System.Collections.Generic;
+using Utilities.Converter;
+using System.Text;
+using System.Configuration;
+using DataLayer;
+using DataLayer.MongoData.Option.Options.Logic;
+using WebAdministration;
+
+public partial class Backend : System.Web.UI.Page
+{
+	protected void Page_Load(object sender, EventArgs e)
+	{
+		Dictionary<string, object> inputParameters = ReadInputParameters();
+
+		string databaseName = ConfigurationManager.AppSettings["mongoDatabaseName"];
+		MongoConnection mongoConnection = MongoConnection.GetConnection(databaseName);
+
+		BackendMessage message = BackendMessage.Create(mongoConnection, "Backend Message", inputParameters);
+
+		OptionHandler handler = new OptionHandler(mongoConnection);
+		Dictionary<string, object> outputParameters = handler.GetResponse(message);
+
+		SendOutputParameters(outputParameters);
+	}
+
+	private void SendOutputParameters(Dictionary<string, object> outputParameters)
+	{
+		Response.Clear();
+
+		StringBuilder responseBuilder = new StringBuilder();
+
+		bool isFirst = true;
+		foreach (KeyValuePair<string, object> parameter in outputParameters)
+		{
+			string serializedValue = StringConverter.SerializeToString(parameter.Value);
+
+			if (isFirst == false)
+			{
+				responseBuilder.Append(";");
+			}
+
+			responseBuilder.Append(parameter.Key);
+			responseBuilder.Append(":");
+			responseBuilder.Append(serializedValue);
+
+			isFirst = false;
+		}
+
+		Response.Write(responseBuilder.ToString());
+	}
+
+	private Dictionary<string, object> ReadInputParameters()
+	{
+		NameValueCollection input = Request.Form;
+
+		if (Request.HttpMethod == "POST")
+		{
+			input = Request.Form;
+		}
+		else
+		{
+			input = Request.QueryString;
+		}
+
+		Dictionary<string, object> inputParameters = new Dictionary<string, object>();
+
+		foreach (string key in input)
+		{
+			string keyString = HttpUtility.HtmlDecode(key);
+			string valueString = HttpUtility.HtmlDecode(input[key]);
+
+			object valueObject = StringConverter.DeserializeFromString(valueString);
+
+			if (inputParameters.ContainsKey(keyString) == false)
+			{
+				inputParameters.Add(keyString, valueObject);
+			}
+		}
+
+		return inputParameters;
+	}
+}
