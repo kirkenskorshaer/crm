fb8e4f335a5c58dfc4795dc3d0488040fe22a2b6
kkkSvend <svend.l@kirkenskorshaer.dk>
2017-02-13 13:26:01 +0100

    Adding project DynamicsCrmPluginTest

---
+++ DynamicsCrmPluginTest/Mock/ServiceProviderTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace DynamicsCrmPluginTest.Mock
+{
+	public class ServiceProviderTest : IServiceProvider
+	{
+		private TracingServiceTest _tracingService = new TracingServiceTest();
+		private IPluginExecutionContext _pluginExecutionContext = new PluginExecutionContextTest();
+
+		public object GetService(Type serviceType)
+		{
+			if (serviceType == typeof(ITracingService))
+			{
+				return _tracingService;
+			}
+
+			if (serviceType == typeof(IPluginExecutionContext))
+			{
+				return _pluginExecutionContext;
+			}
+
+			return null;
+		}
+	}
+}
---
+++ DynamicsCrmPluginTest/TestBase.cs
@@ -0,0 +1,18 @@
+﻿using DynamicsCrmPluginTest.Mock;
+using NUnit.Framework;
+using System;
+
+namespace DynamicsCrmPluginTest
+{
+	[TestFixture]
+	public abstract class TestBase
+	{
+		protected IServiceProvider _serviceProvider;
+
+		[OneTimeSetUp]
+		public void TestFixtureSetUp()
+		{
+			_serviceProvider = new ServiceProviderTest();
+		}
+	}
+}
