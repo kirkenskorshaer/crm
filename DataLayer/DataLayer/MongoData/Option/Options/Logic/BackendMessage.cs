db500bc649964a3884afd54e0ae71743c082394a
kkkSvend <svend.l@kirkenskorshaer.dk>
2017-02-13 12:59:12 +0100

    (DataLayer) adding BackendMessage

---
+++ DataLayer/DataLayer/MongoData/Option/Options/Logic/BackendMessage.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataLayer.MongoData.Option.Options.Logic
+{
+	public class BackendMessage : OptionBase
+	{
+		public List<BackendMessageParameter> Parameters = new List<BackendMessageParameter>();
+
+		[BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+		public DateTime ReceivedTime;
+
+		public static BackendMessage Create(MongoConnection mongoConnection, string name, Dictionary<string, object> inputParameters)
+		{
+			BackendMessage message = new BackendMessage()
+			{
+				ReceivedTime = DateTime.Now,
+				Parameters = inputParameters.Select(parameter => new BackendMessageParameter(parameter.Key, parameter.Value)).ToList(),
+			};
+
+			Schedule schedule = new Schedule()
+			{
+				ActionOnFail = Schedule.ActionOnFailEnum.TryAgain,
+				Enabled = false,
+				NextAllowedExecution = DateTime.Now,
+				Recurring = false,
+				TimeBetweenAllowedExecutions = TimeSpan.FromMinutes(10),
+			};
+
+			Create(mongoConnection, message, name, schedule);
+
+			return message;
+		}
+	}
+}
---
+++ DataLayer/DataLayer/MongoData/Option/Options/Logic/BackendMessageParameter.cs
@@ -0,0 +1,14 @@
+﻿namespace DataLayer.MongoData.Option.Options.Logic
+{
+	public class BackendMessageParameter
+	{
+		public string Key;
+		public object Value;
+
+		public BackendMessageParameter(string key, object value)
+		{
+			Key = key;
+			Value = value;
+		}
+	}
+}
