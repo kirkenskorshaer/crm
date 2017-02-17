de49647011a578bd992da23b28eda2a31d78c404
kkkSvend <svend.l@kirkenskorshaer.dk>
2017-02-17 12:52:12 +0100

    (DataLayer) adding CopyNN

---
+++ DataLayer/DataLayer/MongoData/Option/Options/Logic/CopyNN.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace DataLayer.MongoData.Option.Options.Logic
+{
+	public class CopyNN : OptionBase
+	{
+		public string originIntermediateEntityName { get; set; }
+
+		public string insertedEntityName { get; set; }
+		public string insertedEntityIdName { get; set; }
+		public string insertedEntityOriginIdName { get; set; }
+
+		[BsonRepresentation(BsonType.String)]
+		public Guid? insertedEntityId { get; set; }
+
+		public string originEntityIdName { get; set; }
+		public string targetName { get; set; }
+		public string targetIdName { get; set; }
+		public string intermediateEntityRelationshipName { get; set; }
+	}
+}
