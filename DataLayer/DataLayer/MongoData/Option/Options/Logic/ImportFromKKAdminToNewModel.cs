5a5882ca0e1de177ffa290e391f483e77e0ecec8
kkkSvend <svend.l@kirkenskorshaer.dk>
2017-03-31 15:02:02 +0200

    (DataLayer) added ImportFromKKAdminToNewModel

---
+++ DataLayer/DataLayer/MongoData/Option/Options/Logic/ImportFromKKAdminToNewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DataLayer.MongoData.Option.Options.Logic
+{
+	public class ImportFromKKAdminToNewModel : OptionBase
+	{
+		public string ReportFileName { get; set; }
+		public string StamDataFileName { get; set; }
+		public string TilknytningerFileName { get; set; }
+		public string IndbetalingerFileName { get; set; }
+		public bool Import { get; set; }
+		public DateTime FirstValidIndbetaling { get; set; }
+		public int? MaxNumberOfImports { get; set; }
+	}
+}
